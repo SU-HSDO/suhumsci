497c2c913eca2cf46761d79c9be757d6ab20cba4
Mike Decker <pookmish@gmail.com>
2018-05-17 13:44:12 -0700

    added common executable

--- hooks/common/post-db-copy/post-db-copy.sh
+++ hooks/common/post-db-copy/post-db-copy.sh
@@ -14,20 +14,6 @@
 # not support SVN.
 set -ev
 
-site="$1"
-target_env="$2"
-source_branch="$3"
-deployed_tag="$4"
-repo_url="$5"
-repo_type="$6"
-
-# Prep for BLT commands.
-repo_root="/var/www/html/$site.$target_env"
-export PATH=$repo_root/vendor/bin:$PATH
-cd $repo_root
-
-
-drush --root=/var/www/html/[site].[target_env]/docroot @sites updatedb --yes
-drush --root=/var/www/html/[site].[target_env]/docroot @sites cr
+../update-db.sh $1 $2 $3 $4 $5 $6
 
 set +v

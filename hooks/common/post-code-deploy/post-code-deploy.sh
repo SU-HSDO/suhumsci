9b720f018fd3b2def7ed9fbc1f0420157cca05ba
Mike Decker <pookmish@gmail.com>
2018-05-17 14:00:35 -0700

    added archaeology drush alias

--- hooks/common/post-code-deploy/post-code-deploy.sh
+++ hooks/common/post-code-deploy/post-code-deploy.sh
@@ -12,6 +12,24 @@
 
 set -ev
 
-././../update-db.sh $1 $2 $3 $4 $5 $6
+
+site="$1"
+target_env="$2"
+source_branch="$3"
+deployed_tag="$4"
+repo_url="$5"
+repo_type="$6"
+
+# Prep for BLT commands.
+repo_root="/var/www/html/$site.$target_env"
+export PATH=$repo_root/vendor/bin:$PATH
+cd $repo_root
+
+if [[ $target_env == "test" ]]; then
+    target_env="stage"
+fi
+
+drush --root=/var/www/html/[site].[target_env]/docroot -l archaeology-$target_env.stanford.edu updatedb --yes
+drush --root=/var/www/html/[site].[target_env]/docroot -l archaeology-$target_env.stanford.edu cr
 
 set +v

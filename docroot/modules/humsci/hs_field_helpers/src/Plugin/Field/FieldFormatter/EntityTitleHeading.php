41e9ac0359fdecdeacb2401c164a131d26cd6a03
Mike Decker <pookmish@gmail.com>
2018-05-29 19:10:47 -0700

    hide front page h1 title

--- docroot/modules/humsci/hs_field_helpers/src/Plugin/Field/FieldFormatter/EntityTitleHeading.php
+++ docroot/modules/humsci/hs_field_helpers/src/Plugin/Field/FieldFormatter/EntityTitleHeading.php
@@ -2,10 +2,14 @@
 
 namespace Drupal\hs_field_helpers\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Template\Attribute;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provide a string field to be used as a heading.
@@ -18,12 +22,43 @@ use Drupal\Core\Template\Attribute;
  *   }
  * )
  */
-class EntityTitleHeading extends FormatterBase {
+class EntityTitleHeading extends FormatterBase implements ContainerFactoryPluginInterface {
+
+  protected $pathMatcher;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('path.matcher')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, $path_matcher) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+    $this->pathMatcher = $path_matcher;
+  }
 
   /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
+    // Home page we don't want to display an H1 tag (page title).
+    if ($this->pathMatcher->isFrontPage() && $this->getSetting('tag') == 'h1') {
+      return [];
+    }
+
     $attributes = new Attribute();
     $classes = $this->getSetting('classes');
     if (!empty($classes)) {
@@ -35,7 +70,7 @@ class EntityTitleHeading extends FormatterBase {
     $text = $parent->get('title')->getValue()[0]['value'];
 
     if ($this->getSetting('linked')) {
-      $text = $this->l($text, $parent->toUrl());
+      $text = Link::fromTextAndUrl($text, $parent->toUrl())->toString();
     }
     $output[] = [
       '#type' => 'html_tag',
@@ -43,6 +78,7 @@ class EntityTitleHeading extends FormatterBase {
       '#attributes' => $attributes->toArray(),
       '#value' => $text,
     ];
+
     return $output;
   }
 

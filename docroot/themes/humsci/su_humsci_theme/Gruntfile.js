51e3a964f73e34b33dbcd3ff6cd65c3257433652
Joe Knox <jgknox@stanford.edu>
2018-06-19 13:21:00 -0700

    vertical, horizontal, and date stacked vertical patterns

--- docroot/themes/humsci/su_humsci_theme/Gruntfile.js
+++ docroot/themes/humsci/su_humsci_theme/Gruntfile.js
@@ -111,10 +111,10 @@ module.exports = function(grunt) {
           // 'css/ckeditor.css':  'scss/ckeditor.scss',
 
           // PATTERNS
-          'patterns/molecules/featured-card/css/featured-card.css':     'patterns/molecules/featured-card/scss/featured-card.scss',
-          'patterns/molecules/horizontal-card/css/horizontal-card.css': 'patterns/molecules/horizontal-card/scss/horizontal-card.scss',
-          'patterns/organisms/masonry/css/masonry.css':                 'patterns/organisms/masonry/scss/masonry.scss',
-          'patterns/molecules/vertical-card/css/vertical-card.css':     'patterns/molecules/vertical-card/scss/vertical-card.scss',
+          'patterns/molecules/date-stacked-vertical-card/css/date-stacked-vertical-card.css': 'patterns/molecules/date-stacked-vertical-card/scss/date-stacked-vertical-card.scss',
+          'patterns/molecules/horizontal-card/css/horizontal-card.css':                       'patterns/molecules/horizontal-card/scss/horizontal-card.scss',
+          'patterns/organisms/masonry/css/masonry.css':                                       'patterns/organisms/masonry/scss/masonry.scss',
+          'patterns/molecules/vertical-card/css/vertical-card.css':                           'patterns/molecules/vertical-card/scss/vertical-card.scss',
 
           // COMPONENTS
           'css/components/molecules/hover-menu.css':  'scss/components/molecules/hover-menu.scss',

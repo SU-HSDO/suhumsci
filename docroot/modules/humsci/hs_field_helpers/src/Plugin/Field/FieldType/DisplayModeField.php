a85a4c3a412c3f22b644ff7983bc63a3303b7897
Mike Decker <pookmish@gmail.com>
2018-06-21 13:56:42 -0700

    todo comment

--- docroot/modules/humsci/hs_field_helpers/src/Plugin/Field/FieldType/DisplayModeField.php
+++ docroot/modules/humsci/hs_field_helpers/src/Plugin/Field/FieldType/DisplayModeField.php
@@ -36,6 +36,8 @@ class DisplayModeField extends ListItemBase {
       '#element_validate' => [[$this, 'elementValidate']],
     ];
 
+    // Todo: change this to dependency injection when
+    // https://www.drupal.org/node/2053415 is resolved.
     /** @var \Drupal\Core\Entity\EntityDisplayRepository $display_repo */
     $display_repo = \Drupal::service('entity_display.repository');
     $view_modes = $display_repo->getViewModeOptionsByBundle($this->getEntity()

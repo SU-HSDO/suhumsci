f6c947d12ef1ce996bb335d91c0ea0936935c896
Richard Gilbert <gilbert67@gmail.com>
2018-07-05 15:22:38 -0700

    ARCH-131: Final updates and generated css.

--- docroot/themes/humsci/su_humsci_theme/js/nodes/hs_person.js
+++ docroot/themes/humsci/su_humsci_theme/js/nodes/hs_person.js
@@ -3,8 +3,8 @@
   Drupal.behaviors.hsPerson = {
     attach: function (context, settings) {
       $('.node-hs-person-field-hs-person-email', context).once('wrapped').each(function () {
-        $(this).next('.node-hs-person-field-hs-person-office').addBack().wrapAll('<div class="email-office-wrapper" />')
-        if ($(this).parent().hasClass('email-office-wrapper')) {
+        if ($(this).next('.node-hs-person-field-hs-person-office').length) {
+          $(this).next('.node-hs-person-field-hs-person-office').addBack().wrapAll('<div class="email-office-wrapper" />')
           $(this).after('<div class="divider">|</div>');
         }
       });

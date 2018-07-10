5c228783877f8dd0f48efcce4620c9a81fae302e
Joe Knox <jgknox@stanford.edu>
2018-07-10 14:05:59 -0700

    ARCH-183 masonry item shadow fix and general clean up

--- docroot/themes/humsci/su_humsci_theme/Gruntfile.js
+++ docroot/themes/humsci/su_humsci_theme/Gruntfile.js
@@ -113,7 +113,7 @@ module.exports = function(grunt) {
           // PATTERNS
           'patterns/molecules/date-stacked-vertical-card/css/date-stacked-vertical-card.css': 'patterns/molecules/date-stacked-vertical-card/scss/date-stacked-vertical-card.scss',
           'patterns/molecules/horizontal-card/css/horizontal-card.css':                       'patterns/molecules/horizontal-card/scss/horizontal-card.scss',
-          'patterns/organisms/masonry/css/masonry.css':                                       'patterns/organisms/masonry/scss/masonry.scss',
+          'patterns/molecules/masonry-item/css/masonry-item.css':                             'patterns/molecules/masonry-item/scss/masonry-item.scss',
           'patterns/molecules/vertical-card/css/vertical-card.css':                           'patterns/molecules/vertical-card/scss/vertical-card.scss',
 
           // COMPONENTS

b72991745b5deb8915bbdb86ce50c63291f0d719
pookmish <pookmish@stanford.edu>
2018-07-30 16:52:59 -0700

    Arch 306 search (#56)
    * ARCH-306 Use Search API
    
    * added search index views
    
    * style search only in search region
    
    * Changed hooks to be prevent indexing entirely instead of altering the search query
    
    * fixed update hook
    
    * added comment
    
    * ignore course tags that arent translated.
    
    * removed unwanted use
    
    * create translation tag if none exists

--- docroot/themes/humsci/su_humsci_theme/js/su_humsci_theme.js
+++ docroot/themes/humsci/su_humsci_theme/js/su_humsci_theme.js
@@ -1,18 +1,24 @@
-(function($, Drupal) {
+(function ($, Drupal) {
   'use strict';
   Drupal.behaviors.suHumSciTheme = {
-    attach: function(context, settings) {
-      $('.search-block-form input[type=search]').focus(function() {
+    attach: function (context, settings) {
+      $('#search-region .search-block-form input[type=search], #search-region .views-exposed-filter-block.hs-search-search-page input[type=text]', context).focus(function () {
         $(this).closest('form').addClass('expanded');
-      }).blur(function() {
+      }).blur(function () {
         var $this = $(this);
-        setTimeout(function() {
+        setTimeout(function () {
           if (!$this.closest('form').find('input[type=submit]').is(':focus')) {
             $this.closest('form').removeClass('expanded');
           }
         }, 200);
       });
 
+      /**
+       * On the first tab, apply a class to the body.
+       *
+       * @param e
+       *   The keydown event.
+       */
       function handleFirstTab(e) {
         if (e.keyCode === 9) { // the "I am a keyboard user" key
           document.body.classList.add('user-is-tabbing');

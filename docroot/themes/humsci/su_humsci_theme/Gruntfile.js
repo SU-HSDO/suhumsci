c97c932180c97aed0109eebf6162e0ca5894bb63
Mike Decker <pookmish@gmail.com>
2018-08-03 15:05:00 -0700

    ARCH-323 Full Link Postcard

--- docroot/themes/humsci/su_humsci_theme/Gruntfile.js
+++ docroot/themes/humsci/su_humsci_theme/Gruntfile.js
@@ -115,6 +115,7 @@ module.exports = function(grunt) {
           'patterns/molecules/horizontal-card/css/horizontal-card.css':                       'patterns/molecules/horizontal-card/scss/horizontal-card.scss',
           'patterns/molecules/masonry-item/css/masonry-item.css':                             'patterns/molecules/masonry-item/scss/masonry-item.scss',
           'patterns/molecules/vertical-card/css/vertical-card.css':                           'patterns/molecules/vertical-card/scss/vertical-card.scss',
+          'patterns/molecules/vertical-link-card/css/vertical-link-card.css':                 'patterns/molecules/vertical-link-card/scss/vertical-link-card.scss',
           'patterns/molecules/table_row/css/table_row.css':                           'patterns/molecules/table_row/scss/table_row.scss',
           'patterns/organisms/table_pattern/css/table_pattern.css':                           'patterns/organisms/table_pattern/scss/table_pattern.scss',
           'patterns/organisms/masonry/css/masonry.css':                                       'patterns/organisms/masonry/scss/masonry.scss',

da6cbd46aa1c54b6e54687bd549b8acb058d0369
Mike Decker <pookmish@gmail.com>
2018-08-06 13:29:02 -0700

    ARCH-309 Permissions to edit block content

---
+++ docroot/profiles/humsci/su_humsci_profile/src/Routing/RouteSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\su_humsci_profile\Routing;
+
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class RouteSubscriber
+ *
+ * @package Drupal\su_humsci_profile\Routing
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * RouteSubscriber constructor.
+   *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   */
+  public function __construct(ModuleHandlerInterface $module_handler) {
+    $this->moduleHandler = $module_handler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if (($route = $collection->get('block.admin_display')) && $this->moduleHandler->moduleExists('block_content_permissions')) {
+      $route->setRequirement('_permission', 'administer blocks+view restricted block content');
+    }
+  }
+
+}

019a9845f5abd65b4eb2c67ffd3dbc613fc4714e
Mike Decker <pookmish@gmail.com>
2018-08-16 09:47:49 -0700

    namespaced

--- docroot/themes/humsci/su_humsci_admin/js/node-protection.js
+++ docroot/themes/humsci/su_humsci_admin/js/node-protection.js
@@ -10,7 +10,7 @@
   // Flag the form has been edited.
   var edit = false;
 
-  Drupal.behaviors.nodeEditProtection = {
+  Drupal.behaviors.SuHumsciAdminNodeProtection = {
     attach: function (context, settings) {
       // Add listeners for all types of data entry.
       $("input, button, :input", context).on('blur change click dblclick keydown mousedown select submit', function (e) {

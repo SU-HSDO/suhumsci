78c88372888ee54d662c4b6e6447c7707d7fba15
Mike Decker <pookmish@gmail.com>
2018-08-17 22:39:37 -0700

    changed web directory

--- .circleci/RoboFile.php
+++ .circleci/RoboFile.php
@@ -189,9 +189,9 @@ class RoboFile extends \Robo\Tasks {
 //          ->option('verbose')
 //          ->option('yes');
     $tasks[] = $this->drush()->rawArg("@$site.dev sql-connect");
-    $tasks[] = $this->drush()->rawArg("@$site.dev sql-dump > clean_dump.sql");
+    $tasks[] = $this->drush()->rawArg("@$site.dev sql-dump > dump.sql");
     $tasks[] = $this->taskExecStack()->exec("grep -v '^Connection to' dump.sql > clean_dump.sql");
-    $tasks[] = $this->drush()->dir('docroot')->rawArg('sql-cli < dump.sql');
+    $tasks[] = $this->drush()->rawArg('sql-cli < clean_dump.sql');
     return $tasks;
   }
 

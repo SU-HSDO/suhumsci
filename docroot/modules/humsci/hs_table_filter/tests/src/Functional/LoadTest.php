36c4c42cafae4ec5eb4e9ce81dabec877659b50f
Mike Decker <pookmish@gmail.com>
2018-08-24 16:30:52 -0700

    make tests pass

--- docroot/modules/humsci/hs_table_filter/tests/src/Functional/LoadTest.php
+++ docroot/modules/humsci/hs_table_filter/tests/src/Functional/LoadTest.php
@@ -8,7 +8,7 @@ use Drupal\Tests\BrowserTestBase;
 /**
  * Simple test to ensure that main page loads with module enabled.
  *
- * @group hs_table_formatter
+ * @group hs_table_filter
  */
 class LoadTest extends BrowserTestBase {
 
@@ -17,7 +17,7 @@ class LoadTest extends BrowserTestBase {
    *
    * @var array
    */
-  public static $modules = ['hs_table_formatter'];
+  public static $modules = ['hs_table_filter'];
 
   /**
    * A user with permission to administer site configuration.
---
+++ docroot/modules/humsci/hs_table_filter/tests/src/Unit/HsTableFilterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\Tests\hs_table_filter\Unit;
+
+use Drupal\hs_table_filter\Plugin\Filter\HsTableFilter;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\hs_table_filter\Plugin\Filter\HsTableFilter
+ *
+ * @group hs_table_filter
+ */
+class HsTableFilterTest extends UnitTestCase {
+
+  /**
+   * @covers ::process
+   */
+  public function testTableFilter() {
+    $this->assertEquals(1, 1);
+  }
+
+}

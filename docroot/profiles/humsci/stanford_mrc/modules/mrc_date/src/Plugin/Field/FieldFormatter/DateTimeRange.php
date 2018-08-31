7ba735415d90f458e7c7ba8a7967722951de2abd
Mike Decker <pookmish@gmail.com>
2018-08-31 09:20:44 -0700

    MRCD8-349 allow more years in date picker

--- docroot/profiles/humsci/stanford_mrc/modules/mrc_date/src/Plugin/Field/FieldFormatter/DateTimeRange.php
+++ docroot/profiles/humsci/stanford_mrc/modules/mrc_date/src/Plugin/Field/FieldFormatter/DateTimeRange.php
@@ -2,16 +2,10 @@
 
 namespace Drupal\mrc_date\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\datetime\Plugin\Field\FieldFormatter\DateTimeCustomFormatter;
-use Drupal\datetime_range\DateTimeRangeTrait;
+use Drupal\hs_field_helpers\Plugin\Field\FieldFormatter\DateTimeRange as NewDateTimeRange;
 
 /**
- * Plugin implementation of the 'Custom' formatter for 'daterange' fields.
- *
- * This formatter renders the data range as plain text, with a fully
- * configurable date format using the PHP date syntax and separator.
+ * Placeholder for formatter id, the code has been moved into hs_field_helpers.
  *
  * @FieldFormatter(
  *   id = "datetimerange_custom",
@@ -21,65 +15,6 @@ use Drupal\datetime_range\DateTimeRangeTrait;
  *   }
  * )
  */
-class DateTimeRange extends DateTimeCustomFormatter {
-
-  use DateTimeRangeTrait;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function defaultSettings() {
-    return [
-        'display' => 'start_date',
-      ] + parent::defaultSettings();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function viewElements(FieldItemListInterface $items, $langcode) {
-
-    $elements = [];
-    $display = $this->getSetting('display');
-    foreach ($items as $delta => $item) {
-      if (!empty($item->{$display})) {
-        /** @var \Drupal\Core\Datetime\DrupalDateTime $date */
-        $date = $item->{$display};
-        $elements[$delta] = $this->buildDate($date);
-      }
-    }
-
-    return $elements;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsForm(array $form, FormStateInterface $form_state) {
-    $form = parent::settingsForm($form, $form_state);
-    unset($form['separator']);
-    $form['display'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Display'),
-      '#default_value' => $this->getSetting('display'),
-      '#options' => [
-        'start_date' => $this->t('Start date only'),
-        'end_date' => $this->t('End date only'),
-      ],
-    ];
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsSummary() {
-    $summary = parent::settingsSummary();
-    if ($display = $this->getSetting('display')) {
-      $summary[] = $this->t('Display: %display', ['%display' => $display]);
-    }
-    return $summary;
-  }
+class DateTimeRange extends NewDateTimeRange {
 
 }

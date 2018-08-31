98754bbf497fa3c74074a521de6ead9a19462714
Mike Decker <pookmish@gmail.com>
2018-08-31 14:18:32 -0700

    improved filter form

--- docroot/modules/humsci/hs_field_helpers/src/Plugin/views/filter/AcademicDateFilter.php
+++ docroot/modules/humsci/hs_field_helpers/src/Plugin/views/filter/AcademicDateFilter.php
@@ -4,6 +4,8 @@ namespace Drupal\hs_field_helpers\Plugin\views\filter;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\datetime\Plugin\views\filter\Date;
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Drupal\views\ViewExecutable;
 
 /**
  * Class AcademicDateFilter
@@ -19,7 +21,12 @@ class AcademicDateFilter extends Date {
    */
   protected function defineOptions() {
     $options = parent::defineOptions();
-    //    $options['contains']['exception']['default'] = 0;
+    $options['value']['contains']['exception']['default'] = 0;
+    $options['value']['contains']['exception_start']['default'] = NULL;
+    $options['value']['contains']['exception_end']['default'] = NULL;
+    $options['value']['contains']['exception_value']['default'] = '';
+    $options['value']['contains']['exception_min']['default'] = '';
+    $options['value']['contains']['exception_max']['default'] = '';
     return $options;
   }
 
@@ -28,59 +35,63 @@ class AcademicDateFilter extends Date {
    */
   protected function valueForm(&$form, FormStateInterface $form_state) {
     parent::valueForm($form, $form_state);
-    if (!$form_state->get('exposed')) {
-      $form['exception'] = [
-        '#type' => 'details',
-        '#title' => $this->t('Exception Time Frame'),
-        '#open' => FALSE,
-        '#tree' => TRUE,
-      ];
-      $form['exception']['exception'] = [
-        '#type' => 'checkbox',
-        '#title' => $this->t('Add Exception Time Frame'),
-        //        '#default_value' => isset($this->value['exception']) ? $this->value['exception'] : 0,
-      ];
-      $states = [
-        'visible' => [
-          '::input[name="options[exception][exception]"]' => ['checked' => TRUE],
-        ],
-      ];
-
-      $form['exception']['exception_start'] = [
-        '#type' => 'date',
-        '#title' => $this->t('Start Exception'),
-        '#states' => $states,
-      ];
-
-      $form['exception']['exception_end'] = [
-        '#type' => 'date',
-        '#title' => $this->t('End Exception'),
-        '#states' => $states,
-      ];
-
-      $form['exception']['exception_value'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('Exception Value'),
-        '#size' => 30,
-        '#states' => $states,
-      ];
-
-      $form['exception']['exception_min'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('Min'),
-        '#size' => 30,
-        '#states' => $states,
-      ];
-
-      $form['exception']['exception_max'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('Max'),
-        '#size' => 30,
-        '#states' => $states,
-      ];
-
-      dpm($form);
+    if ($form_state->get('exposed')) {
+      return;
     }
+
+    $form['value']['exception'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Add Exception Time Frame'),
+      '#default_value' => $this->value['exception'] ?? 0,
+    ];
+
+    $form['value']['exception_start'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Start Exception'),
+      '#states' => [
+        'visible' => [
+          ':input[name="options[value][exception]"]' => ['checked' => TRUE],
+        ],
+      ],
+      '#default_value' => $this->value['exception_start'] ?? NULL,
+    ];
+
+    $form['value']['exception_end'] = [
+      '#type' => 'date',
+      '#title' => $this->t('End Exception'),
+      '#states' => [
+        'visible' => [
+          ':input[name="options[value][exception]"]' => ['checked' => TRUE],
+        ],
+      ],
+      '#default_value' => $this->value['exception_end'] ?? NULL,
+    ];
+
+    $form['value']['exception_value'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Exception Value'),
+      '#size' => 30,
+      '#states' => $form['value']['value']['#states'],
+      '#default_value' => $this->value['exception_value'] ?? '',
+    ];
+
+    $form['value']['exception_min'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Min'),
+      '#size' => 30,
+      '#states' => $form['value']['min']['#states'],
+      '#default_value' => $this->value['exception_min'] ?? '',
+    ];
+
+    $form['value']['exception_max'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Max'),
+      '#size' => 30,
+      '#states' => $form['value']['max']['#states'],
+      '#default_value' => $this->value['exception_max'] ?? '',
+    ];
+
+//        dpm($form);
   }
 
 }

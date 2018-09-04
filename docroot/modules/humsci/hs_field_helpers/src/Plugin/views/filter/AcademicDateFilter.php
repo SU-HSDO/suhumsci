8a1e13463e300a05a925a165fa453f4ede444fc5
Mike Decker <pookmish@gmail.com>
2018-09-04 11:44:13 -0700

    course filter query

--- docroot/modules/humsci/hs_field_helpers/src/Plugin/views/filter/AcademicDateFilter.php
+++ docroot/modules/humsci/hs_field_helpers/src/Plugin/views/filter/AcademicDateFilter.php
@@ -2,10 +2,9 @@
 
 namespace Drupal\hs_field_helpers\Plugin\views\filter;
 
+use Drupal\Component\Datetime\DateTimePlus;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\datetime\Plugin\views\filter\Date;
-use Drupal\views\Plugin\views\display\DisplayPluginBase;
-use Drupal\views\ViewExecutable;
 
 /**
  * Class AcademicDateFilter
@@ -22,8 +21,10 @@ class AcademicDateFilter extends Date {
   protected function defineOptions() {
     $options = parent::defineOptions();
     $options['value']['contains']['exception']['default'] = 0;
-    $options['value']['contains']['exception_start']['default'] = NULL;
-    $options['value']['contains']['exception_end']['default'] = NULL;
+    $options['value']['contains']['exception_start_month']['default'] = NULL;
+    $options['value']['contains']['exception_start_day']['default'] = NULL;
+    $options['value']['contains']['exception_end_month']['default'] = NULL;
+    $options['value']['contains']['exception_end_day']['default'] = NULL;
     $options['value']['contains']['exception_value']['default'] = '';
     $options['value']['contains']['exception_min']['default'] = '';
     $options['value']['contains']['exception_max']['default'] = '';
@@ -45,26 +46,56 @@ class AcademicDateFilter extends Date {
       '#default_value' => $this->value['exception'] ?? 0,
     ];
 
-    $form['value']['exception_start'] = [
-      '#type' => 'date',
-      '#title' => $this->t('Start Exception'),
+    $months = cal_info(0);
+    $days = range(0, 31);
+    unset($days[0]);
+
+    $form['value']['exception_start_month'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Start Exception Month'),
+      '#default_value' => $this->value['exception_start_month'] ?? NULL,
+      '#options' => $months['months'],
       '#states' => [
         'visible' => [
           ':input[name="options[value][exception]"]' => ['checked' => TRUE],
         ],
       ],
-      '#default_value' => $this->value['exception_start'] ?? NULL,
     ];
 
-    $form['value']['exception_end'] = [
-      '#type' => 'date',
-      '#title' => $this->t('End Exception'),
+    $form['value']['exception_start_day'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Start Exception Day'),
+      '#default_value' => $this->value['exception_start_day'] ?? NULL,
+      '#options' => $days,
       '#states' => [
         'visible' => [
           ':input[name="options[value][exception]"]' => ['checked' => TRUE],
         ],
       ],
-      '#default_value' => $this->value['exception_end'] ?? NULL,
+    ];
+
+    $form['value']['exception_end_month'] = [
+      '#type' => 'select',
+      '#title' => $this->t('End Exception Month'),
+      '#options' => $months['months'],
+      '#default_value' => $this->value['exception_end_month'] ?? NULL,
+      '#states' => [
+        'visible' => [
+          ':input[name="options[value][exception]"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
+    $form['value']['exception_end_day'] = [
+      '#type' => 'select',
+      '#title' => $this->t('End Exception Day'),
+      '#options' => $days,
+      '#default_value' => $this->value['exception_end_day'] ?? NULL,
+      '#states' => [
+        'visible' => [
+          ':input[name="options[value][exception]"]' => ['checked' => TRUE],
+        ],
+      ],
     ];
 
     $form['value']['exception_value'] = [
@@ -90,8 +121,71 @@ class AcademicDateFilter extends Date {
       '#states' => $form['value']['max']['#states'],
       '#default_value' => $this->value['exception_max'] ?? '',
     ];
+  }
 
-//        dpm($form);
+  /**
+   * {@inheritdoc}
+   */
+  protected function opBetween($field) {
+    if ($this->inException()) {
+      $this->value['value'] = $this->value['exception_value'];
+      $this->value['min'] = $this->value['exception_min'];
+      $this->value['max'] = $this->value['exception_max'];
+    }
+    parent::opBetween($field);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function opSimple($field) {
+    if ($this->inException()) {
+      $this->value['value'] = $this->value['exception_value'];
+      $this->value['min'] = $this->value['exception_min'];
+      $this->value['max'] = $this->value['exception_max'];
+    }
+    parent::opSimple($field);
+  }
+
+  /**
+   * @return bool
+   * @throws \Exception
+   */
+  protected function inException() {
+    if ($this->value['exception']) {
+      $this_year = date('Y');
+      $end_year = $this_year;
+
+      // Add one year if the start and end dates span over January 1st.
+      if ($this->value['exception_start_month'] > $this->value['exception_end_month']) {
+        $end_year++;
+      }
+      elseif ($this->value['exception_start_month'] == $this->value['exception_end_month'] && $this->value['exception_start_day'] > $this->value['exception_end_day']) {
+        $end_year++;
+      }
+
+      try {
+        $start_exception = "$this_year-{$this->value['exception_start_month']}-{$this->value['exception_start_day']}";
+        $end_exception = "$end_year-{$this->value['exception_end_month']}-{$this->value['exception_end_day']}";
+
+        $timezone = $this->getTimezone();
+        $start = new DateTimePlus($start_exception, new \DateTimeZone($timezone));
+        $end = new DateTimePlus($end_exception, new \DateTimeZone($timezone));
+
+        // Add 1 day to the end date since the timestamp will be midnight of that
+        // day. This will include the end date in the exception.
+        $end->add(new \DateInterval('P2D'));
+      }
+      catch (\Exception $e) {
+        return FALSE;
+      }
+
+      $now = time();
+      if ($now >= $start->getTimestamp() && $now <= $end->getTimestamp()) {
+        return TRUE;
+      }
+    }
+    return FALSE;
   }
 
 }

86065520d7d58ff5bb798e945ec26f5f0f544a75
Mike Decker <pookmish@gmail.com>
2018-09-06 14:34:54 -0700

    simply click on the submit button

--- docroot/modules/humsci/hs_field_helpers/js/hs_field_helpers.views_reset.js
+++ docroot/modules/humsci/hs_field_helpers/js/hs_field_helpers.views_reset.js
@@ -3,16 +3,23 @@
 
   Drupal.behaviors.hsFieldHelpersViewsReset = {
     attach: function attach(context, settings) {
-      console.log(context);
-      console.log(Drupal.views);
 
-      $.each(Drupal.views.instances, function (key, view) {
-        $(view['$exposed_form']).find('input[data-drupal-selector=edit-reset]').click(function (e) {
-          e.preventDefault();
+      $.each(Drupal.views.instances, function (key, ajaxView) {
+        ajaxView['$exposed_form'].find('input[data-drupal-selector=edit-reset]').once('views-reset').each(function () {
+          $(this).click(function (e) {
+            e.preventDefault();
 
-          $(view['$exposed_form'])[0].reset();
-          $(view['$view']).trigger('RefreshView');
-        });
+            // Reset the form and trigger chosen select fields.
+            ajaxView['$exposed_form'][0].reset();
+            $(ajaxView['$exposed_form'][0]).find('select').trigger("chosen:updated");
+
+            // Trigger('RefreshView') causes the view to reload twice. So we use
+            // a click action on the submit button after clearing all the field
+            // values. Keeping the RefreshView in case we need it later.
+            // Drupal.views.instances[key]['$view'].trigger('RefreshView');
+            $(this).siblings('input[data-drupal-selector^="edit-submit-"]').click();
+          });
+        })
       });
     }
   };

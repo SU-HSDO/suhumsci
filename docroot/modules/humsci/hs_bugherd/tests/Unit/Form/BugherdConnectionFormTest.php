eab473142a5f24ed3f42530296e265c0ca7266d4
Mike Decker <pookmish@gmail.com>
2018-09-25 15:19:49 -0700

    test

--- docroot/modules/humsci/hs_bugherd/tests/Unit/Form/BugherdConnectionFormTest.php
+++ docroot/modules/humsci/hs_bugherd/tests/Unit/Form/BugherdConnectionFormTest.php
@@ -7,6 +7,8 @@ use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\EntityTypeRepositoryInterface;
 use Drupal\Core\Form\FormState;
+use Drupal\Core\Language\Language;
+use Drupal\hs_bugherd\Entity\BugherdConnection;
 use Drupal\hs_bugherd\Form\BugherdConnectionForm;
 use Drupal\hs_bugherd\HsBugherd;
 use Drupal\Tests\UnitTestCase;
@@ -22,7 +24,37 @@ class BugherdConnectionFormTest extends UnitTestCase {
   protected function setUp() {
     parent::setUp();
 
+
+    $this->entityTypeId = $this->randomMachineName();
+
+    $this->entityType = $this->getMock('\Drupal\Core\Entity\EntityTypeInterface');
+    $this->entityType->expects($this->any())
+      ->method('getListCacheTags')
+      ->willReturn([$this->entityTypeId . '_list']);
+
+    $this->entityTypeManager = $this->getMockForAbstractClass(EntityTypeManagerInterface::class);
+    $this->entityTypeManager->expects($this->any())
+      ->method('getDefinition')
+      ->with($this->entityTypeId)
+      ->will($this->returnValue($this->entityType));
+
+    $this->uuid = $this->getMock('\Drupal\Component\Uuid\UuidInterface');
+
+    $this->languageManager = $this->getMock('\Drupal\Core\Language\LanguageManagerInterface');
+    $this->languageManager->expects($this->any())
+      ->method('getLanguage')
+      ->with('en')
+      ->will($this->returnValue(new Language(['id' => 'en'])));
+
+    $this->cacheTagsInvalidator = $this->getMock('Drupal\Core\Cache\CacheTagsInvalidator');
+
     $container = new ContainerBuilder();
+    // Ensure that Entity doesn't use the deprecated entity.manager service.
+    $container->set('entity.manager', NULL);
+    $container->set('entity_type.manager', $this->entityTypeManager);
+    $container->set('uuid', $this->uuid);
+    $container->set('language_manager', $this->languageManager);
+    $container->set('cache_tags.invalidator', $this->cacheTagsInvalidator);
 
     $bugherd_api = $this->createMock(HsBugherd::class);
     $bugherd_api->method('getProjects')->willReturn(['123' => 'Test Project']);
@@ -30,12 +62,8 @@ class BugherdConnectionFormTest extends UnitTestCase {
       ->with('123')
       ->willReturn(['devurl' => 'http://example.com']);
     $container->set('hs_bugherd', $bugherd_api);
-    $entity_type = $this->getMockForAbstractClass(EntityTypeRepositoryInterface::class);
-    $container->set('entity_type.repository', $entity_type);
-    $entity_type_manager = $this->getMockForAbstractClass(EntityTypeManagerInterface::class);
-    $entity_type_manager->method('getStorage')
-      ->willReturn($this->getMockForAbstractClass(ConfigEntityStorageInterface::class));
-    $container->set('entity_type.manager', $entity_type_manager);
+
+
     \Drupal::setContainer($container);
   }
 

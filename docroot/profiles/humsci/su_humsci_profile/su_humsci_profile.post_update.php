d2a01208276867b7d16f23884104cdd11ec09b7c
pookmish <pookmish@stanford.edu>
2018-09-25 19:06:23 -0700

    HSD8-283 Removed blocks in favor of menu item (#102)
    * HSD8-283 Removed blocks in favor of menu item
    
    * add block to user page

--- docroot/profiles/humsci/su_humsci_profile/su_humsci_profile.post_update.php
+++ docroot/profiles/humsci/su_humsci_profile/su_humsci_profile.post_update.php
@@ -5,6 +5,8 @@
  * su_humsci_profile.post_update.php
  */
 
+use Drupal\block\Entity\Block;
+
 /**
  * Outdated.
  */
@@ -24,4 +26,16 @@ function su_humsci_profile_post_update_8_0_2() {
  */
 function su_humsci_profile_post_update_8_0_3() {
 
+}
+
+/**
+ * Delete masquerade blocks.
+ */
+function su_humsci_profile_post_update_8_0_4() {
+  if ($block = Block::load('seven_masquerade')) {
+    $block->delete();
+  }
+  if ($block = Block::load('su_humsci_admin_masquerade')) {
+    $block->delete();
+  }
 }

45455609ca9bca4099a113316d5544fb3dbaed71
Joe Knox <jgknox@stanford.edu>
2018-10-11 12:58:38 -0700

    theme settings comment cleanup

--- docroot/themes/humsci/su_humsci_theme/theme-settings.php
+++ docroot/themes/humsci/su_humsci_theme/theme-settings.php
@@ -4,9 +4,3 @@
  * @file
  * Provides an additional config form for theme settings.
  */
-
-/**
- * Implements hook_form_system_theme_settings_alter().
- *
- * Form override for theme settings.
- */

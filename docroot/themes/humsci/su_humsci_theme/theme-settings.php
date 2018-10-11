d2673700747ef20ec6df5154674b2615163daa8d
Joe Knox <jgknox@stanford.edu>
2018-10-11 11:32:31 -0700

    theme cleanup

--- docroot/themes/humsci/su_humsci_theme/theme-settings.php
+++ docroot/themes/humsci/su_humsci_theme/theme-settings.php
@@ -12,22 +12,3 @@ use Drupal\Core\Form\FormStateInterface;
  *
  * Form override for theme settings.
  */
-function su_humsci_theme_form_system_theme_settings_alter(array &$form, FormStateInterface $form_state) {
-  $form['humsci_settings'] = [
-    '#type' => 'details',
-    '#title' => t('Humsci Settings'),
-    '#open' => TRUE,
-  ];
-  $form['humsci_settings']['humsci_site_styles'] = [
-    '#type' => 'select',
-    '#title' => t('Choose a site style'),
-    '#empty_option' => t('- Default -'),
-    '#default_value' => theme_get_setting('humsci_site_styles'),
-    '#options' => [
-      'archaeology' => t('Archaeology'),
-      'francestanford' => t('France Stanford'),
-    ],
-  ];
-
-  stanford_basic_form_system_theme_settings_alter($form, $form_state);
-}

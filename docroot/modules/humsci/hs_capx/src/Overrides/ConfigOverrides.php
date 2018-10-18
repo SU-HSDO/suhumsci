6f6d435f1469cc1826bca81e878888b12748b6cd
Mike Decker <pookmish@gmail.com>
2018-10-17 17:51:59 -0700

    working importer with simple mapping

--- docroot/modules/humsci/hs_capx/src/Overrides/ConfigOverrides.php
+++ docroot/modules/humsci/hs_capx/src/Overrides/ConfigOverrides.php
@@ -3,8 +3,10 @@
 namespace Drupal\hs_capx\Overrides;
 
 use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Config\ConfigFactoryOverrideInterface;
 use Drupal\Core\Config\StorageInterface;
+use Drupal\key\Entity\Key;
 
 /**
  * Class ConfigOverrides.
@@ -14,13 +16,46 @@ use Drupal\Core\Config\StorageInterface;
 class ConfigOverrides implements ConfigFactoryOverrideInterface {
 
   /**
+   * Config factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * ConfigOverrides constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   Config factory service.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
    * {@inheritdoc}
    *
    * Override the path to the key for the encryption profile.
    */
   public function loadOverrides($names) {
     $overrides = [];
+    if (in_array('migrate_plus.migration.hs_capx', $names)) {
 
+      $config = $this->configFactory->get('hs_capx.settings');
+      $password = '';
+      if ($key = Key::load($config->get('password') ?: '')) {
+        $password = $key->getKeyValue();
+      }
+
+      $overrides['migrate_plus.migration.hs_capx'] = [
+        'source' => [
+          'authentication' => [
+            'client_id' => $config->get('username'),
+            'client_secret' => $password,
+          ],
+        ],
+      ];
+    }
     return $overrides;
   }
 

1f41f34ed8e7b9313c17516947165ba05d85ecb4
Mike Decker <pookmish@gmail.com>
2018-10-17 17:05:38 -0700

    finished autocomplete

--- docroot/modules/humsci/hs_capx/src/Controller/CapxOrgAutocomplete.php
+++ docroot/modules/humsci/hs_capx/src/Controller/CapxOrgAutocomplete.php
@@ -9,35 +9,59 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * Class CapxOrgAutocomplete
+ * Class CapxOrgAutocomplete.
  *
  * @package Drupal\hs_capx\Controller
  */
 class CapxOrgAutocomplete extends ControllerBase {
 
+  /**
+   * @var \Drupal\Core\Database\Connection
+   */
   protected $database;
 
+  /**
+   * {@inheritdoc}
+   */
   public static function create(ContainerInterface $container) {
     return new static($container->get('database'));
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function __construct(Connection $database) {
     $this->database = $database;
   }
 
+  /**
+   * Get organizations that match the autocomplete text entry.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   Ajax Reqeust.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   Found results response.
+   */
   public function autocomplete(Request $request) {
     $string = mb_strtolower($request->query->get('q'));
     /** @var \Drupal\Core\Database\Statement $aresults */
-    $results = $this->database->select('hs_capx_organizations', 'c')
+    $query_results = $this->database->select('hs_capx_organizations', 'c')
       ->fields('c', [
         'alias',
+        'orgcodes',
         'name',
       ])
       ->condition('name', "%$string%", 'LIKE')
-      ->
+      ->range(0, 10)
       ->execute()
-      ->fetchAllKeyed();
-dpm($results);
+      ->fetchAllAssoc('alias');
+
+    $results = [];
+    foreach ($query_results as $alias => $item) {
+      $org_codes = unserialize($item->orgcodes);
+      $results[] = ['value' => end($org_codes), 'label' => $item->name];
+    }
     return new JsonResponse($results);
   }
 

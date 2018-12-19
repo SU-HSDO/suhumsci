17b29f2972d82527c34e2dabd8c8603704be6526
Mike Decker <pookmish@gmail.com>
2018-12-19 11:39:32 -0800

    HSD8-540 Added granular permission and link to add sunetid user

---
+++ docroot/profiles/humsci/su_humsci_profile/src/Plugin/Derivative/HumsciSSPLocalAction.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\su_humsci_profile\Plugin\Derivative;
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class HumsciSSPLocalAction to definite derivative local action links.
+ *
+ * @package Drupal\su_humsci_profile\Plugin\Derivative
+ */
+class HumsciSSPLocalAction extends DeriverBase implements ContainerDeriverInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * Module Handler service..
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    return new static($container->get('module_handler'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(ModuleHandlerInterface $module_handler) {
+    $this->moduleHandler = $module_handler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $this->derivatives = [];
+    if ($this->moduleHandler->moduleExists('stanford_ssp')) {
+      $this->derivatives['su_humsci_profile.add_user'] = [
+        'route_name' => 'stanford_ssp.create_user',
+        'title' => $this->t('Add SUNetID User'),
+        'appears_on' => ['entity.user.collection'],
+      ];
+    }
+
+    return $this->derivatives;
+  }
+
+}

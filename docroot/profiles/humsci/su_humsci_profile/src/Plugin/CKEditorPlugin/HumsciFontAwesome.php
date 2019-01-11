439ae3d899bb13e9524108b2c62b5f000b2ac2e6
Mike Decker <pookmish@gmail.com>
2019-01-11 14:24:26 -0800

    cc fixes

--- docroot/profiles/humsci/su_humsci_profile/src/Plugin/CKEditorPlugin/HumsciFontAwesome.php
+++ docroot/profiles/humsci/su_humsci_profile/src/Plugin/CKEditorPlugin/HumsciFontAwesome.php
@@ -4,6 +4,13 @@ namespace Drupal\su_humsci_profile\Plugin\CKEditorPlugin;
 
 use Drupal\fontawesome\Plugin\CKEditorPlugin\DrupalFontAwesome;
 
+/**
+ * Extend and override the font awesome plugin to change the paths to the js.
+ *
+ * This allows us to change the CKEditor images. It is the only way to do so.
+ *
+ * @package Drupal\su_humsci_profile\Plugin\CKEditorPlugin
+ */
 class HumsciFontAwesome extends DrupalFontAwesome {
 
   /**
@@ -18,7 +25,7 @@ class HumsciFontAwesome extends DrupalFontAwesome {
    */
   public function getButtons() {
     $buttons = parent::getButtons();
-    $buttons['DrupalFontAwesome']['image'] = drupal_get_path('profile', 'su_humsci_profile') .'/js/plugins/drupalfontawesome/icons/drupalfontawesome.png';
+    $buttons['DrupalFontAwesome']['image'] = drupal_get_path('profile', 'su_humsci_profile') . '/js/plugins/drupalfontawesome/icons/drupalfontawesome.png';
     return $buttons;
   }
 

1c0107f67a776d649b7cc859626237576ad21a94
Mike Decker <pookmish@gmail.com>
2019-01-29 17:08:25 -0800

    Stubbed out action class

--- docroot/profiles/humsci/su_humsci_profile/src/Plugin/Action/CloneNode.php
+++ docroot/profiles/humsci/su_humsci_profile/src/Plugin/Action/CloneNode.php
@@ -2,11 +2,13 @@
 
 namespace Drupal\su_humsci_profile\Plugin\Action;
 
-use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Action\ConfigurableActionBase;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
 
 /**
- * Promotes a node.
+ * Clones a node.
  *
  * @Action(
  *   id = "node_clone_action",
@@ -14,13 +16,52 @@ use Drupal\Core\Session\AccountInterface;
  *   type = "node"
  * )
  */
-class CloneNode extends ActionBase {
+class CloneNode extends ConfigurableActionBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'clone_count' => 0,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['clone_count'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Clone how many times'),
+      '#options' => range(1, 10),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['clone_count'] = $form_state->getValue('clone_count');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
-    // TODO: Implement access() method.
+    /** @var \Drupal\node\NodeInterface $object */
+    $result = $object->access('update', $account, TRUE)
+      ->andIf($object->getOwner()->access('edit', $account, TRUE));
+
+    return $return_as_object ? $result : $result->isAllowed();
   }
 
-  public function execute() {
-    // TODO: Implement execute() method.
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    /** @var \Drupal\node\NodeInterface $entity */
   }
+
 }

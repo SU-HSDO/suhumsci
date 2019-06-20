76b65b85be7d4fe07daa244a361288b3621bf274
pookmish <pookmish@gmail.com>
2019-06-20 12:11:13 -0700

    Test circleci workflow (#301)
    * Use circleci workflow to deploy to acquia
    
    * fixed deploy branch command
    
    * fixed array_rand
    
    * use shell commands instead of robo
    
    * add known hosts
    
    * add known hosts
    
    * changed ssh-keyscan
    
    * remove dryrun
    
    * run only deploy task
    
    * set git config email and name
    
    * deploy tag
    
    * use variables in deploy for tag
    
    * removed unused code
    
    * deploy any tag
    
    * updated documentation
    
    * use release it to automatically create a release
    
    * Use robo commands instead of npm package to do github release
    
    * cc fix
    
    * parse git url instead of hard coded git uri

--- .circleci/RoboFile.php
+++ .circleci/RoboFile.php
@@ -1,6 +1,8 @@
 <?php
 
+use Robo\Contract\VerbosityThresholdInterface;
 use Robo\Tasks;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Yaml;
 
 /**
@@ -22,6 +24,13 @@ class RoboFile extends Tasks {
   const DB_URL = 'mysql://root@127.0.0.1/drupal8';
 
   /**
+   * Number of random sites to test behat.
+   *
+   * @var integer
+   */
+  const SITES_TO_TEST = 6;
+
+  /**
    * Directory of drupal installation.
    *
    * @var string
@@ -41,6 +50,187 @@ class RoboFile extends Tasks {
    * @var array
    */
   protected $bltConfig;
+
+  /**
+   * Perform a release in github.
+   */
+  public function jobGithubRelease() {
+
+    $last_version = $this->getLastVersion();
+    // Increment the last version by 1.
+    $version = $this->incrementVersion($last_version);
+    $this->yell("Releasing $version");
+
+    // Get a list of all commits since the last version until now.
+    exec("git log --pretty=format:%h $last_version...HEAD", $commit_hashes);
+
+    $changes = [];
+    // Build an array of change strings.
+    foreach ($commit_hashes as $hash) {
+      exec("git log --format=%B -n 1 $hash", $log);
+      $log = implode(';', array_filter($log));
+      $changes[] = "* $log ($hash)";
+    }
+
+    // Set module and profile version. Then update the changelog.
+    $this->setVersions($version);
+    $this->updateChangelog($version, $changes);
+
+    // Commit all the changes and push to github.
+    $result = $this->taskGitStack()
+      ->add('-A')
+      ->commit("Release $version")
+      ->pull()
+      ->push()
+      ->run();
+
+    if (!$result->wasSuccessful()) {
+      throw new Exception('Release commit was unsuccessful');
+    }
+
+    $github_info = $this->getGitHubInfo();
+    // Create a new release in github. This will generate a tag which will be
+    // used in another CircleCI task.
+    $result = $this->taskGitHubRelease($version)
+      ->accessToken(getenv('GITHUB_TOKEN'))
+      ->uri($github_info['name'] . '/' . $github_info['name'])
+      ->description("Release $version")
+      ->changes($changes)
+      ->run();
+
+    if (!$result->wasSuccessful()) {
+      throw new Exception('Release was unsuccessful');
+    }
+  }
+
+  /**
+   * Git the information of the github remote.
+   *
+   * @return array
+   *   Keyed array with github owner and name.
+   */
+  protected function getGitHubInfo() {
+    $git_remote = exec('git config --get remote.origin.url');
+    $git_remote = str_replace('.git', '', $git_remote);
+    if (strpos($git_remote, 'https') !== FALSE) {
+      $parsed_url = parse_url($git_remote);
+      list($owner, $repo_name) = explode('/', trim($parsed_url['path'], '/'));
+      return ['owner' => $owner, 'name' => $repo_name];
+    }
+    list(, $repo_name) = explode(':', $git_remote);
+    str_replace('.git', '', $git_remote);
+
+    list($owner, $repo_name) = explode('/', $repo_name);
+    return ['owner' => $owner, 'name' => $repo_name];
+  }
+
+  /**
+   * Get the last version from the profile.
+   *
+   * @return string
+   *   Last semver version.
+   */
+  protected function getLastVersion() {
+    $profile_info = Yaml::parseFile(__DIR__ . '/docroot/profiles/humsci/su_humsci_profile/su_humsci_profile.info.yml');
+    return $profile_info['version'];
+  }
+
+  /**
+   * Update the changelog file with the given changes.
+   *
+   * @param string $version
+   *   New version.
+   * @param array $changes
+   *   Array of change strings.
+   */
+  protected function updateChangelog($version, array $changes = []) {
+    $divider = str_repeat('-', 80);
+    $this->taskChangelog(__DIR__ . '/docs/CHANGELOG.md')
+      ->setHeader("$version\n{$divider}\n_Release Date: " . date("Y-m-d") . "_\n\n")
+      ->anchor("# HumSci")
+      ->setBody(implode("\n", $changes))
+      ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
+      ->run();
+  }
+
+  /**
+   * Advance to the next SemVer version.
+   *
+   * The behavior depends on the parameter $stage.
+   *   - If $stage is empty, then the patch or minor version of $version is
+   *     incremented
+   *   - If $stage matches the current stage in the current version, then add
+   *     one to the stage (e.g. alpha3 -> alpha4)
+   *   - If $stage does not match the current stage in the current version, then
+   *     reset to '1' (e.g. alpha4 -> beta1)
+   *
+   * Taken from consolidation/robo library.
+   *
+   * @param string $version
+   *   A SemVer version.
+   * @param string $stage
+   *   Release stage: dev, alpha, beta, rc or an empty string for stable.
+   *
+   * @return string
+   *   New semver version.
+   */
+  protected function incrementVersion($version, $stage = '') {
+    $stable = empty($stage);
+
+    preg_match('/-([a-zA-Z]*)([0-9]*)/', $version, $match);
+    $match += ['', '', ''];
+    $versionStage = $match[1];
+    $versionStageNumber = $match[2];
+    if ($versionStage != $stage) {
+      $versionStageNumber = 0;
+    }
+    $version = preg_replace('/-.*/', '', $version);
+    $versionParts = explode('.', $version);
+    if ($stable) {
+      $versionParts[count($versionParts) - 1]++;
+    }
+    $version = implode('.', $versionParts);
+    if (!$stable) {
+      $version .= '-' . $stage;
+      if ($stage != 'dev') {
+        $versionStageNumber++;
+        $version .= $versionStageNumber;
+      }
+    }
+    return $version;
+  }
+
+  /**
+   * Set the versions in the module & profile info files.
+   *
+   * @param string $version
+   *   New version.
+   */
+  protected function setVersions($version) {
+    // Update only modules in humsci and the appropriate profiles.
+    $dirs = [
+      '*/modules/humsci/*',
+      '*/profiles/humsci/su_*',
+    ];
+
+    foreach ($dirs as $dir) {
+      $modules = Finder::create()
+        ->files()
+        ->name('*.info.yml')
+        ->in($dir);
+
+      /** @var \Symfony\Component\Finder\SplFileInfo $module */
+      foreach ($modules as $module) {
+        // Don't need to update test modules. These inherit their versions.
+        if (strpos($module->getPath(), 'tests') !== FALSE) {
+          continue;
+        }
+        $info_file = Yaml::parseFile($module->getRealPath());
+        $info_file['version'] = $version;
+        file_put_contents($module->getRealPath(), Yaml::dump($info_file));
+      }
+    }
+  }
 
   /**
    * Command to run unit tests.
@@ -379,8 +569,9 @@ class RoboFile extends Tasks {
    *   A drush exec command.
    */
   protected function blt() {
-    return $this->taskExec('vendor/acquia/blt/bin/blt')
-      ->option('verbose');
+    return $this->taskExec('vendor/bin/blt')
+      ->option('verbose')
+      ->option('no-interaction');
   }
 
   /**
@@ -395,10 +586,10 @@ class RoboFile extends Tasks {
     foreach ($blt_config['multisites'] as $site) {
       // Sandbox sites are unpredictable, so lets ignore them.
       if (strpos($site, 'sandbox') === FALSE) {
-        $sites[] = $site;
+        $sites[$site] = $site;
       }
     }
-    return $sites;
+    return array_rand($sites, self::SITES_TO_TEST);
   }
 
   /**

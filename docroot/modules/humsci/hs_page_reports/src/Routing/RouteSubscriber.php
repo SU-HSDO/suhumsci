8bed444405a77cae1a409515be34f7212e66deff
Mike Decker <pookmish@gmail.com>
2019-07-31 15:49:34 -0700

    move dblog routes if they exist

---
+++ docroot/modules/humsci/hs_page_reports/src/Routing/RouteSubscriber.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\hs_page_reports\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Removes 404 and 403 routes provided by dblog.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritDoc}}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('dblog.page_not_found')) {
+      $route->setPath('/admin/reports/dblog/page-not-found');
+    }
+    if ($route = $collection->get('dblog.access_denied')) {
+      $route->setPath('/admin/reports/dblog/access-denied');
+    }
+  }
+
+}

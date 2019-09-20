89c6843cf4324c9d3d844746eb8bb5ce80622056
Mike Decker <pookmish@gmail.com>
2019-09-20 15:46:10 -0700

    cc fix

--- docroot/modules/humsci/hs_actions/src/Plugin/Action/FieldClone/Date.php
+++ docroot/modules/humsci/hs_actions/src/Plugin/Action/FieldClone/Date.php
@@ -106,7 +106,7 @@ class Date extends FieldCloneBase {
     $increment = $increment_config['multiple'] * $increment_config['increment'];
 
     $new_value = new \DateTime($value);
-    $original_daylight_savings = date('I', $new_value->getTimestamp());
+    $daylight_savings = date('I', $new_value->getTimestamp());
 
     // Add the interval that is in the form of "2 days" or "6 hours".
     $interval = \DateInterval::createFromDateString($increment . ' ' . $increment_config['unit']);
@@ -120,9 +120,9 @@ class Date extends FieldCloneBase {
 
     // Adjust the time of the string if the new value skips over the daylight
     // savings time.
-    if (date('I', $new_value->getTimestamp()) != $original_daylight_savings) {
+    if (date('I', $new_value->getTimestamp()) != $daylight_savings) {
       // Accommodate both going into and out of daylight savings time.
-      $interval = $original_daylight_savings ? '1 hour' : '-1 hour';
+      $interval = $daylight_savings ? '1 hour' : '-1 hour';
       $interval = \DateInterval::createFromDateString($interval);
       $new_value->add($interval);
     }

0eb380881078dc44a24a0b8c3ecc51c1b91295ea
Kasey Bonifacio <kasey@heysparkbox.com>
2020-02-17 16:10:30 -0500

    feat: use Decanter masthead layout and styles
    STN-117

--- docroot/themes/humsci/humsci_basic/src/js/index.js
+++ docroot/themes/humsci/humsci_basic/src/js/index.js
@@ -1,3 +1,4 @@
 import 'decanter/core/src/js/components/secondary-nav/secondary-nav-buttons';
+import 'decanter/core/src/js/components/main-nav/main-nav';
 import './tables/wrap';
 import './tables/scope';

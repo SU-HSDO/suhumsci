676cc0e5281a2c6ef0e92a212378851911394a9f
Lindsay Silver <lindsay@heysparkbox.com>
2020-04-09 09:59:39 -0400

    feat(STN-174): fixes stylelint issues that are currently present in other themes
    - also adds new tasks to main package json
    - gets rid of things ignoring humsci_basic scss files in the main
      gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,6 @@ module.exports = function (grunt) {
 
   function getIncludeFiles() {
     var patterns = [
-      'docroot/themes/humsci/!(humsci_basic)/**/*.scss',
       'docroot/modules/humsci/**/*.scss',
       'node_modules/decanter/scss',
       'node_modules/bourbon/core',
@@ -30,7 +29,6 @@ module.exports = function (grunt) {
         files: [
           '**/*.{scss,sass}',
           '!**/node_modules/**',
-          '!docroot/themes/humsci/humsci_basic/**',
         ],
         tasks: ['sass'],
         options: {
@@ -71,7 +69,6 @@ module.exports = function (grunt) {
             cwd: 'docroot',
             src: [
               'modules/humsci/**/[a-z]*.scss',
-              'themes/humsci/!(humsci_basic)/**/[a-z]*.scss',
             ],
             dest: 'docroot',
             ext: '.css',

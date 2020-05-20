c8eb90ae9a5f652ff802fb01a63c6fa9d05b71ab
Lindsay Silver <lindsay@heysparkbox.com>
2020-05-20 16:35:50 -0400

    fix(STN-297): IE11 bug

--- docroot/themes/humsci/humsci_basic/src/js/equal-height-grid/index.js
+++ docroot/themes/humsci/humsci_basic/src/js/equal-height-grid/index.js
@@ -3,7 +3,8 @@ import equalHeightGrid from './equal-height-grid';
 // Make the vertical linked card titles the same max height
 equalHeightGrid('hb-vertical-linked-card__title', 'hb-vertical-linked-card--equal');
 
-// TODO also rather than doing the CSS I wrote, this could also work to make the card description the 
+// TODO NOTE that we can REMOVE
+// also rather than doing the CSS I wrote, this could also work to make the card description the 
 // same height:
 // equalHeightGrid('hb-vertical-linked-card', 'hb-vertical-linked-card--equal');
 

373271f2cdec493e50d9569e6b5911df1dcbb9b2
Lindsay Silver <lindsay@heysparkbox.com>
2020-05-20 15:51:03 -0400

    feat(STN-297): adds JS for equal height grid cards

---
+++ docroot/themes/humsci/humsci_basic/src/js/equal-height-grid/index.js
@@ -0,0 +1,9 @@
+import equalHeightGrid from './equal-height-grid';
+
+// Make the vertical linked card titles the same max height
+equalHeightGrid('hb-vertical-linked-card__title', 'hb-vertical-linked-card--equal');
+
+// TODO also rather than doing the CSS I wrote, this could also work to make the card description the 
+// same height:
+// equalHeightGrid('hb-vertical-linked-card', 'hb-vertical-linked-card--equal');
+
--- docroot/themes/humsci/humsci_basic/src/js/index.js
+++ docroot/themes/humsci/humsci_basic/src/js/index.js
@@ -2,3 +2,4 @@ import './tables/wrap';
 import './tables/scope';
 import './tables/table-pattern';
 import './navigation/index';
+import './equal-height-grid/index';

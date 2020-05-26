1fdfe5755776b1417374e37b46dbc9cbde3272da
Lindsay R. Silver <lindsay@heysparkbox.com>
2020-05-26 08:48:02 -0400

    feat(STN-297): equal grid cards (#609)
    * wip(STN-297): first attempt without JS
    
    * feat(STN-297): adds JS for equal height grid cards
    
    * fix(STN-297): IE11 bug
    
    * refactor: rename the utility class that stretches the vertical linked cards and equalizes the height of the title area
    
    STN-297
    
    * chore: commit compiled css
    
    Co-authored-by: Kasey Bonifacio <kasey@heysparkbox.com>

---
+++ docroot/themes/humsci/humsci_basic/src/js/equal-height-grid/index.js
@@ -0,0 +1,4 @@
+import equalHeightGrid from './equal-height-grid';
+
+// Make the vertical linked card titles the same max height
+equalHeightGrid('hb-vertical-linked-card__title', 'hb-stretch-vertical-linked-cards');

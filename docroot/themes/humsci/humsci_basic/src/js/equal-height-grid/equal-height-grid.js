eb840f26efeba07777932eeae18060119a8c2eb2
Lindsay R. Silver <lindsay@heysparkbox.com>
2020-05-26 08:48:02 -0400

    feat(STN-297): equal grid cards (#609)
    * wip(STN-297): first attempt without JS
    
    * feat(STN-297): adds JS for equal height grid cards
    
    * fix(STN-297): IE11 bug
    
    * refactor: rename the utility class that stretches the vertical linked cards and equalizes the height of the title area
    
    STN-297
    
    * chore: commit compiled css
    
    Co-authored-by: Kasey Bonifacio <kasey@heysparkbox.com>

---
+++ docroot/themes/humsci/humsci_basic/src/js/equal-height-grid/equal-height-grid.js
@@ -0,0 +1,44 @@
+// This function takes two parameters:
+// The first parameter should be whatever type of element you'd like to be the same height, and
+// ideally these types of elements are laid out in a grid that you'd like the height to be the same
+// The second parameter is the special class that will make this function execute and turn elements 
+// into the same height. If the special class isn't present, we won't run this function.
+const equalHeightGrid = (element, specialWrapperClass) => {
+  const equalGridWrapper = document.getElementsByClassName(specialWrapperClass);
+
+  // If the wrapper class for specifying making things the same height is present,
+  // keep going
+  if (equalGridWrapper.length > 0) {
+    const elementList = document.getElementsByClassName(element);
+
+    if (elementList.length > 0) {
+      // Create array with all of the heights of each element
+      const elementHeights = Array.prototype.map.call(elementList, (el) => {
+        return el.scrollHeight;
+      });
+    
+      // Create array with _unique_ height values
+      const uniqueHeights = elementHeights.filter((height, index, array) => {
+        return array.indexOf(height) == index;
+      });
+
+      // If there is only 1 unique value, then all elements are the same height,
+      // and in that case, we don't need to change the height at all
+      if (uniqueHeights.length > 1) {
+        const maxHeight = Math.max.apply(null, elementHeights);
+        const tallestElementIndex = elementHeights.indexOf(maxHeight);
+
+        Array.prototype.forEach.call(elementList, (el, index) => {
+          // Ignore the tallest element as it is already set to the right height
+          if (index != tallestElementIndex) {
+            el.style.height =`${maxHeight}px`;
+          }
+        });
+      }
+    }
+  } else {
+    return null;
+  }
+}
+
+export default equalHeightGrid;

f4f3893eee615045c5ead4c327b10408a66c41c7
Kasey Bonifacio <kasey@heysparkbox.com>
2020-06-09 09:15:36 -0400

    fix: sprint 17 feedback
    - form iframes fill the entire width of a container and have a larger height
    - video iframes fill the entire width of a container and maintian a 16:9 aspect ratio
    - list items flow along with floated content
    - bullet points and numbers no longer overlay floated images
    - wysiwyg tables and table views have margin bottom
    - Traditional secondary button has a more noticable hover state
    - When an h2-h6 is the only element in a row well all margin is removed to "vertically center" the heading in the well
    - text areas in row wells now will the width of the container even when the content doesn't
    - FA email icons are hidden by default and can be added with a utility class `hb-mailto-link`
    - vertical card patterns now display the author and year fields
    - subcontent fields bug where a subcontent field didn't show when only one field was mapped has been fixed
    
    STN-379

--- docroot/themes/humsci/humsci_basic/src/js/index.js
+++ docroot/themes/humsci/humsci_basic/src/js/index.js
@@ -3,3 +3,22 @@ import './tables/scope';
 import './tables/table-pattern';
 import './navigation/index';
 import './equal-height-grid/index';
+
+
+function resizeIFrameToFitContent( iFrame ) {
+  console.log(iFrame);
+    iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
+    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+}
+
+window.addEventListener('DOMContentLoaded', function(e) {
+
+  var iFrame = document.querySelector( '.google-form' );
+  resizeIFrameToFitContent( iFrame );
+
+  // or, to resize all iframes:
+  var iframes = document.querySelectorAll("iframe");
+  for( var i = 0; i < iframes.length; i++) {
+      resizeIFrameToFitContent( iframes[i] );
+  }
+} );

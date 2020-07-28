48c3f1399e7d4a5f1c25ed9cd0120a9b10dd82a3
Lindsay Silver <lindsay@heysparkbox.com>
2020-07-28 11:04:07 -0400

    fix(STN-434): implements both a timeout and promise behavior to try to ensure the stretch cards load correctly

--- docroot/themes/humsci/humsci_basic/src/js/equal-height-grid/index.js
+++ docroot/themes/humsci/humsci_basic/src/js/equal-height-grid/index.js
@@ -1,5 +1,9 @@
 import equalHeightGrid from './equal-height-grid';
 
+// Wait a 1 sec for page to load in before setting heights
 // Make the vertical linked card titles AND cards the same max height
-equalHeightGrid('hb-vertical-linked-card__title', 'hb-stretch-vertical-linked-cards');
-equalHeightGrid('hb-vertical-linked-card', 'hb-stretch-vertical-linked-cards');
+setTimeout(() => {
+  equalHeightGrid('hb-vertical-linked-card__title', 'hb-stretch-vertical-linked-cards')
+  .then((result) => equalHeightGrid('hb-vertical-linked-card', 'hb-stretch-vertical-linked-cards'))
+  .catch((result) => console.error('issue loading equal height cards', result));
+}, 1000);

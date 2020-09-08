be12df70cb10191b60513f8e221ad25ef9596d97
Mike Decker <pookmish@gmail.com>
2020-09-07 21:15:45 -0700

    update hook for event views

--- docroot/profiles/humsci/su_humsci_profile/su_humsci_profile.post_update.php
+++ docroot/profiles/humsci/su_humsci_profile/su_humsci_profile.post_update.php
@@ -16,315 +16,42 @@ use Drupal\user\Entity\Role;
 use GuzzleHttp\Exception\GuzzleException;
 
 /**
- * Outdated.
+ * Implements hook_removed_post_updates().
  */
-function su_humsci_profile_post_update_8_0_1() {
-
-}
-
-/**
- * Outdated.
- */
-function su_humsci_profile_post_update_8_0_2() {
-
-}
-
-/**
- * Outdated.
- */
-function su_humsci_profile_post_update_8_0_3() {
-
-}
-
-/**
- * Delete masquerade blocks.
- */
-function su_humsci_profile_post_update_8_0_4() {
-  if ($block = Block::load('seven_masquerade')) {
-    $block->delete();
-  }
-  if ($block = Block::load('su_humsci_admin_masquerade')) {
-    $block->delete();
-  }
-}
-
-/**
- * Fix layout builder block display.
- *
- * Views exposed filter blocks started showing the view title and we need to
- * hide them as configured.
- */
-function su_humsci_profile_post_update_8_1_0() {
-  $database = \Drupal::database();
-  $tables = [
-    'node__layout_builder__layout',
-    'node_revision__layout_builder__layout',
+function su_humsci_profile_removed_post_updates() {
+  return [
+    'su_humsci_profile_post_update_8200' => '8.x-2.18',
+    'su_humsci_profile_post_update_8201' => '8.x-2.18',
+    'su_humsci_profile_post_update_8202' => '8.x-2.18',
+    'su_humsci_profile_post_update_8203' => '8.x-2.18',
+    'su_humsci_profile_post_update_8204' => '8.x-2.18',
+    'su_humsci_profile_post_update_8211' => '8.x-2.18',
+    'su_humsci_profile_post_update_8212' => '8.x-2.18',
+    'su_humsci_profile_post_update_8213' => '8.x-2.18',
+    'su_humsci_profile_post_update_8214' => '8.x-2.18',
+    'su_humsci_profile_post_update_8215' => '8.x-2.18',
+    'su_humsci_profile_post_update_8_0_1' => '8.x-2.18',
+    'su_humsci_profile_post_update_8_0_2' => '8.x-2.18',
+    'su_humsci_profile_post_update_8_0_3' => '8.x-2.18',
+    'su_humsci_profile_post_update_8_0_4' => '8.x-2.18',
+    'su_humsci_profile_post_update_8_1_0' => '8.x-2.18',
+    'su_humsci_profile_post_update_8_1_1' => '8.x-2.18',
   ];
-
-  foreach ($tables as $table) {
-    $query = $database->select($table, 'l')
-      ->fields('l')
-      ->execute();
-    while ($row = $query->fetchAssoc()) {
-      $changed_row = FALSE;
-      /** @var \Drupal\layout_builder\Section $layout_section */
-      $layout_section = unserialize($row['layout_builder__layout_section']);
-      foreach ($layout_section->getComponents() as $component) {
-        $config = $component->get('configuration');
-        if (
-          isset($config['provider']) &&
-          $config['provider'] == 'views' &&
-          $config['label'] == '' &&
-          $config['views_label'] == ''
-        ) {
-          $config['label_display'] = 0;
-          $component->setConfiguration($config);
-          $changed_row = TRUE;
-        }
-      }
-
-      if ($changed_row) {
-        $database->update($table)
-          ->fields(['layout_builder__layout_section' => serialize($layout_section)])
-          ->condition('entity_id', $row['entity_id'])
-          ->condition('revision_id', $row['revision_id'])
-          ->condition('delta', $row['delta'])
-          ->execute();
-      }
-    }
-  }
-}
-
-/**
- * Adds carousel to the hero field on basic pages.
- */
-function su_humsci_profile_post_update_8_1_1() {
-  /** @var \Drupal\field\FieldConfigInterface $field */
-  $field = FieldConfig::load('node.hs_basic_page.field_hs_page_hero');
-  $settings = $field->getSettings();
-  $settings['handler_settings']['target_bundles']['hs_carousel'] = 'hs_carousel';
-  $settings['handler_settings']['target_bundles_drag_drop']['hs_carousel'] = [
-    'enabled' => TRUE,
-    'weight' => 9,
-  ];
-  $field->set('settings', $settings);
-  $field->save();
-}
-
-/**
- * Uninstall unwanted modules.
- */
-function su_humsci_profile_post_update_8200() {
-  /** @var \Drupal\filter\FilterFormatInterface $filter_format */
-  foreach (FilterFormat::loadMultiple() as $filter_format) {
-    $filters = $filter_format->get('filters');
-    unset($filters['entity_embed']);
-    $filter_format->set('filters', $filters);
-    $filter_format->calculateDependencies();
-    $filter_format->save();
-  }
-
-  /** @var \Drupal\Core\Extension\ModuleInstaller $module_installer */
-  $module_installer = \Drupal::service('module_installer');
-  $module_installer->uninstall(['embed', 'entity_browser']);
-}
-
-/**
- * Add setting to content access to restrict access to private page.
- */
-function su_humsci_profile_post_update_8201() {
-  $config = \Drupal::configFactory()->getEditable('content_access.settings');
-  $node_access = $config->get('content_access_node_type') ?: [];
-
-  $settings = ['view' => ['authenticated'], 'per_node' => 1];
-  $node_access['hs_private_page'] = serialize($settings);
-
-  $config->set('content_access_node_type', $node_access);
-
-  $new_perms = [
-    'create hs_private_page content',
-    'delete any hs_private_page content',
-    'delete own hs_private_page content',
-    'edit any hs_private_page content',
-    'edit own hs_private_page content',
-    'revert hs_private_page revisions',
-    'view hs_private_page revisions',
-  ];
-  user_role_grant_permissions('site_manager', $new_perms);
-  user_role_grant_permissions(AccountInterface::AUTHENTICATED_ROLE, ['use text format basic_html_without_media']);
-
-  /** @var \Drupal\field\FieldConfigInterface $field */
-  $field = \Drupal::entityTypeManager()
-    ->getStorage('field_config')
-    ->load('node.hs_basic_page.field_hs_page_components');
-  $settings = $field->getSettings();
-
-  if ($settings['handler_settings']['negate']) {
-    $settings['handler_settings']['target_bundles']['hs_private_files'] = 'hs_private_files';
-    $settings['handler_settings']['target_bundles']['hs_priv_text_area'] = 'hs_priv_text_area';
-    $settings['handler_settings']['target_bundles_drag_drop']['hs_private_files'] = ['enabled' => FALSE];
-    $settings['handler_settings']['target_bundles_drag_drop']['hs_priv_text_area'] = ['enabled' => FALSE];;
-    $field->set('settings', $settings);
-    $field->calculateDependencies();
-    $field->save();
-  }
-}
-
-/**
- * Create the private page entity form and entity display configs.
- */
-function su_humsci_profile_post_update_8202() {
-  $configs = [
-    'core.entity_form_display.node.hs_private_page.default',
-    'core.entity_view_display.node.hs_private_page.default',
-    'core.entity_view_display.node.hs_private_page.teaser',
-  ];
-  $config_factory = \Drupal::configFactory();
-  foreach ($configs as $config) {
-    $data = Yaml::decode(file_get_contents(DRUPAL_ROOT . "/../config/default/$config.yml"));
-    $config_factory->getEditable($config)
-      ->setData($data)
-      ->save();
-  }
-}
-
-/**
- * Copy the media module icon to the site's files directory.
- */
-function su_humsci_profile_post_update_8203() {
-  /** @var \Drupal\Core\File\FileSystem $file_system */
-  $file_system = \Drupal::service('file_system');
-  $source = DRUPAL_ROOT . '/' . drupal_get_path('module', 'media') . '/images/icons/generic.png';
-  $destination = $file_system->realpath('public://media-icons/generic/generic.png');
-  if (!file_exists($destination)) {
-    $directory = 'public://media-icons/generic';
-    $file_system->prepareDirectory($directory, FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS);
-    $file_system->copy($source, "$directory/generic.png", FileSystemInterface::EXISTS_REPLACE);
-  }
-}
-
-/**
- * Uninstall update module.
- */
-function su_humsci_profile_post_update_8204() {
-  \Drupal::service('module_installer')->uninstall(['update']);
-}
-
-/**
- * Modify the events importer database to use the new unique IDS.
- */
-function su_humsci_profile_post_update_8211(&$sandbox) {
-  $schema = \Drupal::database()->schema();
-
-  \Drupal::configFactory()
-    ->getEditable('migrate_plus.migration.hs_events_image_importer')
-    ->delete();
-  if ($schema->tableExists('migrate_map_hs_events_image_importer')) {
-    $schema->dropTable('migrate_map_hs_events_image_importer');
-  }
-
-  if (!$schema->tableExists('migrate_map_hs_events_importer') ||
-    ($schema->fieldExists('migrate_map_hs_events_importer', 'sourceid2') && $schema->fieldExists('migrate_map_hs_events_importer', 'sourceid3'))
-  ) {
-    return;
-  }
-  $field_info = [
-    'type' => 'varchar',
-    'length' => '255',
-    'not null' => TRUE,
-    'initial' => '[replace]',
-  ];
-
-  if (!$schema->fieldExists('migrate_map_hs_events_importer', 'sourceid2')) {
-    $schema->addField('migrate_map_hs_events_importer', 'sourceid2', $field_info);
-  }
-  if (!$schema->fieldExists('migrate_map_hs_events_importer', 'sourceid3')) {
-    $schema->addField('migrate_map_hs_events_importer', 'sourceid3', $field_info);
-  }
-
-  $event_urls = \Drupal::config('hs_events_importer.settings')->get('urls');
-  if (empty($event_urls)) {
-    return;
-  }
-
-  $database = \Drupal::database();
-  $guzzle = \Drupal::httpClient();
-
-  foreach ($event_urls as $url) {
-    try {
-      $response = $guzzle->request('GET', $url);
-    }
-    catch (GuzzleException | \Exception $e) {
-      continue;
-    }
-
-    $body = (string) $response->getBody();
-    $xml = simplexml_load_string($body);
-
-    /** @var \SimpleXMLElement $event */
-    foreach ($xml->xpath('//Event') as $event) {
-      $guid = (string) $event->xpath('guid')[0];
-      $event_id = (string) $event->xpath('eventID')[0];
-      $start = (string) $event->xpath('isoEventDate')[0];
-      $end = (string) $event->xpath('isoEventEndDate')[0];
-      $source_id_values = [$event_id, $start, $end];
-      $hash = hash('sha256', serialize(array_map('strval', $source_id_values)));;
-
-      $database->update('migrate_map_hs_events_importer')
-        ->condition('sourceid1', $guid)
-        ->fields([
-          'source_ids_hash' => $hash,
-          'sourceid1' => $event_id,
-          'sourceid2' => $start,
-          'sourceid3' => $end,
-        ])
-        ->execute();
-    }
-  }
-}
-
-/**
- * Implements hook_post_update_NAME().
- */
-function su_humsci_profile_post_update_8212(&$sandbox) {
-  \Drupal::service('module_installer')->uninstall(['jira_rest']);
-}
-
-/**
- * Adjust media permissions.
- */
-function su_humsci_profile_post_update_8213() {
-  foreach (Role::loadMultiple() as $role) {
-    user_role_revoke_permissions($role->id(), ['administer media']);
-  }
-  user_role_grant_permissions('site_manager', ['access media overview']);
-  user_role_grant_permissions('contributor', ['access media overview']);
-}
-
-/**
- * Exclude private files and text area from basic page.
- */
-function su_humsci_profile_post_update_8214() {
-  $field_config = \Drupal::configFactory()
-    ->getEditable('field.field.node.hs_basic_page.field_hs_page_components');
-  $settings = 'settings.handler_settings.target_bundles_drag_drop';
-  $negate = (bool) $field_config->get('settings.handler_settings.negate');
-  $field_config->set("$settings.hs_private_files.enabled", $negate);
-  $field_config->set("$settings.hs_priv_text_area.enabled", $negate);
-  $field_config->save();
-}
-
-/**
- * Uninstall rules module.
- */
-function su_humsci_profile_post_update_8215(&$sandbox) {
-  \Drupal::service('module_installer')->uninstall(['rules']);
 }
 
 /**
  * Convert event date field to smart date field.
  */
 function su_humsci_profile_post_update_8216() {
+
+  $view_config_names = \Drupal::configFactory()->listAll('views.view');
+  $views = [];
+  foreach ($view_config_names as $config_name) {
+    $views[$config_name] = \Drupal::configFactory()
+      ->getEditable($config_name)
+      ->getRawData();
+  };
+
   \Drupal::service('module_installer')->install(['smart_date']);
   drupal_flush_all_caches();
   $db = \Drupal::database();
@@ -372,4 +99,66 @@ function su_humsci_profile_post_update_8216() {
       $db->insert($table)->fields($row)->execute();
     }
   }
+
+  array_walk($views, '_suhumsci_profile_post_update_fix_view');
+}
+
+/**
+ * Modify the field config data to work with the new smart date field.
+ *
+ * @param array $old_view
+ *   Raw data from the view pre-update.
+ */
+function _suhumsci_profile_post_update_fix_view(array $old_view) {
+  $fixed_data = $old_view;
+  $view_changed = FALSE;
+  foreach ($fixed_data['display'] as &$display) {
+    if (empty($display['display_options']['fields'])) {
+      continue;
+    }
+
+    foreach ($display['display_options']['fields'] as &$field) {
+      if ($field['field'] != 'field_hs_event_date') {
+        continue;
+      }
+      $view_changed = TRUE;
+
+      switch ($field['type']) {
+        case 'datetime_hs':
+          $format = $field['settings']['date_format'];
+          $column = $field['settings']['display'];
+          $field['field'] = 'field_hs_event_date_' . ($column == 'start_date' ? 'value' : 'end_value');
+          $field['date_format'] = 'custom';
+          $field['custom_date_format'] = $format;
+          $field['plugin_id'] = 'date';
+          unset($field['settings']);
+          break;
+
+        case 'daterange_custom':
+          $field['type'] = 'smartdate_custom';
+          $field['settings'] = [
+            'date_format' => $field['settings']['date_format'],
+            'separator' => $field['settings']['separator'],
+          ];
+          break;
+
+        case 'daterange_default':
+          $field['settings']['format_type'] = 'default';
+          break;
+      }
+    }
+  }
+
+  if (!$view_changed) {
+    return;
+  }
+  \Drupal::logger('su_humsci_profile')
+    ->info(t('Updated view: @view_name'), ['@view_name' => $old_view['label']]);
+  \Drupal::messenger()
+    ->addStatus(t('Updated view: @view_name', ['@view_name' => $old_view['label']]));
+
+  \Drupal::configFactory()
+    ->getEditable('views.view.' . $old_view['id'])
+    ->setData($fixed_data)
+    ->save();
 }

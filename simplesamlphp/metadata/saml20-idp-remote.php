37bc195959a3bb1fd0f8909a747ef0b0d0fbec24
Mike Decker <pookmish@gmail.com>
2020-11-25 11:07:19 -0800

    initial drupal 9

--- simplesamlphp/metadata/saml20-idp-remote.php
+++ simplesamlphp/metadata/saml20-idp-remote.php
@@ -1,14 +1,9 @@
 <?php
+
 /**
  * SAML 2.0 remote IdP metadata for SimpleSAMLphp.
  *
  * Remember to remove the IdPs you don't use from this file.
  *
- * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-idp-remote 
+ * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-idp-remote
  */
-
-
-// Load file on acquia server.
-if (file_exists('/mnt/gfs/swshumsci.prod/nobackup/apikeys/saml/saml20-idp-remote.php')) {
-  include_once '/mnt/gfs/swshumsci.prod/nobackup/apikeys/saml/saml20-idp-remote.php';
-}

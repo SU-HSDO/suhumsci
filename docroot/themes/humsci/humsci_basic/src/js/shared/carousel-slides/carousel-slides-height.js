99cedd49c5af4b21d3047e4342fa55e9fd748530
Heather Taylor <heather@heysparkbox.com>
2021-03-02 21:24:38 -0500

    feat: refactor 2 loops into 1 loop to improve performance

--- docroot/themes/humsci/humsci_basic/src/js/shared/carousel-slides/carousel-slides-height.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/carousel-slides/carousel-slides-height.js
@@ -12,14 +12,12 @@ function restrictHeight() {
     // Find all the textBoxes inside each carousel
     textBoxes = slides[i].getElementsByClassName('hb-hero-overlay__text');
 
-    // Clear any inline styles that may have been set previously
-    // This is necessary to determine the default height of text boxes
-    for (let n = 0; n < textBoxes.length; n++) {
-      textBoxes[n].removeAttribute('style');
-    }
-
     // Loop through all the textBoxes and gather their heights into an array
     for (let j = 0; j < textBoxes.length; j++) {
+      // Clear any inline styles that may have been set previously
+      // This is necessary to determine the default height of text boxes
+      textBoxes[j].removeAttribute('style');
+
       let boxHeight = textBoxes[j].offsetHeight;
 
       // Parse boxHeight to be a number that can be used to set the min-height value
@@ -33,8 +31,10 @@ function restrictHeight() {
     let maxBoxHeight = Math.max(...boxHeightArray);
 
     // Give all textBoxes the same height on medium and larger sized screens
-    for (let k = 0; k < textBoxes.length; k++) {
-      textBoxes[k].setAttribute('style', `min-height: ${maxBoxHeight}px`);
+    if (window.innerWidth > mediumScreenBreakpoint) {
+      for (let k = 0; k < textBoxes.length; k++) { // TODO: refactor this into the loop above
+        textBoxes[k].setAttribute('style', `min-height: ${maxBoxHeight}px`);
+      }
     }
   }
 }

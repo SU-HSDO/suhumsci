f84e532712043478de225da3bc4bbef0ff5963a7
Kasey Bonifacio <kasey@heysparkbox.com>
2021-03-08 18:38:06 -0500

    refactor: equal-height-grid.js
    - equalHeightGrid() only accepts a list of elements to run against
    - Logic for when equalHeightGrid() runs is separated into the index.js file
    - Bug fix: equalHeightGrid() will run without errors if a vertical linked card does not have a title
    - Set a max-height on cards instead of height to prevent inaccessible text on page resize.
    
    STN-693

--- docroot/themes/humsci/humsci_basic/src/js/shared/equal-height-grid/index.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/equal-height-grid/index.js
@@ -1,9 +1,23 @@
 import equalHeightGrid from './equal-height-grid';
 
+const hasStretchClass = document.querySelector('.hb-stretch-vertical-linked-cards');
+const verticalLinkedCardTitles = document.querySelectorAll('.hb-vertical-linked-card__title');
+const verticalLinkedCards = document.querySelectorAll('.hb-vertical-linked-card');
+
 // Wait a 1 sec for page to load in before setting heights
-// Make the vertical linked card titles AND cards the same max height
 setTimeout(() => {
-  equalHeightGrid('hb-vertical-linked-card__title', 'hb-stretch-vertical-linked-cards')
-  .then(() => equalHeightGrid('hb-vertical-linked-card', 'hb-stretch-vertical-linked-cards'))
-  .catch((result) => console.error('issue loading equal height cards', result));
+  if (hasStretchClass) {
+    if (verticalLinkedCardTitles.length > 0) {
+      // Make the vertical linked card titles AND cards the same max height.
+      // The title height has to be set first because it influences the final
+      // height of the card.
+      equalHeightGrid(verticalLinkedCardTitles)
+      .then(() => equalHeightGrid(verticalLinkedCards))
+      .catch((result) => console.error('issue loading equal height cards', result));
+    } else if (verticalLinkedCards.length > 0) {
+      // Since card titles are not required we still want to run the equal height
+      // function on remaining cards
+      equalHeightGrid(verticalLinkedCards.length > 0);
+    }
+  }
 }, 1000);

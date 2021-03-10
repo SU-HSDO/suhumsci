4aeddd0d716dedd78f5c2c5754b2a4f8a6c2cfa9
pookmish <pookmish@gmail.com>
2021-03-10 09:32:18 -0800

    HSD8-1008 add campus image gallery (#829)
    * HSD8-1008 add campus image gallery
    
    * Exclude gallery for now
    
    * changed image style
    
    * CC fix
    
    * Added very minimal styles

---
+++ docroot/modules/humsci/hs_paragraph_types/src/Plugin/paragraphs/Behavior/GalleryBehaviors.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\hs_paragraph_types\Plugin\paragraphs\Behavior;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\Entity\ParagraphsType;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\paragraphs\ParagraphsBehaviorBase;
+
+/**
+ * Class GalleryBehaviors.
+ *
+ * @ParagraphsBehavior(
+ *   id = "image_gallery",
+ *   label = @Translation("Image Gallery"),
+ *   description = @Translation("Setting for the image gallery")
+ * )
+ */
+class GalleryBehaviors extends ParagraphsBehaviorBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function isApplicable(ParagraphsType $paragraphs_type) {
+    return $paragraphs_type->id() == 'stanford_gallery';
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state) {
+    $form = parent::buildBehaviorForm($paragraph, $form, $form_state);
+    $form['display_mode'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Display Mode'),
+      '#options' => [
+        'default' => $this->t('Modal Grid'),
+        'hs_gallery_slideshow' => $this->t('Slideshow'),
+      ],
+      '#default_value' => $paragraph->getBehaviorSetting('image_gallery', 'display_mode', 'default'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode) {
+    $build['#attached']['library'][] = 'hs_paragraph_types/stanford_gallery';
+    $build['#attributes']['class'][] = Html::cleanCssIdentifier($paragraph->bundle());
+    $build['#attributes']['class'][] = Html::cleanCssIdentifier($view_mode);
+  }
+
+}

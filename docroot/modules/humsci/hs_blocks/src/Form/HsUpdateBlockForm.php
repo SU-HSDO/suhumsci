0c9bc81e0836b7831340f65fa0bf01636aeadf19
Mike Decker <pookmish@gmail.com>
2021-03-18 15:26:57 -0700

    HSD8-1014 Add class attributes to group blocks

--- docroot/modules/humsci/hs_blocks/src/Form/HsUpdateBlockForm.php
+++ docroot/modules/humsci/hs_blocks/src/Form/HsUpdateBlockForm.php
@@ -3,7 +3,6 @@
 namespace Drupal\hs_blocks\Form;
 
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Plugin\PluginBase;
 use Drupal\layout_builder\SectionComponent;
 use Drupal\layout_builder\SectionStorageInterface;
 
@@ -32,38 +31,14 @@ class HsUpdateBlockForm extends HsConfigureBlockFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state, SectionStorageInterface $section_storage = NULL, $delta = NULL, $group = NULL, $uuid = NULL) {
-    $group_component = $this->getGroupComponent($section_storage, $delta, $group);
+    $section = $section_storage->getSection($delta);
+    $group_component = $section->getComponent($group);
     $group_config = $group_component->get('configuration');
 
     $component = new SectionComponent($uuid, $group, $group_config['children'][$uuid]);
     $form_state->set('layout_builder__component', $component);
 
     return $this->doBuildForm($form, $form_state, $section_storage, $delta, $component);
-  }
-
-  /**
-   * Get the component for the given group within the section.
-   *
-   * @param \Drupal\layout_builder\SectionStorageInterface $section_storage
-   *   Entity section storage object.
-   * @param int $delta
-   *   Section delta in the storage.
-   * @param string $group_name
-   *   Group machine name.
-   *
-   * @return \Drupal\layout_builder\SectionComponent
-   *   Section component object for the group.
-   */
-  protected function getGroupComponent(SectionStorageInterface $section_storage, $delta, $group_name) {
-    $section = $section_storage->getSection($delta);
-    foreach ($section->getComponents() as $component) {
-      $component_config = $component->get('configuration');
-      list($component_id) = explode(PluginBase::DERIVATIVE_SEPARATOR, $component_config['id']);
-
-      if ($component_id == 'group_block' && $component_config['machine_name'] == $group_name) {
-        return $component;
-      }
-    }
   }
 
 }

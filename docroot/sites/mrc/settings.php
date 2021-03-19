14bba6e96e7c1309761d29878cc17f189f09d10f
Mike Decker <pookmish@gmail.com>
2021-03-18 17:01:30 -0700

    fixed mrc settings

--- docroot/sites/mrc/settings.php
+++ docroot/sites/mrc/settings.php
@@ -780,7 +780,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 require DRUPAL_ROOT . "/../vendor/acquia/blt/settings/blt.settings.php";
-$settings['config_sync_directory'] =  = '../config/mrc';
+$settings['config_sync_directory'] = '../config/mrc';
 
 /**
  * IMPORTANT.

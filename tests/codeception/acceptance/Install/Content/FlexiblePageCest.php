395ad05f9f8bcfab8ffc5ec5eece96a91116fa0c
Heather Taylor <heather@heysparkbox.com>
2021-04-02 10:12:27 -0400

    feat: update codeception test for the gradient hero paragraph type

--- tests/codeception/acceptance/Install/Content/FlexiblePageCest.php
+++ tests/codeception/acceptance/Install/Content/FlexiblePageCest.php
@@ -14,7 +14,7 @@ class FlexiblePageCest {
     $I->logInWithRole('contributor');
     $I->amOnPage('/node/add/hs_basic_page');
     $I->canSeeNumberOfElements('#edit-field-hs-page-hero-wrapper', 1);
-    $I->canSeeNumberOfElements('#edit-field-hs-page-components-add-more input', 11);
+    $I->canSeeNumberOfElements('#edit-field-hs-page-components-add-more input', 12;
     $I->fillField('Title', 'Demo Basic Page');
     $I->click('Add Postcard');
     $I->canSee('Card Title');

bf2f7b0ff8a0a1216270041f4bb4aaf401672a28
Mike Decker <pookmish@gmail.com>
2021-04-16 14:25:56 -0700

    Update hook to disable paragraph from the rows

--- docroot/profiles/humsci/su_humsci_profile/su_humsci_profile.post_update.php
+++ docroot/profiles/humsci/su_humsci_profile/su_humsci_profile.post_update.php
@@ -5,6 +5,8 @@
  * su_humsci_profile.post_update.php
  */
 
+use Drupal\field\FieldConfigInterface;
+
 /**
  * Implements hook_removed_post_updates().
  */
@@ -18,9 +20,19 @@ function su_humsci_profile_removed_post_updates() {
 }
 
 /**
- * Disable the new timeline paragraph type on older themes.
+ * Disable a new paragraph type from being available in the UI.
+ *
+ * @param string $paragraph_type
+ *   New paragraph type name.
+ * @param bool $all_themes
+ *   If the paragraph should be disabled on every theme, false if only legacy.
+ * @param false $only_rows
+ *   True if disable it on row components but enabled on regular fields.
+ *
+ * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+ * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
  */
-function su_humsci_profile_post_update_9001() {
+function _humsci_profile_disable_paragraph_type($paragraph_type, $all_themes = TRUE, $only_rows = FALSE) {
   $theme = \Drupal::config('system.theme')->get('default');
   $newer_themes = [
     'humsci_airy',
@@ -33,37 +45,37 @@ function su_humsci_profile_post_update_9001() {
   $field = \Drupal::entityTypeManager()
     ->getStorage('field_config')
     ->load('node.hs_basic_page.field_hs_page_components');
-  $settings = $field->getSettings();
-  $settings['handler_settings']['target_bundles']['hs_timeline_item'] = 'hs_timeline_item';
-  $settings['handler_settings']['target_bundles_drag_drop']['hs_timeline_item'] = [
-    'enabled' => TRUE,
-    'weight' => 99,
-  ];
 
-  if (!in_array($theme, $newer_themes)) {
-    $settings['handler_settings']['target_bundles']['hs_timeline'] = 'hs_timeline';
-    $settings['handler_settings']['target_bundles_drag_drop']['hs_timeline'] = $settings['handler_settings']['target_bundles_drag_drop']['hs_timeline_item'];
+  if (!$only_rows && ($all_themes || !in_array($theme, $newer_themes))) {
+    _humsci_profile_disable_paragraph_on_field($field, $paragraph_type);
   }
-  $field->set('settings', $settings);
-  $field->save();
 
   /** @var \Drupal\field\FieldConfigInterface $field */
   $field = \Drupal::entityTypeManager()
     ->getStorage('field_config')
     ->load('paragraph.hs_row.field_hs_row_components');
-  $settings = $field->getSettings();
-  $settings['handler_settings']['target_bundles']['hs_timeline_item'] = 'hs_timeline_item';
-  $settings['handler_settings']['target_bundles_drag_drop']['hs_timeline_item'] = [
+  if ($all_themes || !in_array($theme, $newer_themes)) {
+    _humsci_profile_disable_paragraph_on_field($field, $paragraph_type);
+  }
+}
+
+function _humsci_profile_disable_paragraph_on_field(FieldConfigInterface $field_config, $paragraph_type){
+  $settings = $field_config->getSettings();
+  $settings['handler_settings']['target_bundles'][$paragraph_type] = $paragraph_type;
+  $settings['handler_settings']['target_bundles_drag_drop'][$paragraph_type] = [
     'enabled' => TRUE,
     'weight' => 99,
   ];
+  $field_config->set('settings', $settings);
+  $field_config->save();
+}
 
-  if (!in_array($theme, $newer_themes)) {
-    $settings['handler_settings']['target_bundles']['hs_timeline'] = 'hs_timeline';
-    $settings['handler_settings']['target_bundles_drag_drop']['hs_timeline'] = $settings['handler_settings']['target_bundles_drag_drop']['hs_timeline_item'];
-  }
-  $field->set('settings', $settings);
-  $field->save();
+/**
+ * Disable the new timeline paragraph type on older themes.
+ */
+function su_humsci_profile_post_update_9001() {
+  _humsci_profile_disable_paragraph_type('hs_timeline_item', FALSE);
+  _humsci_profile_disable_paragraph_type('hs_timeline', FALSE);
 }
 
 /**
@@ -77,4 +89,11 @@ function su_humsci_profile_post_update_9002() {
       $config->set('source.plugin', 'url')->save();
     }
   }
+}
+
+/**
+ * Disable the new collection paragraph type on rows.
+ */
+function su_humsci_profile_post_update_9004() {
+  _humsci_profile_disable_paragraph_type('hs_collection', TRUE, TRUE);
 }

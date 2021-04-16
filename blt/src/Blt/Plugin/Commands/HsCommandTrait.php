2e217ab4766157be47a46ac50769441de9d31aed
Mike Decker <pookmish@gmail.com>
2021-04-16 15:25:19 -0700

    HSD8-1038 fixed missing "askQuestion" method

---
+++ blt/src/Blt/Plugin/Commands/HsCommandTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Humsci\Blt\Plugin\Commands;
+
+/**
+ * Trait HsCommandTrait.
+ *
+ * @package Humsci\Blt\Plugin\Commands
+ */
+trait HsCommandTrait {
+
+  /**
+   * Ask a question to the user.
+   *
+   * @param string $question
+   *   The question to ask.
+   * @param string $default
+   *   Default value.
+   * @param bool $required
+   *   If a response is required.
+   *
+   * @return string
+   *   Response to the question.
+   */
+  protected function askQuestion($question, $default = '', $required = FALSE) {
+    if ($default) {
+      $response = $this->askDefault($question, $default);
+    }
+    else {
+      $response = $this->ask($question);
+    }
+    if ($required && !$response) {
+      return $this->askQuestion($question, $default, $required);
+    }
+    return $response;
+  }
+
+}

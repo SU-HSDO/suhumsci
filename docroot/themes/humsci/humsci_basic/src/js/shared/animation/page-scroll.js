d415e2881f8f469636ec27741b62770b8b335c3d
Heather Taylor <heather@heysparkbox.com>
2021-04-20 15:31:31 -0400

    STN-675: Add Gradient Hero Styles 🎨 (#846)
    * feat (STN-675): add styles to the gradient hero paragraph type
    
    * feat: add compiled CSS & JS

--- docroot/themes/humsci/humsci_basic/src/js/shared/animation/page-scroll.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/animation/page-scroll.js
@@ -4,7 +4,18 @@ let scroll = window.requestAnimationFrame || function(callback) {
 };
 
 // The classes of items we want to add animations to
-let showAnimation = document.querySelectorAll('.hb-hero-overlay, .hb-hero-overlay__text, .hb-hero-overlay__image-wrapper, .field-hs-hero-image');
+const classesToAnimate = [
+  '.hb-hero-overlay',
+  '.hb-hero-overlay__text',
+  '.hb-hero-overlay__image-wrapper',
+  '.field-hs-hero-image',
+  '.hb-gradient-hero',
+  '.hb-gradient-hero__text',
+  '.hb-gradient-hero__image-wrapper',
+  '.field-hs-gradient-hero-image'
+];
+
+let showAnimation = document.querySelectorAll(classesToAnimate);
 
 // Check to see if the animation enhancement theme toggle has been
 // activiated. If so, then add the `animate` class when an item

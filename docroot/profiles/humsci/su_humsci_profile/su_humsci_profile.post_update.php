47427c8aba6f80adea5660140894019f6b9d0f20
Mike Decker <pookmish@gmail.com>
2021-04-27 17:00:58 -0700

    Fixed update hook and added some news layout styles

--- docroot/profiles/humsci/su_humsci_profile/su_humsci_profile.post_update.php
+++ docroot/profiles/humsci/su_humsci_profile/su_humsci_profile.post_update.php
@@ -19,20 +19,7 @@ function su_humsci_profile_removed_post_updates() {
   ];
 }
 
-/**
- * Disable a new paragraph type from being available in the UI.
- *
- * @param string $paragraph_type
- *   New paragraph type name.
- * @param bool $all_themes
- *   If the paragraph should be disabled on every theme, false if only legacy.
- * @param bool $only_rows
- *   True if disable it on row components but enabled on regular fields.
- *
- * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
- * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
- */
-function _humsci_profile_disable_paragraph_type($paragraph_type, $all_themes = TRUE, $only_rows = FALSE) {
+function _humsci_profile_disable_page_paragraph($paragraph_type, $all_themes = TRUE){
   $theme = \Drupal::config('system.theme')->get('default');
   $newer_themes = [
     'humsci_airy',
@@ -46,14 +33,25 @@ function _humsci_profile_disable_paragraph_type($paragraph_type, $all_themes = T
     ->getStorage('field_config')
     ->load('node.hs_basic_page.field_hs_page_components');
 
-  if (!$only_rows && ($all_themes || !in_array($theme, $newer_themes))) {
+  if ($all_themes || !in_array($theme, $newer_themes)) {
     _humsci_profile_disable_paragraph_on_field($field, $paragraph_type);
   }
+}
+
+function _humsci_profile_disable_row_paragraph($paragraph_type, $all_themes = TRUE){
+  $theme = \Drupal::config('system.theme')->get('default');
+  $newer_themes = [
+    'humsci_airy',
+    'humsci_basic',
+    'humsci_colorful',
+    'humsci_traditional',
+  ];
 
   /** @var \Drupal\field\FieldConfigInterface $field */
   $field = \Drupal::entityTypeManager()
     ->getStorage('field_config')
     ->load('paragraph.hs_row.field_hs_row_components');
+
   if ($all_themes || !in_array($theme, $newer_themes)) {
     _humsci_profile_disable_paragraph_on_field($field, $paragraph_type);
   }
@@ -84,8 +82,10 @@ function _humsci_profile_disable_paragraph_on_field(FieldConfigInterface $field_
  * Disable the new timeline paragraph type on older themes.
  */
 function su_humsci_profile_post_update_9001() {
-  _humsci_profile_disable_paragraph_type('hs_timeline_item', FALSE);
-  _humsci_profile_disable_paragraph_type('hs_timeline', FALSE);
+  _humsci_profile_disable_page_paragraph('hs_timeline_item');
+  _humsci_profile_disable_row_paragraph('hs_timeline_item');
+  _humsci_profile_disable_page_paragraph('hs_timeline', FALSE);
+  _humsci_profile_disable_row_paragraph('hs_timeline');
 }
 
 /**
@@ -105,22 +105,24 @@ function su_humsci_profile_post_update_9002() {
  * Disable the new collection paragraph type on rows.
  */
 function su_humsci_profile_post_update_9004() {
-  _humsci_profile_disable_paragraph_type('hs_collection');
+  _humsci_profile_disable_page_paragraph('hs_collection');
+  _humsci_profile_disable_row_paragraph('hs_collection');
 }
 
 /**
  * Disable the callout box paragraph type on older themes and rows.
  */
 function su_humsci_profile_post_update_9005() {
-  _humsci_profile_disable_paragraph_type('hs_callout_box', FALSE);
-  _humsci_profile_disable_paragraph_type('hs_callout_box', TRUE, TRUE);
+  _humsci_profile_disable_page_paragraph('hs_callout_box', FALSE);
+  _humsci_profile_disable_row_paragraph('hs_callout_box');
 }
 
 /**
  * Disable the gradient hero paragraph type on all themes and rows.
  */
 function su_humsci_profile_post_update_9006() {
-  _humsci_profile_disable_paragraph_type('hs_gradient_hero', TRUE, TRUE);
+  _humsci_profile_disable_page_paragraph('hs_gradient_hero');
+  _humsci_profile_disable_row_paragraph('hs_gradient_hero');
 }
 
 /**

7e8bb5277c26d3a02f880fa746b3f1bd0a986332
Mike Decker <pookmish@gmail.com>
2021-05-05 14:25:45 -0700

    Take advantage of stanford_fields module

--- docroot/modules/humsci/hs_field_helpers/src/Plugin/Field/FieldFormatter/EntityTitleHeading.php
+++ docroot/modules/humsci/hs_field_helpers/src/Plugin/Field/FieldFormatter/EntityTitleHeading.php
@@ -2,15 +2,9 @@
 
 namespace Drupal\hs_field_helpers\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\Field\FormatterBase;
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Link;
-use Drupal\Core\Path\PathMatcherInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Template\Attribute;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\stanford_fields\Plugin\Field\FieldFormatter\EntityTitleHeading as TitleHeading;
 
 /**
  * Provide a string field to be used as a heading.
@@ -23,98 +17,24 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   }
  * )
  */
-class EntityTitleHeading extends FormatterBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * Path matcher service.
-   *
-   * @var \Drupal\Core\Path\PathMatcherInterface
-   */
-  protected $pathMatcher;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $plugin_id,
-      $plugin_definition,
-      $configuration['field_definition'],
-      $configuration['settings'],
-      $configuration['label'],
-      $configuration['view_mode'],
-      $configuration['third_party_settings'],
-      $container->get('path.matcher')
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, PathMatcherInterface $path_matcher) {
-    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
-    $this->pathMatcher = $path_matcher;
-  }
+class EntityTitleHeading extends TitleHeading {
 
   /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    // Home page we don't want to display an H1 tag (page title).
-    if ($this->pathMatcher->isFrontPage() && $this->getSetting('tag') == 'h1') {
-      return [];
-    }
-
     $attributes = new Attribute();
     $classes = $this->getSetting('classes');
     if (!empty($classes)) {
       $attributes->addClass($classes);
     }
 
-    $parent = $items->getParent()->getValue();
-
-    $output = [];
-    foreach ($items->getValue() as $delta => $item) {
-      $text = $item['value'];
-      if ($this->getSetting('linked')) {
-        $text = Link::fromTextAndUrl($text, $parent->toUrl())->toString();
-      }
-      $output[$delta] = [
-        '#type' => 'html_tag',
-        '#tag' => $this->getSetting('tag'),
-        '#attributes' => $attributes->toArray(),
-        '#value' => $text,
-      ];
+    $elements = parent::viewElements($items, $langcode);
+    foreach ($elements as &$element) {
+      $element['#atributes'] = $attributes->toArray();
     }
-    return $output;
-  }
+    return $elements;
 
-  /**
-   * {@inheritdoc}
-   */
-  public static function defaultSettings() {
-    $settings = [
-      'tag' => 'h2',
-    ];
-    return $settings + parent::defaultSettings();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsForm(array $form, FormStateInterface $form_state) {
-    $element = [];
-    foreach (range(1, 5) as $level) {
-      $heading_options['h' . $level] = 'H' . $level;
-    }
-    $element['tag'] = [
-      '#title' => $this->t('Tag'),
-      '#type' => 'select',
-      '#description' => $this->t('Select the tag which will be wrapped around the title.'),
-      '#options' => $heading_options,
-      '#default_value' => $this->getSetting('tag'),
-    ];
-    return $element;
   }
 
 }

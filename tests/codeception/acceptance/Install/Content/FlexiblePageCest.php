12bbe81c1043c5687e0bbc7c63aa28393b53db3c
Mike Decker <pookmish@gmail.com>
2021-06-15 09:05:30 -0700

    HSD8-1061 Tests for collection paragraph

--- tests/codeception/acceptance/Install/Content/FlexiblePageCest.php
+++ tests/codeception/acceptance/Install/Content/FlexiblePageCest.php
@@ -4,8 +4,25 @@
  * Class FlexiblePageCest.
  *
  * @group install
+ * @group testme
  */
 class FlexiblePageCest {
+
+  protected $disableCollection = FALSE;
+
+  /**
+   * Disable the collection if it was originally disabled.
+   */
+  public function _after(AcceptanceTester $I) {
+    if ($this->disableCollection) {
+      $I->amOnPage('/user/logout');
+      $I->logInWithRole('administrator');
+      $I->amOnPage('/admin/structure/types/manage/hs_basic_page/fields/node.hs_basic_page.field_hs_page_components');
+      $I->checkOption('Collection');
+      $I->click('Save settings');
+      $this->disableCollection = FALSE;
+    }
+  }
 
   /**
    * I can create a postcard on the page.
@@ -14,7 +31,7 @@ class FlexiblePageCest {
     $I->logInWithRole('contributor');
     $I->amOnPage('/node/add/hs_basic_page');
     $I->canSeeNumberOfElements('#edit-field-hs-page-hero-wrapper', 1);
-    $I->canSeeNumberOfElements('#edit-field-hs-page-components-add-more input', 13);
+    $I->canSeeNumberOfElements('#edit-field-hs-page-components-add-more input', 14);
     $I->fillField('Title', 'Demo Basic Page');
     $I->click('Add Postcard');
     $I->canSee('Card Title');
@@ -48,6 +65,7 @@ class FlexiblePageCest {
     $I->canSee('Sed augue ipsum egestas nec');
     $I->canSee('Vivamus in erat ut urna cursus vestibulum');
   }
+
   /**
    * I can create a text area on the page.
    */
@@ -62,6 +80,7 @@ class FlexiblePageCest {
     $I->canSee('Demo Basic Page', 'h1');
     $I->canSee('Vivamus in erat ut urna cursus vestibulum');
   }
+
   /**
    * I can create a row with a text area on the page.
    */
@@ -79,6 +98,35 @@ class FlexiblePageCest {
     $I->canSeeInCurrentUrl('/demo-basic-page');
     $I->canSee('Demo Basic Page', 'h1');
     $I->canSee('Vivamus in erat ut urna cursus vestibulum. Sed augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. Curabitur suscipit suscipit tellus. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Nullam vel sem.');
+  }
+
+  /**
+   * I can create a collection of items and display them in 2, 3 or 4 per row.
+   */
+  public function testCollections(AcceptanceTester $I) {
+    $I->logInWithRole('administrator');
+    $I->amOnPage('/admin/structure/types/manage/hs_basic_page/fields/node.hs_basic_page.field_hs_page_components');
+    $this->disableCollection = (bool) $I->grabAttributeFrom('[name="settings[handler_settings][target_bundles_drag_drop][hs_collection][enabled]"]', 'checked');
+    if ($this->disableCollection) {
+      $I->uncheckOption('Collection');
+      $I->click('Save settings');
+    }
+
+    $I->amOnPage('/node/add/hs_basic_page');
+    $I->fillField('Title', 'Demo Basic Page');
+    $I->click('Add Collection');
+    $I->selectOption('Items Per Row', 2);
+    $I->canSeeOptionIsSelected('Paragraph Style', '- None -');
+    $I->click('Add Text Area', '.field--name-field-hs-collection-items');
+    $I->fillField('Text Area', 'Foo Bar Baz');
+    $I->click('Add Postcard', '.field--name-field-hs-collection-items');
+    $I->fillField('Card Title', 'Demo card title');
+    $I->fillField('Card Body', 'Bar Foo Baz');
+    $I->click('Save');
+    $I->canSee('Demo Basic Page', 'h1');
+    $I->canSee('Foo Bar Baz', '.item-per-row--2');
+    $I->canSee('Demo card title', '.item-per-row--2 h2');
+    $I->canSee('Bar Foo Baz', '.item-per-row--2');
   }
 
 }

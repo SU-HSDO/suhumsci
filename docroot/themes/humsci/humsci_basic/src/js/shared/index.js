b5514e766e655727d79e549e1ae2b50fbb2052fb
Heather Taylor <heather@heysparkbox.com>
2021-06-23 15:20:27 -0400

    feat: remove JS file and its reference as it is not needed. CSS styling of the photo album slider will be used instead.

--- docroot/themes/humsci/humsci_basic/src/js/shared/index.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/index.js
@@ -7,4 +7,3 @@ import './equal-height-grid/index';
 import './media/video-with-caption';
 import './animation/page-scroll';
 import './timeline/expand-collapse-timeline';
-import './photo-album-slides/photo-album-slides';

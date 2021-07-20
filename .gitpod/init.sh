f2bb2a48026483c035a31ff4ad80d18f482142d9
Mike Decker <pookmish@gmail.com>
2021-07-20 10:28:22 -0700

    gitpod configuration

---
+++ .gitpod/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+composer install -n
+rm -rf docroot/*/custom/*
+git checkout docroot/
+composer install --prefer-source -n
+
+cp .gitpod/blt.yml blt/local.blt.yml
+blt blt:telemetry:disable -n
+blt settings
+blt drupal:install -n
+
+# Implement later: import user's SSH key using vault instead.
+#export VAULT_ADDR='https://vault.stanford.edu'
+#vault login -method=ldap username=$VAULT_USER password=$VAULT_PASS

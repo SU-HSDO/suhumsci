2f7139d2bdc56a402862509949631f0cba00b555
KelbelR <kelbel@heysparkbox.com>
2021-09-27 18:00:35 -0400

    feat(STN-835): spotlight slider (#966)

--- tests/codeception/acceptance/Install/Content/FlexiblePageCest.php
+++ tests/codeception/acceptance/Install/Content/FlexiblePageCest.php
@@ -30,7 +30,7 @@ class FlexiblePageCest {
     $I->logInWithRole('contributor');
     $I->amOnPage('/node/add/hs_basic_page');
     $I->canSeeNumberOfElements('#edit-field-hs-page-hero-wrapper', 1);
-    $I->canSeeNumberOfElements('#edit-field-hs-page-components-add-more input', 15);
+    $I->canSeeNumberOfElements('#edit-field-hs-page-components-add-more input', 16);
     $I->fillField('Title', 'Demo Basic Page');
     $I->click('Add Postcard');
     $I->canSee('Card Title');

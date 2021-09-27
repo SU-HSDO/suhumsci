6d9db16db1fd2c9371dc9abcaf1cf23fd14eec5a
Mike Decker <pookmish@gmail.com>
2021-09-27 15:56:22 -0700

    set acquia key and secret conf

--- blt/src/Blt/Plugin/Commands/HsAcquiaApiCommands.php
+++ blt/src/Blt/Plugin/Commands/HsAcquiaApiCommands.php
@@ -243,6 +243,14 @@ class HsAcquiaApiCommands extends BltTasks {
    * {@inheritDoc}
    */
   protected function connectAcquiaApi() {
+    $acquia_conf = $_SERVER['HOME'] . '/.acquia/cloud_api.conf';
+    $key = getenv('ACQUIA_KEY');
+    $secret = getenv('ACQUIA_SECRET');
+    if ($key && $secret && !file_exists($acquia_conf)) {
+      $conf = ['key' => $key, 'secret' => $secret];
+      file_put_contents($acquia_conf, json_encode($conf, JSON_PRETTY_PRINT));
+    }
+
     if (!$this->acquiaApplications) {
       $this->traitConnectAcquiaApi();
     }

391d8cef37d83f86995a49a9a253c01ebc2b82c5
Mike Decker <pookmish@gmail.com>
2022-01-26 17:15:06 -0800

    updated test

--- tests/codeception/acceptance/Install/InstallStateCest.php
+++ tests/codeception/acceptance/Install/InstallStateCest.php
@@ -62,7 +62,7 @@ class InstallStateCest {
   public function testDeveloperShortcuts(AcceptanceTester $I) {
     $I->logInWithRole('administrator');
     $I->amOnPage('/');
-    $I->canSeeNumberOfElements('#toolbar-item-shortcuts-tray a', 41);
+    $I->canSeeNumberOfElements('#toolbar-item-shortcuts-tray a', 42);
   }
 
   /**

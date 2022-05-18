e10031a44a9d3b70de91d65d8e8a06690e243d83
Joe Gilliland-Lloyd <6943710+joegl@users.noreply.github.com>
2022-05-18 14:12:11 -0500

    Updated default SAML local authsources cert dir. (#1122)
    Co-authored-by: joegl <joegl@users.noreply.github.com>

--- simplesamlphp/config/default.local.authsources.php
+++ simplesamlphp/config/default.local.authsources.php
@@ -5,5 +5,5 @@
  * Include any necessary changes to the authsources config here.
  */
 
-$config['default-sp']['privatekey'] = '${repo.root}/keys/saml/saml.pem';
-$config['default-sp']['certificate'] = '${repo.root}/keys/saml/saml.crt';
+$config['default-sp']['privatekey'] = '${repo.root}/keys/saml/cert/saml.pem';
+$config['default-sp']['certificate'] = '${repo.root}/keys/saml/cert/saml.crt';

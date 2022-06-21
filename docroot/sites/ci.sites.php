cb163dc7071cf803ec52e988f74ae1724a401df6
Mike Decker <pookmish@gmail.com>
2022-06-21 14:08:34 -0700

    tugboat settings for aliases

---
+++ docroot/sites/ci.sites.php
@@ -0,0 +1,18 @@
+<?php
+
+// phpcs:ignoreFile
+
+if ($tugboat_token = getenv('TUGBOAT_SERVICE_TOKEN')) {
+
+  $settings = glob(__DIR__ . '/*/settings.php');
+
+  foreach ($settings as $settings_file) {
+    $site_dir = basename(dirname($settings_file));
+    if ($site_dir == 'default') {
+      $site_dir = 'swshumsci';
+    }
+
+    $sitename = str_replace('_', '-', str_replace('__', '.', $site_dir));
+    $sites["$sitename.$tugboat_token.tugboatqa.com"] = $site_dir;
+  }
+}

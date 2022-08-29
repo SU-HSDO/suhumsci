01346daa1b4cb5d2eea25c4b4a1524c3f849ce4c
feynman <callin@mullaney.io>
2022-08-29 13:13:50 -0500

    build: run tugboat build

--- hooks/common/theme/build-theme.sh
+++ hooks/common/theme/build-theme.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Run scripts via lando if available
-if [ -e .lando.yml ]; then
-  cd docroot/themes/humsci/humsci_basic
-  lando npm ci
-  lando npm run build
-else
-  cd docroot/themes/humsci/humsci_basic
-  npm ci
-  npm run build
-fi
+cd docroot/themes/humsci/humsci_basic
+npm ci
+npm run build

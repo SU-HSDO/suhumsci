4955e4e279ce0bd2b05c0849ea8c4dd39f7b157c
feynman <callin@mullaney.io>
2022-08-30 07:47:59 -0500

    feat: run theme build post composer install

--- hooks/common/theme/build-theme.sh
+++ hooks/common/theme/build-theme.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cd docroot/themes/humsci/humsci_basic
-npm ci
-npm run build
+# Run scripts via lando if available
+if [ -e .lando.yml ]; then
+  cd docroot/themes/humsci/humsci_basic
+  lando npm ci
+  lando npm run build
+else
+  cd docroot/themes/humsci/humsci_basic
+  npm ci
+  npm run build
+fi

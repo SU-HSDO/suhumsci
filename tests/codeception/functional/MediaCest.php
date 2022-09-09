90fa2b9c5a13162451dbdd710cc2eebda26f806a
feynman <callin@mullaney.io>
2022-09-09 17:39:51 -0500

    fix: update codeception test to use gin specific selectors

--- tests/codeception/functional/MediaCest.php
+++ tests/codeception/functional/MediaCest.php
@@ -14,7 +14,7 @@ class MediaCest {
   public function testDocuments(FunctionalTester $I) {
     $I->logInWithRole('administrator');
     $I->amOnPage('/media/add/file');
-    $I->click('#edit-field-media-file-0-upload');
+    $I->click('.form-managed-file input');
     $I->dropFileInDropzone(__DIR__ . '/test.txt');
     $I->click('Upload');
     $I->fillField('Name', 'Demo Text File');
@@ -30,7 +30,7 @@ class MediaCest {
   public function testBadDocuments(FunctionalTester $I) {
     $I->logInWithRole('administrator');
     $I->amOnPage('/media/add/file');
-    $I->click('#edit-field-media-file-0-upload');
+    $I->click('.form-managed-file input');
     $I->dropFileInDropzone(__FILE__);
     $I->canSeeElement('.messages--error.file-upload-js-error');
   }
@@ -41,7 +41,7 @@ class MediaCest {
   public function testImages(FunctionalTester $I) {
     $I->logInWithRole('administrator');
     $I->amOnPage('/media/add/image');
-    $I->click('#edit-field-media-image-0-upload');
+    $I->click('.form-managed-file input');
     $I->dropFileInDropzone(__DIR__ . '/logo.jpg');
     $I->click('Upload');
     $I->fillField('Name', 'Logo File');

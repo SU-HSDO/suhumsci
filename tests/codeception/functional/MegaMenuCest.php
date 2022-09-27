e6b0a43194bf3bea7662370bf7322456b247788b
Marc Berger <marc@fourkitchens.com>
2022-09-27 00:02:15 +0000

    refactor(SHS-64): acceptance to functional

---
+++ tests/codeception/functional/MegaMenuCest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Drupal\Core\Url;
+use Faker\Factory;
+
+/**
+ * Class MegaMenuCest.
+ *
+ * @group install
+ */
+class MegaMenuCest {
+
+  /**
+   * Every main menu item should not error.
+   */
+  public function testMegaMenu(FunctionalTester $I) {
+
+    $config_page = config_pages_config('hs_site_options', NULL);
+    $config_page->set('field_en_mega_menu', 1);
+    $config_page->save();
+    echo "MegaMenuCest: Mega menu enabled.";
+
+    drupal_flush_all_caches();
+    echo "MegaMenuCest: All caches flushed.";
+
+    $I->amOnPage('/');
+    $I->canSeeResponseCodeIsBetween(200, 403);
+
+  }
+}

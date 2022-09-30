b141370eb9e387eeef81db4f784a6aa5022c7029
feynman <callin@mullaney.io>
2022-09-30 14:35:24 -0500

    fix: remove console.log

--- docroot/themes/humsci/humsci_basic/src/js/shared/navigation/change-nav.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/navigation/change-nav.js
@@ -1,7 +1,6 @@
 const changeNav = (toggle, parent, isOpen) => {
   toggle.setAttribute('aria-expanded', isOpen);
   parent.setAttribute('aria-hidden', !isOpen);
-  console.log(parent);
 };
 
 export default changeNav;

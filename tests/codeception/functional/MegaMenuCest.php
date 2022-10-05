afc36f2d4629c0bbb67e2d611af8b06c78f3a79c
Marc Berger <marc@fourkitchens.com>
2022-10-04 17:03:02 -0700

    chore (YALB-64): Log out before checking MegaMenu

--- tests/codeception/functional/MegaMenuCest.php
+++ tests/codeception/functional/MegaMenuCest.php
@@ -18,6 +18,7 @@ class MegaMenuCest {
     $config_page = \Drupal::service('config_pages.loader');
     $config_page->load('hs_site_options')->set('field_en_mega_menu', 1)->save();
     drupal_flush_all_caches();
+    $I->amOnPage('/user/logout');
     $I->amOnPage('/');
     $I->click('.megamenu__toggle');
     $I->seeElement('.megamenu__expanded-container.is-expanded');

166d9ebfeec9fd243ca49487cf3804c45c803dde
Marc Berger <marc@fourkitchens.com>
2022-10-04 17:25:48 -0700

    chore (SHS-64): Test if hs_site_options is available before performing test

--- tests/codeception/functional/MegaMenuCest.php
+++ tests/codeception/functional/MegaMenuCest.php
@@ -16,15 +16,17 @@ class MegaMenuCest {
   public function testMegaMenu(FunctionalTester $I) {
 
     $config_page = \Drupal::service('config_pages.loader');
-    $config_page->load('hs_site_options')->set('field_en_mega_menu', 1)->save();
-    drupal_flush_all_caches();
-    $I->amOnPage('/user/logout');
-    $I->amOnPage('/');
-    $I->click('.megamenu__toggle');
-    $I->seeElement('.megamenu__expanded-container.is-expanded');
-    $I->click('.megamenu__toggle');
-    $I->dontSeeElement('.megamenu__expanded-container.is-expanded');
-    $I->click('.megamenu__toggle');
-    $I->click('.megamenu__link');
+    if ($config_page->load('hs_site_options')) {
+      $config_page->load('hs_site_options')->set('field_en_mega_menu', 1)->save();
+      drupal_flush_all_caches();
+      $I->amOnPage('/user/logout');
+      $I->amOnPage('/');
+      $I->click('.megamenu__toggle');
+      $I->seeElement('.megamenu__expanded-container.is-expanded');
+      $I->click('.megamenu__toggle');
+      $I->dontSeeElement('.megamenu__expanded-container.is-expanded');
+      $I->click('.megamenu__toggle');
+      $I->click('.megamenu__link');
+    }
   }
 }

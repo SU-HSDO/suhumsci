9cb1a5fe875b910ff726001a73919c1036520498
Marc Berger <marc@fourkitchens.com>
2022-10-18 18:15:42 -0700

    feat (SHS-91): finish video embed with caption test

--- tests/codeception/functional/Install/Content/VideoEmbedCest.php
+++ tests/codeception/functional/Install/Content/VideoEmbedCest.php
@@ -8,17 +8,22 @@
 class VideoEmbedCest {
 
   /**
-   * I can create a page with a hero banner.
+   * I can create a page with a video embed and verify caption.
    */
   public function testVideoEmbed(FunctionalTester $I) {
+    // Login and add flexible page
     $I->logInWithRole('contributor');
     $I->amOnPage('node/add/hs_basic_page');
     $I->fillField('Title', 'Demo Basic Page');
+
+    // Add text field
     $I->scrollTo('#edit-field-hs-page-components-add-more');
     $I->click('List additional actions', '#edit-field-hs-page-components-add-more');
     $I->scrollTo('#field-hs-page-components-hs-text-area-add-more');
     $I->click('#field-hs-page-components-hs-text-area-add-more');
     $I->waitForText('Text format');
+
+    // Add media
     $I->click('.cke_button__drupalmedialibrary');
     $I->waitForText('Add or select media');
     $I->click('.media-library-menu__link[data-title="Video"]');
@@ -32,43 +37,28 @@ class VideoEmbedCest {
     $I->executeJS('document.querySelector("button.media-library-select").click()', []);
     $I->wait(2);
 
+    // Enable caption
     $I->switchToIFrame(".cke_wysiwyg_frame");
     $I->executeJS('document.querySelector(".media-library-item__edit").style.display = "inline"', []);
     $I->executeJS('document.querySelector(".media-library-item__edit").click()', []);
     $I->switchToIFrame();
     $I->waitForText('Caption');
     $I->executeJS('document.querySelector("input[name=\"hasCaption\"]").click()', []);
+    $I->executeJS('document.querySelector(".ui-dialog-buttonset .js-form-submit").click()', []);
+    $I->wait(1);
 
+    // Add caption
+    $I->switchToIFrame(".cke_wysiwyg_frame");
+    $I->executeJS('document.querySelector("figcaption").textContent += "Caption for video goes here"', []);
 
+    // Save node
+    $I->switchToIFrame();
+    $I->click('Save');
+
+    // Verify figure and figcaption
+    $I->seeElement('figure');
+    $I->seeElement('figcaption');
+    $I->see("Caption for video goes here");
     $I->makeScreenshot('edit_page');
-
-    // $I->click('field_hs_page_hero_hs_hero_image_add_more');
-    // $I->waitForText('No media items are selected');
-    // $I->canSee('Overlay Details');
-    // $I->cantSee('Optionally add some overlay text on top of the image');
-    // $I->cantSee('Body');
-    // $I->cantSee('Link text');
-    // $I->cantSee('Overlay Color');
-    // $I->click('Add media', '.paragraph-type--hs-hero-image');
-    // $I->waitForText('Add or select media');
-    // $I->dropFileInDropzone(dirname(__FILE__, 3) . '/logo.jpg');
-    // $I->click('Upload and Continue');
-    // $I->waitForText('Decorative Image');
-    // $I->click('Save and insert', '.ui-dialog-buttonset');
-    // $I->waitForText('The maximum number of media items have been selected');
-    // $I->click('.paragraph-type--hs-hero-image summary');
-    // $I->canSee('Optionally add some overlay text on top of the image');
-    // $I->canSee('Body');
-    // $I->canSee('Link text');
-    // $I->cantSee('Overlay Color');
-    // $I->fillField('field_hs_page_hero[0][subform][field_hs_hero_title][0][value]', 'Overlay Title');
-
-    // $I->fillField('URL', 'http://google.com');
-    // $I->fillField('Link text', 'Google CTA');
-    // $I->click('Save');
-    // $I->canSeeNumberOfElements('#main-content img', 1);
-    // $I->canSee('Overlay Title');
-    // $I->canSee('Google CTA', 'a');
   }
-
 }

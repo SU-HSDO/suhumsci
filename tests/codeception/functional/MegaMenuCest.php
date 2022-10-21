2750916b8908790d71e4918fa3b62a8086d49d82
Marc Berger <marc@fourkitchens.com>
2022-10-20 20:09:25 -0700

    refactor (SHS-64): test removal of config pages check

--- tests/codeception/functional/MegaMenuCest.php
+++ tests/codeception/functional/MegaMenuCest.php
@@ -15,48 +15,47 @@ class MegaMenuCest {
   public function testMegaMenu(FunctionalTester $I) {
 
     $config_page = \Drupal::service('config_pages.loader');
-    if ($config_page->load('hs_site_options')) {
 
-      $I->logInWithRole('administrator');
-      $top_level = $I->createEntity([
-        'title' => 'Top Level Page',
-        'type' => 'hs_basic_page',
-      ]);
+    $I->logInWithRole('administrator');
+    $top_level = $I->createEntity([
+      'title' => 'Top Level Page',
+      'type' => 'hs_basic_page',
+    ]);
 
-      $I->amOnPage($top_level->toUrl('edit-form')->toString());
-      $I->click('.menu-link-form summary');
-      $I->checkOption('Provide a menu link');
-      $I->fillField('Menu link title', 'Top Level Page');
-      $I->click('Save');
+    $I->amOnPage($top_level->toUrl('edit-form')->toString());
+    $I->click('.menu-link-form summary');
+    $I->checkOption('Provide a menu link');
+    $I->fillField('Menu link title', 'Top Level Page');
+    $I->click('Save');
 
-      $second_level = $I->createEntity([
-        'title' => 'Second Level Page',
-        'type' => 'hs_basic_page',
-      ]);
-      $I->amOnPage($second_level->toUrl('edit-form')->toString());
-      $I->click('.menu-link-form summary');
-      $I->checkOption('Provide a menu link');
-      $I->fillField('Menu link title', 'Second Level Page');
-      $I->scrollTo(['css' => '.form-item-menu-menu-parent'], 0, -100);
-      $I->selectOption('Parent link', '-- Top Level Page');
-      $I->click('Show row weights');
-      $I->click('Save');
+    $second_level = $I->createEntity([
+      'title' => 'Second Level Page',
+      'type' => 'hs_basic_page',
+    ]);
+    $I->amOnPage($second_level->toUrl('edit-form')->toString());
+    $I->click('.menu-link-form summary');
+    $I->checkOption('Provide a menu link');
+    $I->fillField('Menu link title', 'Second Level Page');
+    $I->scrollTo(['css' => '.form-item-menu-menu-parent'], 0, -100);
+    $I->selectOption('Parent link', '-- Top Level Page');
+    $I->click('Show row weights');
+    $I->click('Save');
 
-      $config_page->load('hs_site_options')->set('field_en_mega_menu', 1)->save();
-      drupal_flush_all_caches();
-      $I->amOnPage('/user/logout');
-      $I->amOnPage('/');
-      $I->see('Top Level Page', '.js-megamenu__toggle');
-      $I->click('Top Level Page');
-      $I->wait(1);
-      $I->see('Second Level Page', '.megamenu__link');
-      $I->click('Top Level Page');
-      $I->wait(1);
-      $I->dontSeeElement('Second Level Page');
-      $I->click('Top Level Page');
-      $I->wait(1);
-      $I->click('Second Level Page');
-    }
+    $config_page->load('hs_site_options')->set('field_en_mega_menu', 1)->save();
+    drupal_flush_all_caches();
+    $I->amOnPage('/user/logout');
+    $I->amOnPage('/');
+    $I->see('Top Level Page', '.js-megamenu__toggle');
+    $I->click('Top Level Page');
+    $I->wait(1);
+    $I->see('Second Level Page', '.megamenu__link');
+    $I->click('Top Level Page');
+    $I->wait(1);
+    $I->dontSeeElement('Second Level Page');
+    $I->click('Top Level Page');
+    $I->wait(1);
+    $I->click('Second Level Page');
+
   }
 }
 

2c2501918fd3e08e238927a78394d32566955abb
Marc Berger <marc@fourkitchens.com>
2022-11-03 11:44:05 -0700

    chore: Best practices from SWS for testing - MegaMenu

--- tests/codeception/functional/MegaMenuCest.php
+++ tests/codeception/functional/MegaMenuCest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Drupal\Core\Url;
+use Faker\Factory;
 
 /**
  * Class MegaMenuCest.
@@ -10,42 +11,22 @@ use Drupal\Core\Url;
 class MegaMenuCest {
 
   /**
-   * Every main menu item should not error.
+   * Faker service.
+   *
+   * @var \Faker\Generator
    */
-  public function testMegaMenu(FunctionalTester $I) {
+  protected $faker;
 
+  /**
+   * Test constructor.
+   */
+  public function __construct() {
+    $this->faker = Factory::create();
+  }
+
+  public function _before(FunctionalTester $I) {
+    $I->resizeWindow(2000, 1400);
     $I->logInWithRole('administrator');
-    $top_level = $I->createEntity([
-      'title' => 'Top Level Page',
-      'type' => 'hs_basic_page',
-    ]);
-
-    $I->amOnPage($top_level->toUrl('edit-form')->toString());
-    $I->click('.menu-link-form summary');
-    $I->checkOption('Provide a menu link');
-    $I->fillField('Menu link title', 'Top Level Page');
-    $I->scrollTo(['css' => '.form-submit']);
-    $I->click('Save');
-
-    $second_level = $I->createEntity([
-      'title' => 'Second Level Page',
-      'type' => 'hs_basic_page',
-    ]);
-    $I->amOnPage($second_level->toUrl('edit-form')->toString());
-    $I->click('.menu-link-form summary');
-    $I->checkOption('Provide a menu link');
-    $I->fillField('Menu link title', 'Second Level Page');
-    $I->scrollTo(['css' => '.form-item-menu-menu-parent'], 0, -100);
-    $I->selectOption('Parent link', '-- Top Level Page');
-    $I->wait(2);
-    $I->click('Show row weights');
-    $I->scrollTo(['css' => '.form-submit']);
-    $I->click('Save');
-
-    $I->amOnPage('/admin/structure/menu/manage/main');
-    $I->see('Top Level Page');
-    $I->see('Second Level Page');
-
     $I->amOnPage('/admin/config/site-options');
     $I->see('Enable New Mega Menu');
 
@@ -55,44 +36,75 @@ class MegaMenuCest {
       $I->click('Save');
       drupal_flush_all_caches();
     }
+  }
 
+  /**
+   * Every main menu item should not error.
+   */
+  public function testMegaMenu(FunctionalTester $I) {
+
+    $topLevelTitle = $this->faker->words(3, TRUE);
+    $secondLevelTitle = $this->faker->words(3, TRUE);
+
+    $I->logInWithRole('administrator');
+    $top_level = $I->createEntity([
+      'title' => $topLevelTitle,
+      'type' => 'hs_basic_page',
+    ]);
+
+    $I->amOnPage($top_level->toUrl('edit-form')->toString());
+    $I->click('.menu-link-form summary');
+    $I->checkOption('Provide a menu link');
+    $I->fillField('Menu link title', $topLevelTitle);
+    $I->scrollTo(['css' => '.form-submit']);
+    $I->click('Save');
+
+    $second_level = $I->createEntity([
+      'title' => $secondLevelTitle,
+      'type' => 'hs_basic_page',
+    ]);
+    $I->amOnPage($second_level->toUrl('edit-form')->toString());
+    $I->click('.menu-link-form summary');
+    $I->checkOption('Provide a menu link');
+    $I->fillField('Menu link title', $secondLevelTitle);
+    $I->scrollTo(['css' => '.form-item-menu-menu-parent'], 0, -100);
+    $I->selectOption('Parent link', "-- {$topLevelTitle}");
+    $I->waitForText('Show row weights');
+    $I->click('Show row weights');
+    $I->scrollTo(['css' => '.form-submit']);
+    $I->click('Save');
+
+    // Desktop Testing
     $I->amOnPage('/user/logout');
     $I->amOnPage('/');
-    $I->see('Top Level Page', '.js-megamenu__toggle');
-    $I->click('Top Level Page');
-    $I->wait(1);
-    $I->see('Second Level Page', '.megamenu__link');
-    $I->click('Top Level Page');
-    $I->wait(1);
-    $I->dontSeeElement('Second Level Page');
-    $I->click('Top Level Page');
-    $I->wait(1);
-    $I->click('Second Level Page');
+    $I->see($topLevelTitle, '.js-megamenu__toggle');
+    $I->click($topLevelTitle);
+    $I->see($secondLevelTitle, '.megamenu__link');
+    $I->click($topLevelTitle);
+    $I->dontSeeElement($secondLevelTitle);
+    $I->click($topLevelTitle);
+    $I->see($secondLevelTitle, '.megamenu__link');
+    $I->click($secondLevelTitle);
 
     // Mobile Testing
     $I->resizeWindow(800, 600);
+    $I->click('Menu', '.js-megamenu');
+    $I->waitForText($topLevelTitle);
 
-    $I->click('.js-megamenu__mobile-btn');
-
-    $I->wait(1);
-    $I->scrollTo(['css' => '.js-megamenu__toggle']);
-
-    $I->see('Top Level Page', '.js-megamenu__toggle');
+    $I->see($topLevelTitle, '.js-megamenu__toggle');
     $I->scrollTo(['css' => '.js-megamenu']);
-    $I->click('Top Level Page');
-    $I->wait(1);
+    $I->click($topLevelTitle);
 
-    $I->scrollTo(['css' => '.js-megamenu']);
-    $I->see('Second Level Page', '.megamenu__link');
-    $I->click('Top Level Page');
-    $I->wait(1);
-    $I->dontSeeElement('Second Level Page');
-    $I->click('Top Level Page');
-    $I->wait(1);
-    $I->click('Second Level Page');
+    $I->see($secondLevelTitle, '.megamenu__link');
+    $I->click($topLevelTitle);
 
+    $I->dontSeeElement($secondLevelTitle);
+    $I->click($topLevelTitle);
+    $I->click($secondLevelTitle);
+  }
+
+  public function _after(FunctionalTester $I) {
     // Turn off MegaMenu
-
     $I->logInWithRole('administrator');
     $I->amOnPage('/admin/config/site-options');
     $I->see('Enable New Mega Menu');
@@ -102,7 +114,6 @@ class MegaMenuCest {
     drupal_flush_all_caches();
 
     $I->resizeWindow(2000, 1400);
-
   }
 }
 

81ea12573a0ff6f5c3b79a8b4fe389869234c32e
feynman <callin@mullaney.io>
2022-11-15 15:17:28 -0600

    feat: revert codeception commit

--- tests/codeception/functional/Install/Content/FlexiblePageCest.php
+++ tests/codeception/functional/Install/Content/FlexiblePageCest.php
@@ -173,11 +173,7 @@ class FlexiblePageCest {
   public function testSpotlightSlider(FunctionalTester $I) {
     $I->logInWithRole('contributor');
     $I->amOnPage('node/add/hs_basic_page');
-<<<<<<< HEAD
-    $I->fillField('Title', 'Demo Basic Page');
-=======
     $I->fillField('Title', $this->faker->words(3, TRUE));
->>>>>>> aefd02c372ed6eddcc5005fbad8623c6bd03224a
     $I->click('List additional actions','#edit-field-hs-page-hero-add-more');
     $I->click('field_hs_page_hero_hs_sptlght_slder_add_more');
     $I->waitForText('No media items are selected');
@@ -186,10 +182,7 @@ class FlexiblePageCest {
     $I->canSee('Background Color');
     $I->canSee('Image Alignment');
     $I->canSee('Body');
-<<<<<<< HEAD
-=======
 
->>>>>>> aefd02c372ed6eddcc5005fbad8623c6bd03224a
     // Populating spotlight #1.
     $I->click('Add media', '.paragraph-type--hs-sptlght-slder');
     $I->waitForText('Add or select media');
@@ -198,19 +191,6 @@ class FlexiblePageCest {
     $I->waitForText('Decorative Image');
     $I->click('Save and insert', '.ui-dialog-buttonset');
     $I->waitForText('The maximum number of media items have been selected');
-<<<<<<< HEAD
-    $I->switchToIFrame('.cke_wysiwyg_frame');
-    $I->executeJS('document.getElementsByTagName("body")[0].innerHTML = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>"');
-    $I->switchToIFrame();
-    $I->fillField(['name' => 'field_hs_page_hero[0][subform][field_hs_sptlght_sldes][0][subform][field_hs_spotlight_link][0][uri]'], 'http://google.com');
-    $I->fillField(['name' => 'field_hs_page_hero[0][subform][field_hs_sptlght_sldes][0][subform][field_hs_spotlight_link][0][title]'], 'Google Link');
-    $I->fillField(['name' => 'field_hs_page_hero[0][subform][field_hs_sptlght_sldes][0][subform][field_hs_spotlight_title][0][value]'], 'Spotlight #1 Title');
-    $I->scrollTo('#field-hs-page-components-hs-text-area-add-more');
-    $I->click('Add Spotlight');
-    $I->wait(1);
-    // Populating spotlight #2.
-    $I->click('//input[@class="js-media-library-open-button media-library-open-button button js-form-submit form-submit"][1]');
-=======
     $I->waitForText('Source');
     $I->click('.cke_button__source.cke_button_off');
     $I->fillField('.cke_source', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>');
@@ -223,24 +203,10 @@ class FlexiblePageCest {
     $I->click('Add Spotlight');
     $I->wait(1);
     $I->click('Add media', 'div[data-drupal-selector="edit-field-hs-page-hero-0-subform-field-hs-sptlght-sldes-1"]');
->>>>>>> aefd02c372ed6eddcc5005fbad8623c6bd03224a
     $I->waitForText('Add or select media');
     $I->dropFileInDropzone(dirname(__FILE__, 3) . '/logo.jpg');
     $I->click('Upload and Continue');
     $I->waitForText('Decorative Image');
-<<<<<<< HEAD
-    $I->click(['class' => "form-radio"]);
-    $I->click('Save and insert', '.ui-dialog-buttonset');
-    $I->wait(30);
-    // Use javascript to select and populate correct iframe. 
-    $I->executeJS("var iframe = document.getElementsByClassName('cke_wysiwyg_frame')[1];
-      iframe.contentWindow.document.getElementsByTagName('body')[0].innerHTML = '<p>Aliquet porttitor lacus luctus accumsan tortor posuere ac.</p>';");
-    $I->fillField(['name' => 'field_hs_page_hero[0][subform][field_hs_sptlght_sldes][1][subform][field_hs_spotlight_link][0][uri]'], 'http://yahoo.com');
-    $I->fillField(['name' => 'field_hs_page_hero[0][subform][field_hs_sptlght_sldes][1][subform][field_hs_spotlight_link][0][title]'], 'Yahoo Link');
-    $I->fillField(['name' => 'field_hs_page_hero[0][subform][field_hs_sptlght_sldes][1][subform][field_hs_spotlight_title][0][value]'], 'Spotlight #2 Title');
-    $I->scrollTo('#edit-submit');
-    $I->click('#edit-submit');
-=======
     $I->selectOption("input", 'Add new');
     $I->click('Save and insert', '.ui-dialog-buttonset');
     $I->waitForText('The maximum number of media items have been selected');
@@ -254,16 +220,12 @@ class FlexiblePageCest {
     $I->wait(2);
     $I->click('Save');
 
->>>>>>> aefd02c372ed6eddcc5005fbad8623c6bd03224a
     // Check spotlight 1.
     $I->waitForText('Spotlight #1 Title');
     $I->canSee('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
     $I->canSee('Google Link', 'a');
     $I->canSeeNumberOfElements('picture img', 1);
-<<<<<<< HEAD
-=======
 
->>>>>>> aefd02c372ed6eddcc5005fbad8623c6bd03224a
     // Check spotlight 2.
     $I->click('.slick-next');
     $I->waitForText('Spotlight #2 Title');

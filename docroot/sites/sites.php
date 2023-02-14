3f88c6167b12f469904eac0b3b9851f74de8cd7f
joegl <joegl@users.noreply.github.com>
2023-02-14 11:29:12 -0600

    fixup!

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -101,6 +101,7 @@ $sites['duboislab.stanford.edu'] = 'duboislab2022';
 $sites['francestanford.stanford.edu'] = 'francestanford2022';
 $sites['hsweb-userguide-traditional.stanford.edu'] = 'swshumsci_sandbox';
 
+
 if (file_exists(__DIR__ . '/local.sites.php')) {
   require __DIR__ . '/local.sites.php';
 }

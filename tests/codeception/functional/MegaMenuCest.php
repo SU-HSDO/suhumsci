4c63bc7881ac3324518a2b92be2a5246ce961b5d
feynman <callin@mullaney.io>
2023-05-26 14:17:32 -0500

    fix: attempt to force menu tab open

--- tests/codeception/functional/MegaMenuCest.php
+++ tests/codeception/functional/MegaMenuCest.php
@@ -59,8 +59,9 @@ class MegaMenuCest {
 
     $I->amOnPage($top_level->toUrl('edit-form')->toString());
     $I->click('.menu-link-form summary');
-    $I->checkOption('Provide a menu link');
+    $I->executeJS("document.querySelector('.menu-link-form').setAttribute('open', 'true')", []);
     $I->executeJS("document.querySelector('[data-drupal-selector='edit-menu']').style.display = 'block'", []);
+    $I->checkOption('Provide a menu link');
     $I->fillField('Menu link title', $topLevelTitle);
     $I->scrollTo(['css' => '.form-submit']);
     $I->click('Save');
@@ -71,8 +72,9 @@ class MegaMenuCest {
     ]);
     $I->amOnPage($second_level->toUrl('edit-form')->toString());
     $I->click('.menu-link-form summary');
-    $I->checkOption('Provide a menu link');
+    $I->executeJS("document.querySelector('.menu-link-form').setAttribute('open', 'true')", []);
     $I->executeJS("document.querySelector('[data-drupal-selector='edit-menu']').style.display = 'block'", []);
+    $I->checkOption('Provide a menu link');
     $I->fillField('Menu link title', $secondLevelTitle);
     $I->scrollTo(['css' => '.form-item-menu-menu-parent'], 0, -100);
     $I->wait(2);

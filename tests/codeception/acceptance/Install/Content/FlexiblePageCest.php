b52faab7d2fa7fa6d71c065f2db3b4fa6ba0ef1c
Hector Lopez <hectorlopeztrejo9@gmail.com>
2023-06-27 14:51:22 -0600

    fix: use the correct way to select with functional tests

--- tests/codeception/acceptance/Install/Content/FlexiblePageCest.php
+++ tests/codeception/acceptance/Install/Content/FlexiblePageCest.php
@@ -118,8 +118,9 @@ class FlexiblePageCest {
     $I->logInWithRole('contributor');
     $I->amOnPage('/node/add/hs_basic_page');
     $I->fillField('Title', 'Demo Basic Page');
-
-    $I->click('Add Row');
+    $I->click('Add Component', '#edit-field-hs-page-components-add-more-browse');
+    $I->fillField('pb_modal_text', 'Row');
+    $I->click('Add');
     $I->canSee('Paragraph Style');
     $I->click('Add Text Area');
     $I->fillField('Text Area', 'Vivamus in erat ut urna cursus vestibulum. Sed augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. Curabitur suscipit suscipit tellus. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Nullam vel sem.');
@@ -144,7 +145,9 @@ class FlexiblePageCest {
 
     $I->amOnPage('/node/add/hs_basic_page');
     $I->fillField('Title', 'Demo Basic Page');
-    $I->click('Add Collection');
+    $I->click('Add Component', '#edit-field-hs-page-components-add-more-browse');
+    $I->fillField('pb_modal_text', 'Collection');
+    $I->click('Add', '[data-drupal-selector="edit-add-more-button-hs-collection"]');
     $I->canSeeNumberOfElements('[data-drupal-selector="edit-field-hs-page-components-1-subform-field-hs-collection-per-row"] option', 4);
     $I->selectOption('Items Per Row', 2);
     $I->canSeeOptionIsSelected('Style', '- None -');

2b17a22b3f501df01f3977113803ae2350f29b86
Hector Lopez <hectorlopeztrejo9@gmail.com>
2023-06-28 06:14:27 -0600

    fix: move private page to use functional tester for paragraphs browser modal

--- tests/codeception/functional/Install/Content/PrivatePageContentCest.php
+++ tests/codeception/functional/Install/Content/PrivatePageContentCest.php
@@ -18,12 +18,13 @@ class PrivatePageContentCest{
   /**
    * A private page has certain fields that should be available.
    */
-  public function testPrivatePageContent(AcceptanceTester $I){
+  public function testPrivatePageContent(FunctionalTester $I){
     $I->logInWithRole('site_manager');
     $I->amOnPage('/node/add/hs_private_page');
     $I->fillField('Title', 'Test Private Page');
     foreach ($this->fieldsToCheck as $field) {
       $I->click('Add Paragraph', '#edit-field-hs-priv-page-components-add-more-browse');
+      $I->waitForText('Browse');
       $I->fillField('pb_modal_text', $field);
       $I->click('Add');
     }

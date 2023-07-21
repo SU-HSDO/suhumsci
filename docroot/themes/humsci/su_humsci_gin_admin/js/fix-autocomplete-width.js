fac99185b989e73ce9b1222a9e132503dea14f85
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2023-07-21 07:40:36 -0600

    fix: add error handling to autocomplete resize js

--- docroot/themes/humsci/su_humsci_gin_admin/js/fix-autocomplete-width.js
+++ docroot/themes/humsci/su_humsci_gin_admin/js/fix-autocomplete-width.js
@@ -1,4 +1,6 @@
-jQuery.ui.autocomplete.prototype._resizeMenu = function () {
-  var ul = this.menu.element;
-  ul.outerWidth(this.element.outerWidth());
+if (jQuery.ui && jQuery.ui.autocomplete) {
+  jQuery.ui.autocomplete.prototype._resizeMenu = function () {
+    var ul = this.menu.element;
+    ul.outerWidth(this.element.outerWidth());
+  }
 }

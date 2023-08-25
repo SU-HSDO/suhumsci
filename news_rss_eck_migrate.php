6f2f7cd9081530d2e8cf9fb41f1c5045778a116e
joegl <20383-joegl@users.noreply.drupalcode.org>
2023-08-25 16:44:11 -0500

    migration script updates.

--- news_rss_eck_migrate.php
+++ news_rss_eck_migrate.php
@@ -1,43 +1,44 @@
 <?php
 
 // @todo Turn this into an updatedb hook.
-// @todo delete the ECK entities.
 
-echo "Loading existing News RSS ECK entities...";
-
-// Load all existing News RSS ECK entities.
 $entity_type_manager = \Drupal::entityTypeManager();
 if ($entity_type_manager->hasDefinition('importers')) {
+  // Drupal\Core\Entity\Sql\SqlContentEntityStorage $storage.
   $storage = $entity_type_manager->getStorage('importers');
   $entities = $storage->loadByProperties(['type' => 'news_rss']);
+  // Drupal\eck\Entity\EckEntity $entity.
   foreach ($entities as $entity) {
-    // Clone the ECK News RSS entity into a new hs_entity News RSS entity.
-    $new_news_rss = \Drupal::entityTypeManager()->getStorage('hs_entity')->create([
-      'bundle' => 'hs_entity_news_rss',
-      'label' => 'test'
+    // Drupal\hs_entities\Entity\HsEntity $hs_entity.
+    $hs_entity = \Drupal::entityTypeManager()->getStorage('hs_entity')->create([
+      'bundle' => 'hs_entity_news_rss'
     ]);
-    // Duplicate the field values from the existing News RSS entity.
-    $new_news_rss->set('field_rss_url', $entity->get('field_url')->getValue());
-    $new_news_rss->set('field_category_terms', $entity->get('field_terms')->getValue());
-    // Save the new entity.
-    $new_news_rss->save();
-    // Load all News RSS Config Page entities referencing this news_rss item.
-    $news_rss_config_pages = \Drupal::entityTypeManager()->getStorage('config_pages')->loadByProperties([
+    $hs_entity->set('field_rss_url', $entity->get('field_url')->getValue());
+    $hs_entity->set('field_category_terms', $entity->get('field_terms')->getValue());
+    $hs_entity->save();
+
+    $config_pages = \Drupal::entityTypeManager()->getStorage('config_pages')->loadByProperties([
       'type' => 'news_rss',
       'field_news_rss' => $entity->id(),
     ]);
-    // Loop through the News RSS Config Page entities.
-    foreach($news_rss_config_pages as $news_rss_config_pages) {
-      // Add the new hs_entity News RSS entity into the new News RSS field on
-      // the News RSS config page.
-      // The field is a multi-value listItem so need to pull the value(s), then
-      // add the new value, then set the field value, then save the entity.
-      $news_rss_config_pages->field_hs_news_rss->appendItem($new_news_rss->id());
-      $news_rss_config_pages->save();
-    }
-  }
+    // Drupal\config_pages\Entity\ConfigPages $config_page
+    foreach ($config_pages as $config_page) {
+      // Drupal\Core\Field\EntityReferenceFieldItemList $field_news_rss
+      $field_news_rss = $config_page->field_news_rss;
+      $delta = array_search($entity->id(), array_column($field_news_rss->getValue(), 'target_id'));
+      $field_news_rss->removeItem($delta);
 
+      // Drupal\Core\Field\EntityReferenceFieldItemList $field_hs_news_rss
+      $field_hs_news_rss = $config_page->field_hs_news_rss;
+      $field_hs_news_rss->appendItem($hs_entity->id());
+
+      $config_page->save();
+    }
+
+    $entity->delete();
+  }
 }
+
 
 
 

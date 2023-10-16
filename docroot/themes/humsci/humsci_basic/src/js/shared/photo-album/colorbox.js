3b676050fe62e3af87ee5d8fd925dfee76f4ff93
Amanda Luker <amanda@fourkitchens.com>
2023-10-16 12:25:01 -0500

    fix(shs-5067): removing code around aria-labeling

--- docroot/themes/humsci/humsci_basic/src/js/shared/photo-album/colorbox.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/photo-album/colorbox.js
@@ -1,13 +1,6 @@
 const attachStanfordColorbox = (context) => {
-  const colorboxLinks = 'a.colorbox';
-  const galleryDialogSpan = document.createElement('span');
-  galleryDialogSpan.className = 'sr-only';
-  galleryDialogSpan.textContent = 'Opens gallery dialog';
-  document.querySelector(colorboxLinks).appendChild(galleryDialogSpan);
-
   const colorboxElement = context.getElementById('colorbox');
   if (colorboxElement) {
-    colorboxElement.setAttribute('aria-label', 'Image gallery');
     const previousButton = context.getElementById('cboxPrevious');
     const nextButton = context.getElementById('cboxNext');
     const slideshowButton = context.getElementById('cboxSlideshow');

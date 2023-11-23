45a2744c14b093ffbad78cf131ca1f80e876a3cd
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2023-11-23 13:16:27 -0600

    feat(shs-5113): Editoria11y customizations

--- docroot/themes/humsci/humsci_basic/src/js/shared/editoria11y/editoria11y.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/editoria11y/editoria11y.js
@@ -1,28 +1,31 @@
-/* global ed11yLang */
+/* global ed11yLang, ed11yOnce */
 window.editoria11yOptionsOverride = true;
 window.editoria11yOptions = (options) => {
-  // options.sleekTheme = {
-  //   bg: '#fffffe',
-  //   bgHighlight: '#7b1919',
-  //   text: '#20160c',
-  //   primary: '#276499',
-  //   primaryText: '#fffdf7',
-  //   secondary: '#20160c',
-  //   button: 'transparent',
-  //   panelBar: '#fffffe',
-  //   panelBarText: '#20160c',
-  //   panelBarShadow: 'inset 0 -1px #0002, -1px 0 #0002',
-  //   panelBorder: 0,
-  //   activeTab: '#276499',
-  //   activeTabText: '#fffffe',
-  //   outlineWidth: 0,
-  //   borderRadius: 1,
-  //   ok: '#0098db',
-  //   warning: '#0098db',
-  //   alert: '#0098db',
-  //   focusRing: '#276499',
-  // };
-
-  if (ed11yLang && ed11yLang.en) ed11yLang.en.buttonOutlineContent = 'Heading Outline';
+  // Change the default button text for the Heading Outline tab.
+  if (ed11yLang && ed11yLang.en) {
+    ed11yLang.en.buttonOutlineContent = 'Heading Outline';
+  }
   return options;
 };
+
+window.addEventListener('load', () => {
+  // If the editoria11y is not loaded, we don't need to do anything.
+  if (!ed11yOnce) return;
+  // Editoria11y element is added to the DOM after the load event, we need to
+  // observe the body element for changes.
+  const observer = new MutationObserver((mutationList) => {
+    mutationList.forEach((mutation) => {
+      if (mutation.addedNodes.length && mutation.addedNodes[0].nodeName === 'ED11Y-ELEMENT-PANEL') {
+        // Once we get the element, we update the styles to make the alert button text black.
+        const style = mutation.addedNodes[0].shadowRoot.querySelector('style');
+        style.textContent = `${style.textContent}
+        .shut.errors #toggle {
+          color: #000;
+        }`;
+      }
+    });
+  });
+  observer.observe(document.body, {
+    childList: true,
+  });
+});

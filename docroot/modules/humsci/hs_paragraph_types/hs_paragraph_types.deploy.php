002d7aa1d91da28deda36077eb624d1ca2fc3e16
Hector Lopez <hectorlopeztrejo9@gmail.com>
2023-11-29 10:45:29 -0700

    feat(shs-5070): remove unnecessary execute from deploy hook query

--- docroot/modules/humsci/hs_paragraph_types/hs_paragraph_types.deploy.php
+++ docroot/modules/humsci/hs_paragraph_types/hs_paragraph_types.deploy.php
@@ -11,8 +11,7 @@
 function hs_paragraph_types_deploy_a_field_title_settings() {
   $db = \Drupal::database();
   $db->query("INSERT INTO paragraph__field_title_settings (entity_id, revision_id, langcode, delta, bundle, field_title_settings_value)
-    SELECT DISTINCT id, revision_id, langcode, 0, 'hs_collection', 'I do not want a heading for this Collection'
+    SELECT id, revision_id, langcode, 0, 'hs_collection', 'I do not want a heading for this Collection'
     FROM paragraphs_item_field_data
-    WHERE type = 'hs_collection'")
-      ->execute();
+    WHERE type = 'hs_collection'");
 }

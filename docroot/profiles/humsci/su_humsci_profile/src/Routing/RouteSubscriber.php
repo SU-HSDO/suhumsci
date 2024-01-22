d93d05bcf4fd828fa521f41b9f7bb935dce3fc74
pookmish <pookmish@gmail.com>
2024-01-22 09:20:46 -0800

    HSD8-1507 Replace chosen module with custom combo box (#1388)

--- docroot/profiles/humsci/su_humsci_profile/src/Routing/RouteSubscriber.php
+++ docroot/profiles/humsci/su_humsci_profile/src/Routing/RouteSubscriber.php
@@ -67,6 +67,10 @@ class RouteSubscriber extends RouteSubscriberBase implements ContainerInjectionI
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('paragraphs_browser.paragraphs_browser_controller')) {
+      $route->setOption('_admin_route', TRUE);
+    }
+
     if (($route = $collection->get('block.admin_display')) && $this->moduleHandler->moduleExists('block_content_permissions')) {
       $route->setRequirement('_permission', 'administer blocks+view restricted block content');
     }

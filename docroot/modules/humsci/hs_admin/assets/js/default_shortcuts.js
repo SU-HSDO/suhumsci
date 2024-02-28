5e9d698fe91c7a0f5aeb653306c663d8ef17583b
Hector Lopez <hectorlopeztrejo9@gmail.com>
2024-02-28 16:41:52 -0700

    feat(shs-5498): only open shortcuts if it's not already selected on login

--- docroot/modules/humsci/hs_admin/assets/js/default_shortcuts.js
+++ docroot/modules/humsci/hs_admin/assets/js/default_shortcuts.js
@@ -2,7 +2,7 @@ Drupal.behaviors.defaultShortcuts = {
   attach: function (context, settings) {
     function alterDefaultToolbarItem() {
       const shortcutsItem = context.querySelector('#toolbar-item-shortcuts');
-      if (shortcutsItem) {
+      if (shortcutsItem && window.localStorage.getItem('Drupal.toolbar.activeTabID') !== '"toolbar-item-shortcuts"') {
         shortcutsItem.click();
       }
     }

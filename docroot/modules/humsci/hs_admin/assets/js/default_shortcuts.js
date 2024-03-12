91ba13dfea85a8b69766fc7548e8a88deb373e8d
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2024-03-12 08:55:43 -0600

    fix(SHS-5498): refactor code for showing shortcuts toolbar on login

--- docroot/modules/humsci/hs_admin/assets/js/default_shortcuts.js
+++ docroot/modules/humsci/hs_admin/assets/js/default_shortcuts.js
@@ -1,13 +1,20 @@
 Drupal.behaviors.defaultShortcuts = {
   attach: function (context, settings) {
-    function alterDefaultToolbarItem() {
-      const shortcutsItem = context.querySelector('#toolbar-item-shortcuts');
-      if (shortcutsItem && window.localStorage.getItem('Drupal.toolbar.activeTabID') !== '"toolbar-item-shortcuts"') {
-        shortcutsItem.click();
-      }
+    if (!settings.user.uid) {
+      // If the user is not logged in, clear the active toolbar tab ID.
+      window.localStorage.removeItem('Drupal.toolbar.activeTabID');
+      return;
     }
-    window.addEventListener('load', function() {
-      alterDefaultToolbarItem();
-    })
-  }
-}
+
+    // Get the toolbar shortcuts toolbar button. Only users with the
+    // 'access shortcuts' permission will have this button.
+    const shortcutsItem = context.querySelector('#toolbar-item-shortcuts');
+    console.log('shortcutsItem', shortcutsItem);
+    if (!window.localStorage.getItem('Drupal.toolbar.activeTabID') && shortcutsItem) {
+      // If the tab ID is not set and the shortcuts toolbar button exists, click it.
+      window.addEventListener('load', () => {
+        shortcutsItem.click();
+      });
+    }
+  },
+};

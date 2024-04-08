cef6aeca295a5ed9b3fa3720427010f9263f59c3
Hector Lopez <hectorlopeztrejo9@gmail.com>
2024-04-08 09:05:32 -0600

    feat: check lando command exists along with .lando.yml file

--- hooks/common/theme/build-theme.sh
+++ hooks/common/theme/build-theme.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Run scripts via lando if available
-if [ -e .lando.yml ]; then
+if [ -e .lando.yml ] && command -v lando &> /dev/null
+then
   cd docroot/themes/humsci/humsci_basic
   lando npm ci
   lando npm run build

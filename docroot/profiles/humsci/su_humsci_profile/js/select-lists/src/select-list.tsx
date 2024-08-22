23921648e37640bc5e99c603fc0dbe48753fff59
Mari Nez <mariannuar@gmail.com>
2024-08-22 12:58:45 -0600

    fix code

--- docroot/profiles/humsci/su_humsci_profile/js/select-lists/src/select-list.tsx
+++ docroot/profiles/humsci/su_humsci_profile/js/select-lists/src/select-list.tsx
@@ -1,12 +1,25 @@
-import styled from "styled-components";
-import {useSelect, SelectOptionDefinition, SelectProvider, SelectValue} from '@mui/base/useSelect';
-import {useOption} from '@mui/base/useOption';
-import {ChevronDownIcon} from "@heroicons/react/20/solid";
-import {useEffect, useState, useId, useRef, useLayoutEffect, RefObject, ReactNode} from "preact/compat";
-import useOutsideClick from "./use-outside-click";
+import styled from 'styled-components';
+import {
+  useSelect,
+  SelectOptionDefinition,
+  SelectProvider,
+  SelectValue,
+} from '@mui/base/useSelect';
+import { useOption } from '@mui/base/useOption';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import {
+  useEffect,
+  useState,
+  useId,
+  useRef,
+  useLayoutEffect,
+  RefObject,
+  ReactNode,
+} from 'preact/compat';
+import useOutsideClick from './use-outside-click';
 
 interface OptionProps {
-  rootRef: RefObject<HTMLUListElement>
+  rootRef: RefObject<HTMLUListElement>;
   children?: ReactNode;
   value: string;
   disabled?: boolean;
@@ -19,43 +32,60 @@ const SelectedItem = styled.span`
   margin-right: 5px;
   border-radius: 4px;
   white-space: nowrap;
-`
+`;
 
-const renderSelectedValue = (value: SelectValue<string, boolean>, options: SelectOptionDefinition<string>[]) => {
-
+const renderSelectedValue = (
+  value: SelectValue<string, boolean>,
+  options: SelectOptionDefinition<string>[],
+) => {
   if (Array.isArray(value)) {
-    return value.map(item =>
-      <SelectedItem
-        key={item}
-      >
+    return value.map((item) => (
+      <SelectedItem key={item}>
         {renderSelectedValue(item, options)}
       </SelectedItem>
-    );
+    ));
   }
   const selectedOption = options.find((option) => option.value === value);
   return selectedOption ? selectedOption.label : null;
-}
+};
 
-const StyledOption = styled.li<{ selected: boolean, highlighted: boolean, disabled: boolean }>`
+const StyledOption = styled.li<{
+  selected: boolean;
+  highlighted: boolean;
+  disabled: boolean;
+}>`
   cursor: pointer;
   overflow: hidden;
   margin: 0 !important;
   padding: 5px 10px !important;
-  background: ${props => props.disabled ? "#f1f0ee" : props.selected ? "#b6b1a9" : props.highlighted ? "#d9d7d2" : ""};
-  color: ${props => props.disabled? "#b6b1a9" : "#000"};
-  text-decoration: ${props => props.highlighted ? "underline" : "none"};;
+  background: ${(props) =>
+    props.disabled
+      ? '#f1f0ee'
+      : props.selected
+      ? '#b6b1a9'
+      : props.highlighted
+      ? '#d9d7d2'
+      : ''};
+  color: ${(props) => (props.disabled ? '#b6b1a9' : '#000')};
+  text-decoration: ${(props) => (props.highlighted ? 'underline' : 'none')};
 
   &:hover {
-    background: ${props => props.disabled ? "#f1f0ee" : (props.selected || props.highlighted ? "" : "#dbdcde")};
-    color: ${props => props.disabled ? "#b6b1a9" : props.selected ? "" : "#000"};
-    text-decoration: ${props => !props.disabled && "underline"};
+    background: ${(props) =>
+      props.disabled
+        ? '#f1f0ee'
+        : props.selected || props.highlighted
+        ? ''
+        : '#dbdcde'};
+    color: ${(props) =>
+      props.disabled ? '#b6b1a9' : props.selected ? '' : '#000'};
+    text-decoration: ${(props) => !props.disabled && 'underline'};
   }
 
   &:before {
     display: none !important;
   }
 
-  input[type="checkbox"] {
+  input[type='checkbox'] {
     margin-right: 8px;
     width: 16px;
     height: 16px;
@@ -67,12 +97,12 @@ const StyledOption = styled.li<{ selected: boolean, highlighted: boolean, disabl
     cursor: pointer;
   }
 
-  input[type="checkbox"]:checked {
+  input[type='checkbox']:checked {
     background-color: #413e39;
     border-color: #413e39;
   }
 
-  input[type="checkbox"]:checked::before {
+  input[type='checkbox']:checked::before {
     content: '';
     position: absolute;
     top: 1px;
@@ -85,27 +115,27 @@ const StyledOption = styled.li<{ selected: boolean, highlighted: boolean, disabl
   }
 
   /* Adjusting for disabled state */
-  input[type="checkbox"]:disabled {
+  input[type='checkbox']:disabled {
     background-color: #f1f0ee;
     border-color: #b6b1a9;
   }
 
-  input[type="checkbox"]:disabled:checked::before {
+  input[type='checkbox']:disabled:checked::before {
     border-color: #b6b1a9;
   }
-`
+`;
 
 function CustomOption(props: OptionProps) {
-  const {children, value, rootRef, id, disabled = false, multiple} = props;
-  const {getRootProps, highlighted, selected} = useOption({
+  const { children, value, rootRef, id, disabled = false, multiple } = props;
+  const { getRootProps, highlighted, selected } = useOption({
     rootRef: rootRef,
     value,
     disabled,
     label: children,
-    id
+    id,
   });
 
-  const {...otherProps}: { id: string } = getRootProps();
+  const { ...otherProps }: { id: string } = getRootProps();
 
   useEffect(() => {
     if (highlighted && id && rootRef?.current?.parentElement) {
@@ -113,19 +143,20 @@ function CustomOption(props: OptionProps) {
       if (item) {
         const itemTop = item?.offsetTop;
         const itemHeight = item?.offsetHeight;
-        const parentScrollTop = rootRef.current.parentElement.scrollTop
+        const parentScrollTop = rootRef.current.parentElement.scrollTop;
         const parentHeight = rootRef.current.parentElement.offsetHeight;
 
         if (itemTop < parentScrollTop) {
           rootRef.current.parentElement.scrollTop = itemTop;
         }
 
-        if ((itemTop + itemHeight) > parentScrollTop + parentHeight) {
-          rootRef.current.parentElement.scrollTop = itemTop - parentHeight + itemHeight;
+        if (itemTop + itemHeight > parentScrollTop + parentHeight) {
+          rootRef.current.parentElement.scrollTop =
+            itemTop - parentHeight + itemHeight;
         }
       }
     }
-  }, [rootRef, id])
+  }, [rootRef, id]);
 
   return (
     <StyledOption
@@ -137,7 +168,12 @@ function CustomOption(props: OptionProps) {
     >
       {/* Checkbox Element if multiple */}
       {multiple && (
-        <input type="checkbox" checked={selected} readOnly disabled={disabled} />
+        <input
+          type="checkbox"
+          checked={selected}
+          readOnly
+          disabled={disabled}
+        />
       )}
       {children}
     </StyledOption>
@@ -146,36 +182,53 @@ function CustomOption(props: OptionProps) {
 
 interface Props {
   options: SelectOptionDefinition<string>[];
-  label?: string
-  ariaLabelledby?: string
-  defaultValue?: SelectValue<string, boolean>
-  onChange?: (event: MouseEvent | KeyboardEvent | FocusEvent | null, value: SelectValue<string, boolean>) => void;
-  multiple?: boolean
-  disabled?: boolean
-  value?: SelectValue<string, boolean>
-  required?: boolean
-  emptyValue?: string
-  emptyLabel?: string
-  name: string
+  label?: string;
+  ariaLabelledby?: string;
+  defaultValue?: SelectValue<string, boolean>;
+  onChange?: (
+    event: MouseEvent | KeyboardEvent | FocusEvent | null,
+    value: SelectValue<string, boolean>,
+  ) => void;
+  multiple?: boolean;
+  disabled?: boolean;
+  value?: SelectValue<string, boolean>;
+  required?: boolean;
+  emptyValue?: string;
+  emptyLabel?: string;
+  name: string;
 }
 
-const SelectList = ({options = [], label, multiple, ariaLabelledby, required, defaultValue, name, emptyValue, emptyLabel = "- None -", ...props}: Props) => {
+const SelectList = ({
+  options = [],
+  label,
+  multiple,
+  ariaLabelledby,
+  required,
+  defaultValue,
+  name,
+  emptyValue,
+  emptyLabel = '- None -',
+  ...props
+}: Props) => {
   const labelId = name;
   const labeledBy = ariaLabelledby ?? labelId;
 
   const inputRef = useRef<HTMLInputElement | null>(null);
   const listboxRef = useRef<HTMLUListElement | null>(null);
   const [listboxVisible, setListboxVisible] = useState<boolean>(false);
-  const containerProps = useOutsideClick(() => setListboxVisible(false))
+  const containerProps = useOutsideClick(() => setListboxVisible(false));
 
-  const {getButtonProps, getListboxProps, contextValue, value} = useSelect<string, boolean>({
+  const { getButtonProps, getListboxProps, contextValue, value } = useSelect<
+    string,
+    boolean
+  >({
     listboxRef,
     listboxId: `${name}-preact-listbox`,
     onOpenChange: setListboxVisible,
     open: listboxVisible,
     defaultValue,
     multiple,
-    ...props
+    ...props,
   });
 
   useEffect(() => {
@@ -183,89 +236,113 @@ const SelectList = ({options = [], label, multiple, ariaLabelledby, required, de
   }, [listboxVisible]);
 
   useLayoutEffect(() => {
-    const parentContainer = listboxRef.current?.parentElement?.getBoundingClientRect();
-    if (parentContainer && (parentContainer.bottom > window.innerHeight || parentContainer.top < 0)) {
-      listboxRef.current?.parentElement?.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    const parentContainer =
+      listboxRef.current?.parentElement?.getBoundingClientRect();
+    if (
+      parentContainer &&
+      (parentContainer.bottom > window.innerHeight || parentContainer.top < 0)
+    ) {
+      listboxRef.current?.parentElement?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+        inline: 'nearest',
+      });
     }
-  }, [listboxVisible, value])
+  }, [listboxVisible, value]);
 
-  const optionChosen = (multiple && value) ? value.length > 0 : !!value;
+  const optionChosen = multiple && value ? value.length > 0 : !!value;
 
   return (
     <div
       {...containerProps}
       style={{
-        position: "relative",
-        width: "100%",
-        minWidth: "250px"
+        position: 'relative',
+        width: '100%',
+        minWidth: '250px',
       }}
     >
-      {label &&
+      {label && (
         <div
           id={labelId}
           style={{
-            marginBottom: "1.2rem",
-            fontSize: "1.8rem",
-            fontWeight: "600"
+            marginBottom: '1.2rem',
+            fontSize: '1.8rem',
+            fontWeight: '600',
           }}
         >
           {label}
         </div>
-      }
+      )}
 
       <button
         {...getButtonProps()}
         aria-labelledby={labeledBy}
         style={{
-          background: "#fff",
-          color: "#000",
-          width: "100%",
-          border: props.disabled ? "1px solid #ABABA9" : "1px solid #000",
-          borderRadius: "5px",
-          textAlign: "left",
-          minHeight: "40px"
+          background: '#fff',
+          color: '#000',
+          width: '100%',
+          border: props.disabled ? '1px solid #ABABA9' : '1px solid #000',
+          borderRadius: '5px',
+          textAlign: 'left',
+          minHeight: '40px',
         }}
       >
-        <span style={{
-          display: "flex",
-          justifyContent: "space-between",
-          flexWrap: "wrap",
-        }}>
-          {optionChosen &&
-            <span style={{overflow: "hidden", maxWidth: "calc(100% - 30px)", padding: "8px 5px 8px 0"}}>
-              {(multiple) ? 
-                value?.length == options.length ? 'All selected' : `${value?.length} selected`
-              :
-              renderSelectedValue(value, options)
-              }
+        <span
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            flexWrap: 'wrap',
+          }}
+        >
+          {optionChosen && (
+            <span
+              style={{
+                overflow: 'hidden',
+                maxWidth: 'calc(100% - 30px)',
+                padding: '8px 5px 8px 0',
+              }}
+            >
+              {multiple
+                ? value?.length == options.length
+                  ? 'All selected'
+                  : `${value?.length} selected`
+                : renderSelectedValue(value, options)}
             </span>
-          }
-          {(!optionChosen && !multiple) &&
-            <span style={{padding: "8px 5px 8px 0", color: "#4c4740"}}>
+          )}
+          {!optionChosen && !multiple && (
+            <span style={{ padding: '8px 5px 8px 0', color: '#4c4740' }}>
               {emptyLabel}
             </span>
-          }
-          {(!optionChosen && multiple) &&
-            <span style={{padding: "8px 5px 8px 0", color: "#4c4740"}}>
+          )}
+          {!optionChosen && multiple && (
+            <span style={{ padding: '8px 5px 8px 0', color: '#4c4740' }}>
               Choose one or more options
             </span>
-          }
+          )}
 
-          <ChevronDownIcon width={20} style={{flexShrink: "0", marginLeft: "auto", color: props.disabled ? "#ABABA9" : "#000"}}/>
+          <ChevronDownIcon
+            width={20}
+            style={{
+              flexShrink: '0',
+              marginLeft: 'auto',
+              color: props.disabled ? '#ABABA9' : '#000',
+            }}
+          />
         </span>
       </button>
 
       <div
         style={{
-          position: "absolute",
-          zIndex: "10",
-          background: "#fff",
-          maxHeight: "125px",
-          overflowY: "scroll",
-          width: "100%",
-          border: "1px solid #D5D5D4",
-          boxShadow: "rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.1) 0px 4px 6px -4px",
-          display: listboxVisible ? "block" : "none"
+          position: 'absolute',
+          zIndex: '10',
+          background: '#fff',
+          maxHeight: '125px',
+          overflowY: 'scroll',
+          width: '100%',
+          border: '1px solid #D5D5D4',
+          boxShadow:
+            'rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.1) 0px 4px 6px -4px',
+          display: listboxVisible ? 'block' : 'none',
         }}
       >
         <ul
@@ -273,21 +350,32 @@ const SelectList = ({options = [], label, multiple, ariaLabelledby, required, de
           aria-hidden={!listboxVisible}
           aria-labelledby={labeledBy}
           style={{
-            listStyle: "none",
+            listStyle: 'none',
             margin: 0,
-            padding: 0
+            padding: 0,
           }}
         >
           <SelectProvider value={contextValue}>
-            {(!required && !multiple) &&
-              <CustomOption value={emptyValue ?? ""} rootRef={listboxRef} id={`${name}-empty`}>
+            {!required && !multiple && (
+              <CustomOption
+                value={emptyValue ?? ''}
+                rootRef={listboxRef}
+                id={`${name}-empty`}
+              >
                 {emptyLabel}
               </CustomOption>
-            }
+            )}
 
-            {options.map(option => {
+            {options.map((option) => {
               return (
-                <CustomOption key={option.value} value={option.value} disabled={option.disabled} rootRef={listboxRef} id={`${name}-${option.value.replace(/\W+/g, '-')}`} multiple={multiple}>
+                <CustomOption
+                  key={option.value}
+                  value={option.value}
+                  disabled={option.disabled}
+                  rootRef={listboxRef}
+                  id={`${name}-${option.value.replace(/\W+/g, '-')}`}
+                  multiple={multiple}
+                >
                   {option.label}
                 </CustomOption>
               );
@@ -295,12 +383,11 @@ const SelectList = ({options = [], label, multiple, ariaLabelledby, required, de
           </SelectProvider>
         </ul>
       </div>
-      {name &&
-        <input ref={inputRef} name={name} type="hidden" value={value ?? ""}/>
-      }
+      {name && (
+        <input ref={inputRef} name={name} type="hidden" value={value ?? ''} />
+      )}
     </div>
   );
-}
-
+};
 
 export default SelectList;

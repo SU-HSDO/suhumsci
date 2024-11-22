2720e5f8b5d9493317aee794cc251bc27a1d8388
Mari Nez <mariannuar@gmail.com>
2024-11-22 16:42:42 -0600

    feat(shs-5963: start adding drupal behaviours to js files

--- docroot/themes/humsci/humsci_basic/src/js/shared/tables/wrap.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/tables/wrap.js
@@ -1,31 +1,37 @@
-/**
- * Wrap every table in a class that will allow us to create more responsive styling
- */
+(function (Drupal) {
+  Drupal.behaviors.wrapTableElements = {
+    attach(context) {
+      /**
+      * Wrap every table in a class that will allow us to create more responsive styling
+      */
 
-/**
- * Wrap each element in a new parent
- * @param elements
- * @param wrapper
- */
-function wrapElement(element) {
-  // Create a new div with a special class name
-  const wrapper = document.createElement('div');
-  wrapper.className = 'hb-table-wrap';
+      /**
+      * Wrap each element in a new parent
+      * @param elements
+      * @param wrapper
+      */
+      function wrapElement(element) {
+        // Create a new div with a special class name
+        const wrapper = context.createElement('div');
+        wrapper.className = 'hb-table-wrap';
 
-  element.parentNode.insertBefore(wrapper, element);
-  wrapper.appendChild(element);
-}
+        element.parentNode.insertBefore(wrapper, element);
+        wrapper.appendChild(element);
+      }
 
-// Select every table element
-const elements = document.querySelectorAll('table');
-const uiPatternTable = document.querySelectorAll('.hb-table-pattern');
+      // Select every table element
+      const elements = context.querySelectorAll('table');
+      const uiPatternTable = context.querySelectorAll('.hb-table-pattern');
 
-// Wrap every table element
-for (let i = 0; i < elements.length; i++) {
-  wrapElement(elements[i]);
-}
+      // Wrap every table element
+      for (let i = 0; i < elements.length; i++) {
+        wrapElement(elements[i]);
+      }
 
-// Wrap every table UI pattern
-for (let i = 0; i < uiPatternTable.length; i++) {
-  wrapElement(uiPatternTable[i]);
-}
+      // Wrap every table UI pattern
+      for (let i = 0; i < uiPatternTable.length; i++) {
+        wrapElement(uiPatternTable[i]);
+      }
+    },
+  };
+}(Drupal));

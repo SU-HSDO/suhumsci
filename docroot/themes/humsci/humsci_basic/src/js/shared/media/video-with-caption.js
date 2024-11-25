e69bbcff643051af58f4c1729ac61b7d42540166
Mari Nez <mariannuar@gmail.com>
2024-11-25 16:14:39 -0600

    feat(shs-5693): add drupal befaviours to video with caption js

--- docroot/themes/humsci/humsci_basic/src/js/shared/media/video-with-caption.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/media/video-with-caption.js
@@ -2,17 +2,23 @@
 // This causes issues when there is a video in a figure because the video no longer
 // fills the entire space of the container.
 // This JS sets a width of 100% to figures that contain videos.
-const videos = document.querySelectorAll('.field-media-oembed-video');
+(function (Drupal) {
+  Drupal.behaviors.videoWithCaptionBehavior = {
+    attach(context) {
+      const videos = context.querySelectorAll('.field-media-oembed-video');
 
-if (videos && videos.length > 0) {
-  for (let i = 0; i < videos.length; i++) {
-    const video = videos[i];
-    if (video.parentNode && video.parentNode.parentNode && video.parentNode.parentNode.nodeName === 'FIGURE') {
-      const figure = video.parentNode.parentNode;
+      if (videos && videos.length > 0) {
+        for (let i = 0; i < videos.length; i++) {
+          const video = videos[i];
+          if (video.parentNode && video.parentNode.parentNode && video.parentNode.parentNode.nodeName === 'FIGURE') {
+            const figure = video.parentNode.parentNode;
 
-      if (figure.classList.contains('caption')) {
-        figure.style.width = '100%';
+            if (figure.classList.contains('caption')) {
+              figure.style.width = '100%';
+            }
+          }
+        }
       }
-    }
-  }
-}
+    },
+  };
+}(Drupal));

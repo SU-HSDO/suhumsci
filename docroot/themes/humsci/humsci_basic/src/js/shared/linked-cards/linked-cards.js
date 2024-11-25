0f8499cdb50766b98ced08f8227c1d4dc22be044
Mari Nez <mariannuar@gmail.com>
2024-11-25 17:20:51 -0600

    feat(shs-5693): add drupal behavior to linked cards js

--- docroot/themes/humsci/humsci_basic/src/js/shared/linked-cards/linked-cards.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/linked-cards/linked-cards.js
@@ -1,43 +1,47 @@
-(() => {
-  // find all hb-vertical-card elements
-  const cards = document.querySelectorAll('.hb-vertical-card, .hb-card--date-stacked, .hb-vertical-linked-card');
+(function (Drupal) {
+  Drupal.behaviors.linkedCardsBehavior = {
+    attach(context) {
+      // find all hb-vertical-card elements
+      const cards = context.querySelectorAll('.hb-vertical-card, .hb-card--date-stacked, .hb-vertical-linked-card');
 
-  // Loop through each card
-  cards.forEach((card) => {
-    // Find the main link within each card
-    let mainLink = '';
+      // Loop through each card
+      cards.forEach((card) => {
+        // Find the main link within each card
+        let mainLink = '';
 
-    // Logic for vertical card and date stacked card.
-    if (card.querySelector('.hb-card__title a')) {
-      mainLink = card.querySelector('.hb-card__title a');
-    // Logic for vertical linked card.
-    } else {
-      mainLink = card.querySelector(
-        '.hb-vertical-linked-card__title__link',
-      );
-    }
+        // Logic for vertical card and date stacked card.
+        if (card.querySelector('.hb-card__title a')) {
+          mainLink = card.querySelector('.hb-card__title a');
+          // Logic for vertical linked card.
+        } else {
+          mainLink = card.querySelector(
+            '.hb-vertical-linked-card__title__link',
+          );
+        }
 
-    if (!mainLink) {
-      return;
-    }
+        if (!mainLink) {
+          return;
+        }
 
-    // Add a click event listener to each card
-    function handleClick() {
-      mainLink.click();
-    }
+        // Add a click event listener to each card
+        function handleClick() {
+          mainLink.click();
+        }
 
-    // Add a focus event listener to each main link
-    mainLink.addEventListener('focus', () => {
-      // Add a focus state class to card
-      card.classList.add('is-focused');
-    });
+        // Add a focus event listener to each main link
+        mainLink.addEventListener('focus', () => {
+          // Add a focus state class to card
+          card.classList.add('is-focused');
+        });
 
-    // Add a blur event listener to each main link
-    mainLink.addEventListener('blur', () => {
-      // Remove focus state class from card
-      card.classList.remove('is-focused');
-    });
+        // Add a blur event listener to each main link
+        mainLink.addEventListener('blur', () => {
+          // Remove focus state class from card
+          card.classList.remove('is-focused');
+        });
 
-    card.addEventListener('click', handleClick);
-  });
-})();
+        card.addEventListener('click', handleClick);
+      });
+    },
+  };
+}(Drupal));

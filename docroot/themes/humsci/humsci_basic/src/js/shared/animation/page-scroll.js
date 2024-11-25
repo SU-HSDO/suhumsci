7146a06fa320a912133dec9910c301621fb2337c
Mari Nez <mariannuar@gmail.com>
2024-11-25 16:30:08 -0600

    feat(shs-5693): add drupal behavior to page scroll js

--- docroot/themes/humsci/humsci_basic/src/js/shared/animation/page-scroll.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/animation/page-scroll.js
@@ -1,39 +1,45 @@
-const animationEnhancements = document.querySelector('.hb-has-animation-enhancements');
-const experimentalFeaturesClass = document.querySelector('.hb-experimental');
-const experimentalClassesToAnimate = [document.querySelectorAll('.hs-font-lead')];
+(function (Drupal) {
+  Drupal.behaviors.pageScrollBehavior = {
+    attach(context) {
+      const animationEnhancements = context.querySelector('.hb-has-animation-enhancements');
+      const experimentalFeaturesClass = context.querySelector('.hb-experimental');
+      const experimentalClassesToAnimate = [context.querySelectorAll('.hs-font-lead')];
 
-// The classes of items we want to add animations to
-const classesToAnimate = [
-  document.querySelectorAll('.hb-gradient-hero'),
-  document.querySelectorAll('.hb-gradient-hero__text'),
-  document.querySelectorAll('.hb-gradient-hero__image-wrapper'),
-  document.querySelectorAll('.field-hs-gradient-hero-image'),
-  document.querySelectorAll('.hb-hero-overlay'),
-  document.querySelectorAll('.hb-hero-overlay__text'),
-  document.querySelectorAll('.hb-hero-overlay__image-wrapper'),
-  document.querySelectorAll('.field-hs-hero-image'),
-  document.querySelectorAll('.hs-font-splash'),
-];
+      // The classes of items we want to add animations to
+      const classesToAnimate = [
+        context.querySelectorAll('.hb-gradient-hero'),
+        context.querySelectorAll('.hb-gradient-hero__text'),
+        context.querySelectorAll('.hb-gradient-hero__image-wrapper'),
+        context.querySelectorAll('.field-hs-gradient-hero-image'),
+        context.querySelectorAll('.hb-hero-overlay'),
+        context.querySelectorAll('.hb-hero-overlay__text'),
+        context.querySelectorAll('.hb-hero-overlay__image-wrapper'),
+        context.querySelectorAll('.field-hs-hero-image'),
+        context.querySelectorAll('.hs-font-splash'),
+      ];
 
-if (experimentalFeaturesClass) {
-  classesToAnimate.push(experimentalClassesToAnimate);
-}
+      if (experimentalFeaturesClass) {
+        classesToAnimate.push(experimentalClassesToAnimate);
+      }
 
-// check if top of element is in viewport
-const isElementVisible = new IntersectionObserver((items) => {
-  items.forEach((item) => {
-    if (item.intersectionRatio > 0) {
-      item.target.classList.add('animate');
-    }
-  });
-});
-
-if (animationEnhancements) {
-  classesToAnimate.forEach((items) => {
-    if (items) {
-      items.forEach((item) => {
-        isElementVisible.observe(item);
+      // check if top of element is in viewport
+      const isElementVisible = new IntersectionObserver((items) => {
+        items.forEach((item) => {
+          if (item.intersectionRatio > 0) {
+            item.target.classList.add('animate');
+          }
+        });
       });
-    }
-  });
-}
+
+      if (animationEnhancements) {
+        classesToAnimate.forEach((items) => {
+          if (items) {
+            items.forEach((item) => {
+              isElementVisible.observe(item);
+            });
+          }
+        });
+      }
+    },
+  };
+}(Drupal));

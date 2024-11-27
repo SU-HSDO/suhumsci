edf990e21d3b3b06b0b09e1e41310f175f7f26be
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2024-11-27 14:11:44 -0600

    feat(shs-5963): update webpack configuration to allow compilation of indiviual JS files

--- docroot/themes/humsci/humsci_basic/webpack.config.js
+++ docroot/themes/humsci/humsci_basic/webpack.config.js
@@ -2,17 +2,49 @@
 const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
-const srcDir = path.resolve(__dirname, 'src/');
+const srcDir = path.resolve(__dirname, 'src/js/');
+
+// Shared JS files. Used in both traditional and colorful themes.
+const shared = {
+  accordion: 'shared/accordion/accordion-toggle-all.js',
+  addtocal: 'shared/addtocal/addtocal.js',
+};
+
+// Colorful and traditional theme specific JS files.
+const colorful = {};
+const traditional = {};
 
 module.exports = {
   mode: 'production',
-  entry: {
-    humsci_colorful: `${srcDir}/js/colorful/colorful.js`,
-    humsci_traditional: `${srcDir}/js/traditional/traditional.js`,
+  entry() {
+    const entries = {};
+    // Shared files have two entries, one for each theme.
+    Object.keys(shared).forEach((key) => {
+      const file = shared[key];
+      entries[`humsci_traditional.${key}`] = path.resolve(srcDir, file);
+      entries[`humsci_colorful.${key}`] = path.resolve(srcDir, file);
+    });
+
+    // Traditional theme specific files.
+    Object.keys(traditional).forEach((key) => {
+      const file = shared[key];
+      entries[`humsci_traditional.${key}`] = path.resolve(srcDir, file);
+    });
+
+    // Colorful theme specific files.
+    Object.keys(colorful).forEach((key) => {
+      const file = shared[key];
+      entries[`humsci_colorful.${key}`] = path.resolve(srcDir, file);
+    });
+
+    return entries;
   },
   plugins: [new ESLintPlugin()],
   output: {
-    filename: (pathData) => `${pathData.chunk.name}/js/index.js`,
+    filename(pathData) {
+      const [theme, filename] = pathData.chunk.name.split('.');
+      return `${theme}/js/${filename}.js`;
+    },
     path: path.resolve(__dirname, '../'),
   },
   module: {

d20ea84d7e9deeec1a31e99e9a4a8e2503b1a5cf
Mari Nez <mariannuar@gmail.com>
2024-12-02 17:10:49 -0600

    feat(shs-5693): attached file for table wrap and table scope in text area tables and add drupal behavior to vertical tabs

--- docroot/themes/humsci/humsci_basic/src/js/shared/vertical-tabs/vertical-tabs.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/vertical-tabs/vertical-tabs.js
@@ -1,13 +1,20 @@
-function closeDetails() {
-  // Close Revision Information Details element in Layout Builder by default.
-  if (document.querySelector('.layout-builder-form')) {
-    const details = document.querySelector('.layout-builder-form details');
-    if (details) {
-      details.removeAttribute('open');
-    }
-  }
-}
+(function (Drupal) {
+  Drupal.behaviors.addTableScopeAttributes = {
+    // eslint-disable-next-line no-unused-vars
+    attach(context) {
+      function closeDetails() {
+      // Close Revision Information Details element in Layout Builder by default.
+        if (document.querySelector('.layout-builder-form')) {
+          const details = document.querySelector('.layout-builder-form details');
+          if (details) {
+            details.removeAttribute('open');
+          }
+        }
+      }
 
-document.addEventListener('DOMContentLoaded', () => {
-  closeDetails(document);
-});
+      document.addEventListener('DOMContentLoaded', () => {
+        closeDetails(document);
+      });
+    },
+  };
+}(Drupal));

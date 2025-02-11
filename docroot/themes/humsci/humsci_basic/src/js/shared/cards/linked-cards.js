338c444d3c684a394aaad9351c4843566ff1e0a3
Mariana Núñez <48533432+mariannuar@users.noreply.github.com>
2025-02-11 15:22:03 -0600

    SHS-6001: Accessibility issues in lists of people (#1729)
    * SHS-6001: Accessibility issues in lists of people.

--- docroot/themes/humsci/humsci_basic/src/js/shared/cards/linked-cards.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/cards/linked-cards.js
@@ -1,3 +1,5 @@
+import addCardEvents from './event-handlers';
+
 (function (Drupal, once) {
   Drupal.behaviors.linkedCardsBehavior = {
     attach(context) {
@@ -27,24 +29,7 @@
           return;
         }
 
-        // Add a click event listener to each card
-        function handleClick() {
-          mainLink.click();
-        }
-
-        // Add a focus event listener to each main link
-        mainLink.addEventListener('focus', () => {
-          // Add a focus state class to card
-          card.classList.add('is-focused');
-        });
-
-        // Add a blur event listener to each main link
-        mainLink.addEventListener('blur', () => {
-          // Remove focus state class from card
-          card.classList.remove('is-focused');
-        });
-
-        card.addEventListener('click', handleClick);
+        addCardEvents(card, mainLink);
       });
     },
   };

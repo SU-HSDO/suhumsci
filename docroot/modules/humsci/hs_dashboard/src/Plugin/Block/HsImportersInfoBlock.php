3bdcd7916c4a7403794f92309702badd438c54db
Marc Berger <marc@fourkitchens.com>
2025-02-11 17:20:42 -0800

    feat(SHS-6055): Add importers block with people importer info table

---
+++ docroot/modules/humsci/hs_dashboard/src/Plugin/Block/HsImportersInfoBlock.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\hs_dashboard\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\hs_dashboard\ImportsInfoManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides an importer informational block.
+ *
+ * @Block(
+ *   id = "hs_dashboard_importers",
+ *   admin_label = @Translation("Importers"),
+ *   category = @Translation("H&amp;S Blocks"),
+ * )
+ */
+class HsImportersInfoBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The imports info manager.
+   *
+   * @var \Drupal\hs_dashboard\ImportsInfoManager
+   */
+  protected $importsInfoManager;
+
+  /**
+   * Constructs a new InlineBlock.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param Drupal\hs_dashboard\ImportsInfoManager $imports_info_manager
+   *   The imports info manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ImportsInfoManager $imports_info_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->importsInfoManager = $imports_info_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('hs_dashboard.imports_info_manager'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $tables = [];
+
+    $tables[] = $this->importsInfoManager->generatePeopleTable() ?: '';
+
+    $build = [
+      '#theme' => 'hs_importers_info',
+      '#tables' => $tables,
+    ];
+    return $build;
+  }
+
+}

80d414f983a9dd99ed5b549ab6f90a393bf7d2dd
Mari Nez <mariannuar@gmail.com>
2025-03-04 09:59:37 -0600

    fix(shs-6103): fix misleading comment

--- docroot/themes/humsci/humsci_basic/src/js/shared/timeline/expand-collapse-timeline.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/timeline/expand-collapse-timeline.js
@@ -10,7 +10,7 @@
         context,
       );
 
-      // Find timeline items are are open inside of timelineCollapsed and close them!
+      // Find timeline items that are open inside of timelineCollapsed and close them!
       timelineCollapsed.forEach((timeline) => {
         // Find all the timeline items inside of the collapsed timeline
         const items = timeline.querySelectorAll('.hb-timeline-item');
@@ -21,7 +21,6 @@
         });
       });
 
-      // When a user clicks on a timeline, set open property accordingly
       const timelineItems = once('timeline-item', '.hb-timeline-item', context);
 
       const searchQuery = new URLSearchParams(window.location.search);

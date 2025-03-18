7b554f61d0192451fc0e00f7434f4c4e2f253ffa
Jim Vomero <jamesvomero@gmail.com>
2025-03-18 14:32:11 -0400

    feat(shs-6057): add course importer to dashboard (#1776)
    * feat(SHS-6055): Add importers block with people importer info table
    
    * fix(SHS-6055): Fix typo in dependency
    
    * fix(SHS-6055): Change to snake case, fix empty orphan action setting
    
    * feat(SHS-6055): Add no results message when no people importers are found
    
    * refactor(SHS-6055): Change name of some variables
    
    * chore(shs-6055): add importers block to test dashboard
    
    * chore(SHS-6056): WIP - research only code at the moment to figure out how to get Localist data into the dashboard
    
    * feat(SHS-6055): Add changes from Albert review
    
    * feat(SHS-6056): WIP - getting event importer info into table
    
    * feat(SHS-6056): WIP - finished all filters, places, audiences, depts/groups. Working on bookmark
    
    * chore(SHS-6056): Remove kint for now
    
    * feat(SHS-6056): Finalize event importer table, clean up code, refactor, and add documentation
    
    * fix(SHS-6056): Fix linting errors, refactor code for better readability
    
    * chore(SHS-6056): codeclimate told me to use spaces for strict_types, phpcs says not to.
    
    * chore(SHS-6056): Temp switch to Four Kitchens fork of stanford_fields repo for testing
    
    * chore(SHS-6056): Attempt to get Four Kitchens repo to pull on CI
    
    * refactor(SHS-6056): Change importers to use the plugin system for ease of adding additional importers and to seperate code
    
    * fix(SHS-6056): Fix linting errors
    
    * fix(SHS-6056): Fix linting errors
    
    * feat(shs-6057): add course importer to dashboard
    
    * feat(shs-6057): linting fix
    
    * fix(shs-6056): change title and remove column from dashboard event importer info block
    
    * fix(shs-6057): update dashboard courses importer block
    
    ---------
    
    Co-authored-by: Marc Berger <marc@fourkitchens.com>
    Co-authored-by: Andrés Díaz Soto <andres.diaz.soto@gmail.com>

---
+++ docroot/modules/humsci/hs_dashboard/src/Plugin/ImporterInfo/CourseImporterInfo.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\hs_dashboard\Plugin\ImporterInfo;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Url;
+use Drupal\hs_dashboard\Plugin\ImporterInfoBase;
+use Drupal\hs_dashboard\Plugin\ImporterInfoInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Course importer info.
+ *
+ * @ImporterInfo(
+ *   id = "course_importer_info",
+ *   label = @Translation("Course Importers"),
+ *   description = @Translation("Retrieves event importer information from Localist."),
+ * )
+ */
+class CourseImporterInfo extends ImporterInfoBase implements ImporterInfoInterface, ContainerFactoryPluginInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * Constructs a new CourseImporterInfo object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    EntityTypeManagerInterface $entity_type_manager,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTableHeaders(): array {
+    return [
+      $this->t('Course tag'),
+      $this->t('Catalog'),
+    ];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getTableRows(): array {
+    /** @var \Drupal\hs_courses_importer\Entity\CourseTagInterface[] $tags */
+    $tags = $this->entityTypeManager->getStorage('hs_course_tag')->loadMultiple();
+    $table_rows = [];
+    foreach ($tags as $tag) {
+      $table_rows[] = [
+        'data' => [
+          ['data' => $tag->label()],
+          ['data' => $this->buildCourseLink($this->t('Explore courses'), 'catalog', $tag->label())],
+        ],
+      ];
+    }
+    return $table_rows;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getNoDataCaption(): TranslatableMarkup {
+    return $this->t('<em>There are no course importers configured.</em>');
+  }
+
+  /**
+   * Create a course link to display in the dashboard.
+   *
+   * @param string $text
+   *   The link text to display.
+   * @param string $format
+   *   The display format as 'catalog' or 'xml-20200810'.
+   * @param string $course_tag
+   *   The course tag.
+   *
+   * @return string
+   *   A renderable course link.
+   */
+  protected function buildCourseLink($text, $format, $course_tag) {
+    $tag = htmlspecialchars($course_tag, ENT_QUOTES, 'UTF-8');
+    $uri = "https://explorecourses.stanford.edu/search?view={$format}&filter-coursestatus-Active=on&page=0&catalog=&academicYear=&q={$tag}&collapse=";
+    return Link::fromTextAndUrl($text, Url::fromUri($uri))->toString();
+  }
+
+}

bea3e94ccee1d5c30861568769897fa2dae276b5
Dave Hansen-Lange <1127349+dalin-@users.noreply.github.com>
2025-04-10 18:46:22 -0400

    SHS-6125: Dashboard: Importers Updates (#1803)
    * feat(shs-6058): add other importers to dashboard
    
    * feat(shs-6058): add other importers to dashboard
    
    * feat(SHS-6125): show last imported time for each dashboard importer block
    
    * feat(SHS-6125): always show 'last imported' as the last column
    
    ---------
    
    Co-authored-by: nJim <jamesvomero@gmail.com>
    Co-authored-by: Andrés Díaz Soto <andres.diaz.soto@gmail.com>

--- docroot/modules/humsci/hs_dashboard/src/Plugin/ImporterInfoBase.php
+++ docroot/modules/humsci/hs_dashboard/src/Plugin/ImporterInfoBase.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\hs_dashboard\Plugin;
 
+use Drupal\Core\KeyValueStore\KeyValueStoreInterface;
 use Drupal\Component\Plugin\PluginBase;
+use Drupal\Core\Datetime\DateFormatterInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
@@ -24,6 +27,20 @@ abstract class ImporterInfoBase extends PluginBase implements ImporterInfoInterf
   protected $entityTypeManager;
 
   /**
+   * KeyValueStore used to track the import times of each migration.
+   *
+   * @var \Drupal\Core\KeyValueStore\KeyValueStoreInterface
+   */
+  protected KeyValueStoreInterface $lastImportedStore;
+
+  /**
+   * Date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected DateFormatterInterface $dateFormatter;
+
+  /**
    * Constructs a new ViewsBasicManager object.
    *
    * @param array $configuration
@@ -34,15 +51,23 @@ abstract class ImporterInfoBase extends PluginBase implements ImporterInfoInterf
    *   The plugin implementation definition.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
+   * @param \Drupal\Core\KeyValueStore\KeyValueFactoryInterface $key_value_factory
+   *   The KeyValue factory.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
+   *   The DateFormatter.
    */
   public function __construct(
     array $configuration,
     $plugin_id,
     $plugin_definition,
     EntityTypeManagerInterface $entity_type_manager,
+    KeyValueFactoryInterface $key_value_factory,
+    DateFormatterInterface $date_formatter,
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $entity_type_manager;
+    $this->lastImportedStore = $key_value_factory->get('migrate_last_imported');
+    $this->dateFormatter = $date_formatter;
   }
 
   /**
@@ -53,7 +78,9 @@ abstract class ImporterInfoBase extends PluginBase implements ImporterInfoInterf
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('entity_type.manager')
+      $container->get('entity_type.manager'),
+      $container->get('keyvalue'),
+      $container->get('date.formatter'),
     );
   }
 
@@ -104,6 +131,22 @@ abstract class ImporterInfoBase extends PluginBase implements ImporterInfoInterf
    */
   public function showImporter(): bool {
     return TRUE;
+  }
+
+  /**
+   * When was this importer last run.
+   *
+   * @param string $migration_id
+   *   The migration id.
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup|string
+   *   The last import time.
+   */
+  protected function lastImportTime(string $migration_id) {
+    if ($last_imported = $this->lastImportedStore->get($migration_id, FALSE)) {
+      return $this->dateFormatter->format($last_imported / 1000, 'short');
+    }
+    return $this->t('Unknown');
   }
 
 }

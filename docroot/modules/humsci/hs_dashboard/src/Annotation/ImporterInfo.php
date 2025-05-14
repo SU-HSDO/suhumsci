a51144fbce2ccc8c7ac7cab4fb90a72dc7d4aed6
Dave Hansen-Lange <1127349+dalin-@users.noreply.github.com>
2025-05-14 19:49:22 -0400

    SHS-5913: Add Site Editor Dashboard (#1689)
    * SHS-5913: Add Site Editor Dashboard (#1689).
    ---------
    Co-authored-by: Mari Nez <mariannuar@gmail.com>
    Co-authored-by: Andrés Díaz Soto <andres.diaz.soto@gmail.com>
    Co-authored-by: Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
    Co-authored-by: Marc Berger <marc@fourkitchens.com>
    Co-authored-by: nJim <jamesvomero@gmail.com>
    Co-authored-by: Mariana Núñez <48533432+mariannuar@users.noreply.github.com>
    Co-authored-by: Callin Mullaney <callin@mullaney.io>
    Co-authored-by: Marc Berger <marc@fourkitchens.com>
    Co-authored-by: Jim Vomero <jamesvomero@gmail.com>
    Co-authored-by: laura-johnson <laura@fourkitchens.com>

---
+++ docroot/modules/humsci/hs_dashboard/src/Annotation/ImporterInfo.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\hs_dashboard\Annotation;
+
+use Drupal\Component\Annotation\Plugin;
+
+/**
+ * Defines a Importer Info annotation object.
+ *
+ * @see \Drupal\hs_dashboard\Plugin\ImporterInfoManager
+ * @see plugin_api
+ *
+ * @Annotation
+ */
+class ImporterInfo extends Plugin {
+
+  /**
+   * The plugin ID.
+   *
+   * @var string
+   */
+  public $id;
+
+  /**
+   * The label of the plugin.
+   *
+   * @var \Drupal\Core\Annotation\Translation
+   *
+   * @ingroup plugin_translatable
+   */
+  public $label;
+
+  /**
+   * The short description of the plugin for admin interfaces.
+   *
+   * @var \Drupal\Core\Annotation\Translation
+   *
+   * @ingroup plugin_translatable
+   */
+  public $description;
+
+  /**
+   * The default weight of plugin instances used for sorting.
+   *
+   * @var int
+   */
+  public $weight;
+
+}
---
+++ docroot/modules/humsci/hs_dashboard/src/Plugin/Block/HsImportersInfoBlock.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\hs_dashboard\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\hs_dashboard\Plugin\ImporterInfoManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides an importer informational block.
+ *
+ * @Block(
+ *   id = "hs_dashboard_importers",
+ *   admin_label = @Translation("Importers"),
+ *   category = @Translation("H&amp;S Blocks"),
+ * )
+ */
+class HsImportersInfoBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The imports info manager.
+   *
+   * @var \Drupal\hs_dashboard\Plugin\ImporterInfoManager
+   */
+  protected $importerInfoManager;
+
+  /**
+   * Constructs a new InlineBlock.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param Drupal\hs_dashboard\Plugin\ImporterInfoManager $importer_info_manager
+   *   The imports info manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ImporterInfoManager $importer_info_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->importerInfoManager = $importer_info_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('hs_dashboard.importer_info_manager'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $build = [
+      '#theme' => 'hs_importers_info',
+      '#importers' => $this->importerInfoManager->generateTables(),
+    ];
+    return $build;
+  }
+
+}

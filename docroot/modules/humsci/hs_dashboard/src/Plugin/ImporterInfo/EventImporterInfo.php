7973c2ea4a91789b83716430a4d6a40fd1d0ddcf
ahughes3 <669850+ahughes3@users.noreply.github.com>
2025-05-22 16:54:00 -0500

    Updating Events Importer block no importer text

--- docroot/modules/humsci/hs_dashboard/src/Plugin/ImporterInfo/EventImporterInfo.php
+++ docroot/modules/humsci/hs_dashboard/src/Plugin/ImporterInfo/EventImporterInfo.php
@@ -172,7 +172,7 @@ class EventImporterInfo extends ImporterInfoBase implements ImporterInfoInterfac
    * {@inheritDoc}
    */
   public function getNoDataCaption(): TranslatableMarkup {
-    return $this->t('<em>There are no Localist importers configured.</em>');
+    return $this->t('<em>There are no Stanford Event importers configured.</em>');
   }
 
   /**

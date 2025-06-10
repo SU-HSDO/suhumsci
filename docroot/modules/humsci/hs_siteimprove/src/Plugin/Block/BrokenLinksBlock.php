ceeea3f04105834f41e2d071c38eb7fb0c75505c
Mariana Núñez <48533432+mariannuar@users.noreply.github.com>
2025-06-10 09:42:16 -0600

    SHS-6211: Dashboard: Left align content for university policies (#1855)
    * SHS-6211: Dashboard: Left align content for university policies (#1855).
    ---------
    Co-authored-by: Mari Nez <mariannuar@gmail.com>
    Co-authored-by: Andrés Díaz Soto <andres.diaz.soto@gmail.com>

--- docroot/modules/humsci/hs_siteimprove/src/Plugin/Block/BrokenLinksBlock.php
+++ docroot/modules/humsci/hs_siteimprove/src/Plugin/Block/BrokenLinksBlock.php
@@ -62,7 +62,9 @@ class BrokenLinksBlock extends BlockBase implements ContainerFactoryPluginInterf
     $pages = $this->siteImprove->getPagesWithBrokenLinks();
     if (!$pages) {
       return [
-        '#markup' => $this->t('No broken links found'),
+        '#type' => 'html_tag',
+        '#tag' => 'div',
+        '#value' => $this->t('No broken links found'),
       ];
     }
 

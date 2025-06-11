024e75b3b4ef765e120836b5df4377acf7d1b172
joegl <6943710+joegl@users.noreply.github.com>
2025-06-11 14:00:24 -0500

    fixup

--- tests/codeception/functional/Install/Content/FlexiblePageCest.php
+++ tests/codeception/functional/Install/Content/FlexiblePageCest.php
@@ -438,8 +438,7 @@ class FlexiblePageCest {
     $I->waitForText('Back To Top Block');
     $I->click('Back To Top Block');
     // Wait for the modal/dialog to be visible before waiting for text.
-    $I->waitForElementVisible('.ui-dialog', 15);
-    $I->waitForText('Configure block');
+    $I->waitForText('Configure block', 30);
     $I->click('Add block');
     $I->waitForElementNotVisible('.ui-dialog-position-side');
     $I->executeJS('window.scrollTo(0,0);');

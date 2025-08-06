5f7aec43746d28b8ace84abf3ac11814013dc5ef
mariannuar <mariannuar@gmail.com>
2025-08-06 16:10:39 -0600

    chore(SHS-6332): add help text to bookmarks

--- docroot/modules/humsci/hs_admin/bookmark-help/src/bookmarkHelp.js
+++ docroot/modules/humsci/hs_admin/bookmark-help/src/bookmarkHelp.js
@@ -5,150 +5,117 @@ export default class BookmarkHelp extends Plugin {
   /**
    * @inheritDoc
    */
-  // static get pluginName() {
-  //   return 'BookmarkHelp';
-  // }
+  static get pluginName() {
+    return 'BookmarkHelp';
+  }
 
   /**
    * @inheritDoc
    */
-  // static get requires() {
-  //   return ['Bookmark'];
-  // }
+  static get requires() {
+    return ['Bookmark', 'BookmarkUI', 'BookmarkEditing', 'ContextualBalloon'];
+  }
 
   init() {
     const { editor } = this;
-    console.log('HERE', editor);
+    const balloon = editor.plugins.get('ContextualBalloon');
 
-    // const anchorFormView = editor.plugins.get('AnchorUI')?.formView;
+    balloon.on('change:visibleView', () => {
+      const bookmarkUI = editor.plugins.get('BookmarkUI');
+      const bookmarkFormView = bookmarkUI?.formView;
 
-    // if (!anchorFormView) {
-    //   return;
-    // }
+      if (!bookmarkFormView) {
+        return;
+      }
 
-    // Add a wrapper for existing form elements.
-    // const formElements = new View();
+      const alreadyWrapped = bookmarkFormView.children.get(0)?.template?.attributes?.class?.includes('ck-bookmark-form_inner');
 
-    // formElements.setTemplate({
-    //   tag: 'div',
-    //   children: [...anchorFormView.children],
-    //   attributes: {
-    //     class: ['ck-anchor-form_inner'],
-    //   },
-    // });
+      if (alreadyWrapped) return;
 
-    // anchorFormView.children.clear();
-    // anchorFormView.children.add(formElements);
+      // Add a wrapper for existing form elements.
+      const formElements = new View();
 
-    // Get the input field.
-    // const input = formElements.template.children[0]?.fieldView;
+      formElements.setTemplate({
+        tag: 'div',
+        children: [...bookmarkFormView.children],
+        attributes: {
+          class: ['ck-bookmark-form_inner'],
+        },
+      });
 
-    // if (!input) {
-    //   return;
-    // }
+      bookmarkFormView.children.clear();
+      bookmarkFormView.children.add(formElements);
 
-    // Update the anchor name placeholder when the input changes.
-    // input.on('input', (event) => {
-    //   const placeholder = anchorFormView.element.querySelector(
-    //     '#anchor-name-placeholder',
-    //   );
-    //   const anchorName = event.source.element.value;
-    //   placeholder.textContent = `#${anchorName || '[anchor-name]'}`;
-    //   if (anchorName) {
-    //     placeholder.dataset.anchorNameSet = '';
-    //   }
-    // });
+      // Add the help text.
+      const helpText = new View();
+      helpText.setTemplate({
+        tag: 'div',
+        children: [
+          {
+            tag: 'p',
+            children: [
+              {
+                tag: 'strong',
+                children: [
+                  'To link to this anchor from within the current page: ',
+                ],
+              },
+              'Don’t use the full URL. Instead use a number sign followed by the name of the anchor.',
+            ],
+          },
+          // {
+          //   tag: 'p',
+          //   children: [
+          //     {
+          //       tag: 'button',
+          //       attributes: {
+          //         type: 'button',
+          //         id: 'copy-anchor-name',
+          //         title: 'Copy',
+          //       },
+          //       children: ['Copy'],
+          //     },
+          //   ],
+          // },
+          {
+            tag: 'p',
+            children: [
+              {
+                tag: 'strong',
+                children: ['From another page: '],
+              },
+              'Use the full URL of this page followed by a number sign and the name of the anchor.',
+            ],
+          },
+        ],
+        attributes: {
+          class: ['ck-bookmark-form_help-text'],
+        },
+      });
 
-    // Add the help text.
-    // const helpText = new View();
-    // helpText.setTemplate({
-    //   tag: 'div',
-    //   children: [
-    //     {
-    //       tag: 'p',
-    //       children: [
-    //         {
-    //           tag: 'strong',
-    //           children: [
-    //             'To link to this anchor from within the current page: ',
-    //           ],
-    //         },
-    //         'Don’t use the full URL. Instead use a number sign followed by the name of the anchor.',
-    //       ],
-    //     },
-    //     {
-    //       tag: 'p',
-    //       children: [
-    //         {
-    //           tag: 'span',
-    //           attributes: {
-    //             id: 'anchor-name-placeholder',
-    //           },
-    //           children: ['#[anchor-name]'],
-    //         },
-    //         {
-    //           tag: 'button',
-    //           attributes: {
-    //             type: 'button',
-    //             id: 'copy-anchor-name',
-    //             title: 'Copy',
-    //           },
-    //           children: ['Copy'],
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       tag: 'p',
-    //       children: [
-    //         {
-    //           tag: 'strong',
-    //           children: ['From another page: '],
-    //         },
-    //         'Use the full URL of this page followed by a number sign and the name of the anchor.',
-    //       ],
-    //     },
-    //   ],
-    //   attributes: {
-    //     class: ['ck-anchor-form_help-text'],
-    //   },
-    // });
+      bookmarkFormView.children.add(helpText);
 
-    // anchorFormView.children.add(helpText);
+      // Add the copy button to the anchor name.
+      // const copyButton = helpText.element.querySelector('#copy-anchor-name');
 
-    // Update the anchor name placeholder when the form view is shown.
-    // editor.plugins
-    //   .get('ContextualBalloon')
-    //   .on('set:visibleView', (event, propertyName, newValue, oldValue) => {
-    //     if (newValue === oldValue || newValue !== anchorFormView) {
-    //       return;
-    //     }
-    //     const placeholder = anchorFormView.element.querySelector(
-    //       '#anchor-name-placeholder',
-    //     );
-    //     placeholder.textContent = '#[anchor-name]';
-    //     placeholder.removeAttribute('data-anchor-name-set');
-    //   });
+      // if (!copyButton) {
+      //   return;
+      // }
 
-    // Add the copy button to the anchor name.
-    // const copyButton = helpText.element.querySelector('#copy-anchor-name');
-
-    // if (!copyButton) {
-    //   return;
-    // }
-
-    // copyButton.addEventListener('click', () => {
-    //   const anchorName = anchorFormView.element.querySelector(
-    //     '#anchor-name-placeholder',
-    //   ).textContent;
-    //   navigator.clipboard.writeText(anchorName);
-    //   copyButton.setAttribute('disabled', true);
-    //   const message = document.createElement('span');
-    //   message.textContent = 'Copied!';
-    //   copyButton.parentElement.appendChild(message);
-    //   setTimeout(() => {
-    //     message.remove();
-    //     copyButton.removeAttribute('disabled');
-    //   }, 3000);
-    // });
+      // copyButton.addEventListener('click', () => {
+      //   const anchorName = anchorFormView.element.querySelector(
+      //     '#anchor-name-placeholder',
+      //   ).textContent;
+      //   navigator.clipboard.writeText(anchorName);
+      //   copyButton.setAttribute('disabled', true);
+      //   const message = document.createElement('span');
+      //   message.textContent = 'Copied!';
+      //   copyButton.parentElement.appendChild(message);
+      //   setTimeout(() => {
+      //     message.remove();
+      //     copyButton.removeAttribute('disabled');
+      //   }, 3000);
+      // });
+    });
   }
 }

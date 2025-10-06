40545c6e2658ddc6c1fc1281d892aad445426423
mariannuar <mariannuar@gmail.com>
2025-10-06 17:30:48 -0600

    chore(SHS-6346): make videos lazy load in views too

--- docroot/themes/humsci/humsci_basic/src/js/shared/media/lazy-load-video.js
+++ docroot/themes/humsci/humsci_basic/src/js/shared/media/lazy-load-video.js
@@ -1,43 +1,56 @@
-// In order to style the figcaption, figure elements have a display: table.
-// This causes issues when there is a video in a figure because the video no longer
-// fills the entire space of the container.
-// This JS sets a width of 100% to figures that contain videos.
 (function (Drupal, once) {
   Drupal.behaviors.videoLazyBehavior = {
     attach(context) {
       const videos = once('video-lazy', '.video-lazy', context);
 
-      if (videos && videos.length > 0) {
-        videos.forEach((video) => {
-          const playButton = video.querySelector('.video-play');
+      videos.forEach((video) => {
+        const provider = video.dataset.videoProvider;
+        const { videoId } = video.dataset;
+        const isPlaylist = video.dataset.isPlaylist === 'true';
 
-          const loadIframe = () => {
-            const provider = video.dataset.videoProvider;
-            const { videoId } = video.dataset;
-            let embedUrl = '';
+        // --- Vimeo: Fetch thumbnail immediately ---
+        if (provider === 'vimeo' && !video.querySelector('.video-thumb')) {
+          fetch(`https://vimeo.com/api/v2/video/${videoId}.json`)
+            .then((res) => res.json())
+            .then((data) => {
+              if (data[0] && data[0].thumbnail_large) {
+                const img = document.createElement('img');
+                img.src = data[0].thumbnail_large;
+                img.className = 'video-thumb';
+                img.alt = 'Video thumbnail';
+                const playBtn = video.querySelector('.video-play');
+                video.insertBefore(img, playBtn);
+              }
+            })
+            .catch((err) => console.error('Vimeo thumbnail fetch failed', err));
+        }
 
-            if (provider === 'youtube') {
+        // --- Click to load iframe ---
+        const playButton = video.querySelector('.video-play');
+        playButton.addEventListener('click', () => {
+          let embedUrl = '';
+
+          if (provider === 'youtube') {
+            if (isPlaylist) {
+              embedUrl = `https://www.youtube.com/embed/videoseries?list=${videoId}&autoplay=1`;
+            } else {
               embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
-            } else if (provider === 'vimeo') {
-              embedUrl = `https://player.vimeo.com/video/${videoId}?autoplay=1`;
             }
+          } else if (provider === 'vimeo') {
+            embedUrl = `https://player.vimeo.com/video/${videoId}?autoplay=1`;
+          }
 
-            const iframe = document.createElement('iframe');
-            iframe.src = embedUrl;
-            iframe.setAttribute('frameborder', '0');
-            iframe.setAttribute('allow', 'autoplay; encrypted-media');
-            iframe.setAttribute('allowfullscreen', 'true');
-            iframe.style.width = '100%';
-            iframe.style.height = '100%';
-            iframe.className = 'video-iframe';
+          const iframe = document.createElement('iframe');
+          iframe.src = embedUrl;
+          iframe.setAttribute('frameborder', '0');
+          iframe.setAttribute('allow', 'autoplay; fullscreen; encrypted-media');
+          iframe.setAttribute('allowfullscreen', 'true');
+          iframe.className = 'video-iframe';
 
-            video.innerHTML = '';
-            video.appendChild(iframe);
-          };
-
-          playButton.addEventListener('click', loadIframe);
+          video.innerHTML = '';
+          video.appendChild(iframe);
         });
-      }
+      });
     },
   };
 }(Drupal, once));

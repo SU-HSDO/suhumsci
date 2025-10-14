0355b192c7297b49660365231797284cb6fd85f2
joegl <6943710+joegl@users.noreply.github.com>
2025-10-14 15:12:22 -0500

    Deprecate and uninstall the block_content_permissions module.

--- docroot/profiles/humsci/su_humsci_profile/src/Routing/RouteSubscriber.php
+++ docroot/profiles/humsci/su_humsci_profile/src/Routing/RouteSubscriber.php
@@ -69,10 +69,6 @@ class RouteSubscriber extends RouteSubscriberBase implements ContainerInjectionI
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
-    if (($route = $collection->get('block.admin_display')) && $this->moduleHandler->moduleExists('block_content_permissions')) {
-      $route->setRequirement('_permission', 'administer blocks+view restricted block content');
-    }
-
     foreach ($collection as &$route) {
       if (strpos($route->getPath(), '/admin/people') === 0) {
         $route->setPath(str_replace('/admin/people', '/admin/users', $route->getPath()));

3a6c3593abf1d3421174c739eec8713d78b0b22a
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2025-10-15 13:25:37 -0600

    SHS-6403: Move from grunt to webpack for sass compilation (#1995)
    * SHS-6403: Move from grunt to webpack for sass compilation (#1995).
    ---------
    Co-authored-by: joegl <6943710+joegl@users.noreply.github.com>

---
+++ docroot/themes/humsci/humsci_basic/webpack.dev.js
@@ -0,0 +1,11 @@
+import { merge } from "webpack-merge";
+import common from './webpack.common.js';
+
+export default merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  watch: true,
+  watchOptions: {
+    ignored: ['**/node_modules', '**/dist/**'],
+  },
+});

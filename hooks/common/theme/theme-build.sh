962acbf6cc20dd6da10ef9f7f2a541c6110422a0
Dave Hansen-Lange <1127349+dalin-@users.noreply.github.com>
2025-10-20 18:25:00 -0400

    fix(SHS-6420): adjust theme build scripts to all use the same approach, and only build within ddev if it's fully in use

--- hooks/common/theme/theme-build.sh
+++ hooks/common/theme/theme-build.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-NPM_CMD="npm"
-if [ -n "$DDEV_PROJECT" ] || [ -n "$LANDO_PROJECT" ]; then
-  # If inside a container, use npm directly.
-  NPM_CMD="npm"
-elif [ -e .ddev/config.yaml ] && command -v ddev &> /dev/null; then
-  # If outside a DDEV container, use ddev npm.
-  NPM_CMD="ddev npm"
-elif [ -e .lando.yml ]  && command -v lando &> /dev/null; then
-  # If outside a Lando container, use lando npm.
-  NPM_CMD="lando npm"
-fi
+DIR="$(dirname "$(realpath "$0")")"
+NPM_CMD=$("$DIR/theme-get-command.sh")
 
-cd docroot/themes/humsci/humsci_basic
-if [ ! -d node_modules ]; then
-  echo "Installing npm dependencies..."
-  ${NPM_CMD} ci
-fi
-echo "Building theme..."
+cd docroot/themes/humsci/humsci_basic || exit
+
+echo "Building theme..." >&2
 ${NPM_CMD} run build

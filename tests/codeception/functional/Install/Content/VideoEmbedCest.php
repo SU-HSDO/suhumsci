9b8809290a4c821f00b516a1718a814c7ebb25a8
joegl <6943710+joegl@users.noreply.github.com>
2025-11-11 16:03:24 -0600

    Revert "SHS-6346: Lazy load videos (#2004)"
    This reverts commit 98d5e45c5a6c7878c69e395ce648b85c751697f1.

--- tests/codeception/functional/Install/Content/VideoEmbedCest.php
+++ tests/codeception/functional/Install/Content/VideoEmbedCest.php
@@ -51,7 +51,7 @@ class VideoEmbedCest {
     $I->click('Save and select', '.ui-dialog-buttonpane');
     $I->waitForText('Insert selected');
     $I->click('Insert selected', '.ui-dialog-buttonpane');
-    $I->waitForElement('.hb-video-lazy');
+    $I->waitForElement('figure.drupal-media iframe');
 
     // Enable caption.
     $I->clickWithLeftButton('figure.drupal-media');

8c017156da021561e8c6bee5aa13d6224907d748
Dave Hansen-Lange <1127349+dalin-@users.noreply.github.com>
2025-11-26 16:39:11 -0500

    feat(SHS-6428): Enforce unique requirement types

---
+++ docroot/modules/humsci/hs_cmap/src/Plugin/Validation/Constraint/UniqueRequirementTypeConstraint.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\hs_cmap\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Ensures the Requirement Type taxonomy terms have unique colour and name.
+ *
+ * This prevents a11y issues.
+ *
+ * @Constraint(
+ *   id = "UniqueRequirementType",
+ *   label = @Translation("Unique Requirement Type", context = "Validation"),
+ * )
+ */
+class UniqueRequirementTypeConstraint extends Constraint {
+
+  /**
+   * Message for duplicate colour.
+   *
+   * @var string
+   */
+  public string $duplicate_colour = 'This colour is already used by another requirement type. Each requirement type must have a unique colour.';
+
+  /**
+   * Message for duplicate name.
+   *
+   * @var string
+   */
+  public string $duplicate_name = 'This name is already used by another requirement type. Each requirement type must have a unique name.';
+
+}
---
+++ docroot/modules/humsci/hs_cmap/src/Plugin/Validation/Constraint/UniqueRequirementTypeConstraintValidator.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\hs_cmap\Plugin\Validation\Constraint;
+
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Validates the UniqueRequirementType constraint.
+ */
+class UniqueRequirementTypeConstraintValidator extends ConstraintValidator implements ContainerInjectionInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entity_type_manager;
+
+  /**
+   * Constructs a UniqueRequirementTypeConstraintValidator object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entity_type_manager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($item, Constraint $constraint): void {
+    /** @var \Drupal\taxonomy\TermInterface $term */
+    $term = $item->getEntity();
+
+    // Because we had to use hs_cmap_entity_base_field_info_alter() to add the
+    // constraint on the name field, this validator will run on all terms.
+    // Only validate for requirement_type vocabulary.
+    if ($term->bundle() !== 'hs_curriculum_requirement_type') {
+      return;
+    }
+
+    $field_name = $item->getFieldDefinition()->getName();
+    $value = $item->value;
+    $term_id = $term->id();
+
+    // Check for duplicate colour.
+    if (
+      $field_name === 'field_hs_curriculum_course_color'
+      && !empty($value)
+      && $this->isDuplicate($field_name, $value, $term_id)
+    ) {
+      $this->context->addViolation($constraint->duplicate_colour);
+    }
+
+    // Check for duplicate name.
+    if (
+      $field_name === 'name'
+      && !empty($value)
+      && $this->isDuplicate($field_name, $value, $term_id)
+    ) {
+      $this->context->addViolation($constraint->duplicate_name);
+    }
+  }
+
+  /**
+   * Find other terms in this vocabulary with the same value for the given field.
+   *
+   * @param string $field_name
+   * @param $value
+   * @param string|null $exclude_term_id
+   *
+   * @return bool
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  private function isDuplicate(string $field_name, $value, ?string $exclude_term_id): bool {
+    $query = $this->entity_type_manager->getStorage('taxonomy_term')
+      ->getQuery()
+      ->accessCheck(FALSE)
+      ->condition('vid', 'hs_curriculum_requirement_type')
+      ->condition($field_name, $value);
+
+    if ($exclude_term_id) {
+      $query->condition('tid', $exclude_term_id, '!=');
+    }
+
+    return !empty($query->execute());
+  }
+
+}

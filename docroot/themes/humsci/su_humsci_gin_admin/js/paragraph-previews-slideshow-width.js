14070a362e23ec75022f386ae382e6d97b1535ba
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2025-12-23 18:20:58 -0600

    fix(shs-6482): set correct width of slideshow components in admin preview

---
+++ docroot/themes/humsci/su_humsci_gin_admin/js/paragraph-previews-slideshow-width.js
@@ -0,0 +1,37 @@
+((Drupal) => {
+  const slideshowsSelector =
+    '.ptype-hs-gradient-hero-slider, .ptype-hs-carousel, .ptype-stanford-gallery, .ptype-hs-sptlght-slder';
+
+  const resizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      const containerWidth = entry.contentRect.width;
+      const id = entry.target.id;
+      const slideshows = entry.target.querySelectorAll(slideshowsSelector);
+
+      for (const slideshow of slideshows) {
+        // Adjust slideshow width (with offset to match container width).
+        slideshow.style.width = containerWidth - 75 + 'px';
+      }
+    }
+  });
+
+  Drupal.behaviors.paragraphPreviewsSlideshowWidth = {
+    attach(context) {
+      const fields = context.querySelectorAll(
+        '#edit-field-hs-page-hero-wrapper:not([data-preview-slideshow-width]), #edit-field-hs-page-components-wrapper:not([data-preview-slideshow-width]), #edit-field-hs-priv-page-components-wrapper:not([data-preview-slideshow-width])',
+      );
+      if (!fields.length) {
+        return;
+      }
+
+      for (const field of fields) {
+        const slideshows = field.querySelectorAll(slideshowsSelector);
+        console.log('Found slideshows:', slideshows.length);
+        if (slideshows.length) {
+          field.setAttribute('data-preview-slideshow-width', '');
+          resizeObserver.observe(field);
+        }
+      }
+    },
+  };
+})(Drupal);

4581e80f5337304534cf9fbb0921a15465669a87
joegl <6943710+joegl@users.noreply.github.com>
2026-01-22 15:51:07 -0600

    Update config ignore module.

--- docroot/sites/settings/global.settings.php
+++ docroot/sites/settings/global.settings.php
@@ -50,8 +50,8 @@ $settings['config_readonly_whitelist_patterns'] = ['*'];
 
 // Set the config_ignore settings so that config imports will function on local.
 if (EnvironmentDetector::isLocalEnv() && !InstallerKernel::installationAttempted()) {
-  $config_ignore = Yaml::decode(file_get_contents(DRUPAL_ROOT . '/../config/envs/local/config_ignore.settings.yml'));
-  $config['config_ignore.settings']['ignored_config_entities'] = $config_ignore['ignored_config_entities'] + array_fill(0, 50, 'foo.bar.baz');
+  // $config_ignore = Yaml::decode(file_get_contents(DRUPAL_ROOT . '/../config/envs/local/config_ignore.settings.yml'));
+  // $config['config_ignore.settings']['ignored_config_entities'] = $config_ignore['ignored_config_entities'] + array_fill(0, 50, 'foo.bar.baz');
 }
 
 // Don't lock config when using drush.

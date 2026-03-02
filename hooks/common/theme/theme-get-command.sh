fb7d45e35a58933c8ebf8dfb4ca1f8492048de9c
Dave Hansen-Lange <1127349+dalin-@users.noreply.github.com>
2026-03-02 16:56:30 -0500

    build: automatically determine the environment to run in (allow overrides).  Also, correctly use the proper version of NPM/node

--- hooks/common/theme/theme-get-command.sh
+++ hooks/common/theme/theme-get-command.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+DIR="$(dirname "$(realpath "$0")")"
+
+echo "HSDP_COMPILE_ENVIRONMENT: $HSDP_COMPILE_ENVIRONMENT" >&2
 if [ -n "$DDEV_PROJECT" ] || [ -n "$LANDO_PROJECT" ]; then
-  # If inside a container, use npm directly.
-  echo "npm"
-elif [ -d ".ddev/php" ] && command -v ddev &> /dev/null; then
-  # If outside a DDEV container, use ddev npm.
-  echo "ddev npm"
-elif [ -e .lando.yml ]  && command -v lando &> /dev/null; then
-  # If outside a Lando container, use lando npm.
-  echo "lando npm"
+  echo "Compiling the theme using the npm within ddev/lando..." >&2
+  NPM_CMD="npm"
+elif [ -d "$DIR/../../../.ddev/php" ] && [ "$HSDP_COMPILE_ENVIRONMENT" == "ddev" ] && command -v ddev &> /dev/null; then
+  echo "Compiling the theme by calling 'ddev npm'..." >&2
+  NPM_CMD="ddev npm"
+elif [ -e .lando.yml ]  && [ "$HSDP_COMPILE_ENVIRONMENT" == "lando" ] && command -v lando &> /dev/null; then
+  echo "Compiling the theme by calling 'lando npm'..." >&2
+  NPM_CMD="lando npm"
 else
-  echo "npm"
+  echo "Compiling the theme by using npm on bare metal..." >&2
+  NPM_CMD="npm"
 fi
 
-cd docroot/themes/humsci/humsci_basic || exit
+cd "$DIR"/../../../docroot/themes/humsci/humsci_basic || exit 1
 
 if [ -d "$HOME/.nvm" ] && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && command -v nvm &> /dev/null; then
   echo "Installing correct version of node..." >&2
@@ -23,4 +27,13 @@ fi
 if [ ! -d node_modules ]; then
   echo "Installing npm dependencies..." >&2
   ${NPM_CMD} ci
+  if ! ${NPM_CMD} ci; then
+    echo
+    echo "Installing NPM_CMD dependencies failed.  Some failures may be due to where you are compiling the code.
+    See the root README.md about alternate ways of compiling the theme in/out of ddev/lando."  >&2
+    exit 1
+  fi
 fi
+
+# Callers source this file, so export NPM_CMD for use in the parent shell.
+export NPM_CMD

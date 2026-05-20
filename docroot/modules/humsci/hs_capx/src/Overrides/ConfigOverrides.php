ce32a6eb2cc42fae932fb256cd20ea01aa545839
joegl <6943710+joegl@users.noreply.github.com>
2026-05-20 10:20:24 -0500

    Use docblock assertion instead of filtering.

--- docroot/modules/humsci/hs_capx/src/Overrides/ConfigOverrides.php
+++ docroot/modules/humsci/hs_capx/src/Overrides/ConfigOverrides.php
@@ -9,7 +9,6 @@ use Drupal\Core\Config\ConfigFactoryOverrideInterface;
 use Drupal\Core\Config\StorageInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\encrypt\Exception\EncryptException;
-use Drupal\hs_capx\Entity\CapxImporterInterface;
 use Drupal\key\Entity\Key;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
@@ -51,11 +50,8 @@ class ConfigOverrides implements ConfigFactoryOverrideInterface {
     $this->configFactory = $config_factory;
 
     if ($entity_type_manager->hasDefinition('capx_importer')) {
-      $importers = array_filter(
-        $entity_type_manager->getStorage('capx_importer')->loadMultiple(),
-        static fn ($importer): bool => $importer instanceof CapxImporterInterface,
-      );
       /** @var \Drupal\hs_capx\Entity\CapxImporterInterface[] $importers */
+      $importers = $entity_type_manager->getStorage('capx_importer')->loadMultiple();
       $this->importers = $importers;
     }
   }

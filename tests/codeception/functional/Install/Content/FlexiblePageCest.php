878d9c1b44f5272e030ee5d9016da17fb6300912
robherba <roberto@fourkitchens.com>
2026-06-04 10:07:03 -0600

    chore(shs-6567): revert change in FlexiblePageCest.php

--- tests/codeception/functional/Install/Content/FlexiblePageCest.php
+++ tests/codeception/functional/Install/Content/FlexiblePageCest.php
@@ -74,11 +74,11 @@ class FlexiblePageCest {
     $I->waitForText('No media items are selected.');
 
     $card_title = $this->faker->words(3, TRUE);
-    $I->fillField('field_hs_page_components[1][subform][field_hs_collection_items][0][subform][field_hs_postcard_title][0][value]', $card_title);
+    $I->fillField('field_hs_page_components[0][subform][field_hs_collection_items][0][subform][field_hs_postcard_title][0][value]', $card_title);
     $I->cantSeeElement('.hs-duplicated');
     $I->click('Toggle Actions', '.paragraph-type--hs-postcard');
     $I->click('Duplicate', '.paragraph-type--hs-postcard');
-    $I->waitForText('Title', 10, '.hs-duplicated');
+    $I->waitForText('Title', 30, '.hs-duplicated');
     $I->canSeeInField('Title', $card_title);
   }
 
